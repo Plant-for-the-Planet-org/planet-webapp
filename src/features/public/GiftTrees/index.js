e31a22cc208b11d69f079b3d30a651282011f816
Harsh Vitra <harshvitra@gmail.com>
2020-06-25 03:07:54 +0530

    Place holder

---
+++ src/features/public/GiftTrees/index.js
@@ -0,0 +1,14 @@
+import Image from 'react-bootstrap/Image'
+
+export default function Gift() {
+    return (
+      <main>
+       <div style={{width:'100vw'}}>
+        <Image src='/static/images/home/background.jpg' fluid/>
+        {/* <div style={{height:400,width:300,backgroundColor:'#fff',border:'1px solid black'}}>
+            <p>Gift Form</p>
+        </div> */}
+       </div>
+      </main>
+    )
+}
--- src/features/public/Home/index.js
+++ src/features/public/Home/index.js
@@ -1,9 +1,27 @@
+import Image from 'react-bootstrap/Image'
+import styles from './Home.module.css'
 export default function Home() {
     return (
       <main>
-       <div style={{width:'100vw',height:'100vh'}}>
-         <img style={{height:'100%',overflow:'hidden',width:'100%'}} src='/static/images/home/background.jpg' alt="Image" />
+       <div style={{width:'100vw'}}>
+        <Image src='/static/images/home/background.jpg' fluid/>
        </div>
+
+       <section className={styles.section}>
+         <h2>Leaderboard</h2>
+       </section>
+
+       <section className={styles.section}>
+         <h2>Stories</h2>
+       </section>
+
+       <section className={styles.section}>
+         <h2>Timeline</h2>
+       </section>
+
+       <section className={styles.section}>
+         <h2>Tree Inventory</h2>
+       </section>
       </main>
     )
 }
