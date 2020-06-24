0740d43ab9c20cf7c7cd010bc5906ec93dde0148
Harsh Vitra <harshvitra@gmail.com>
2020-06-25 02:07:35 +0530

    Home page image

--- src/features/public/Home/index.js
+++ src/features/public/Home/index.js
@@ -1,43 +1,9 @@
 export default function Home() {
     return (
-        <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+       <div style={{width:'100vw',height:'100vh'}}>
+         <img style={{height:'100%',overflow:'hidden',width:'100%'}} src='/static/images/home/background.jpg' alt="Image" />
+       </div>
       </main>
     )
 }
