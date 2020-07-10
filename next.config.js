4325543508ddd09fb4ece4d4dadcd627f418e26a
Harsh Vitra <harshvitra@gmail.com>
2020-07-10 19:19:14 +0530

    ENV setup

--- next.config.js
+++ next.config.js
@@ -3,5 +3,22 @@ module.exports ={
   // your config for other plugins or the general next.js here...
   devIndicators: {
     autoPrerender: false,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    API_ENDPOINT: 'https://app-development.plant-for-the-planet.org'
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_ENDPOINT: 'https://app-development.plant-for-the-planet.org',
+    googleMapApiKey:'',
+    debug:true,
+    currency:'EUR',
+    mapIdsInventory:'',
+    bugsnagApiKey:'',
+    androidAppId:'org.pftp',
+    iosAppId:'1444740626',
+    locationApikKey:'',
+    mediaPath:'/media/cache'
   }
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Layout from '../src/features/common/Layout'
 import HomePage from '../src/features/public/Home'
 import {useDispatch} from 'react-redux'
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+
+// This is just to test the env vairables
+console.log('API ENDPOINT',publicRuntimeConfig.API_ENDPOINT);
+
 export default function Home() {
 
   const dispatch = useDispatch()
