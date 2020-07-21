5b3f546724b45d85ed1cd521eb4e1b06aef79629
Harsh Vitra <jainharsh44@gmail.com>
2020-07-21 21:30:29 +0530

    removed unnecessary page

--- src/features/common/Navbar/index.tsx
+++ src/features/common/Navbar/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Navbar,Nav} from 'react-bootstrap'
 import LOGO from '../../../assets/images/PlanetLogo'
-import Gift from '../../../assets/images/navigation/Gift'
-import Compete from '../../../assets/images/navigation/Compete'
 import Globe from '../../../assets/images/navigation/Globe'
 import Map from '../../../assets/images/navigation/Map'
 import Donate from '../../../assets/images/navigation/Donate'
@@ -16,9 +14,7 @@ export default function NavbarComponent(props:any) {
     const router = useRouter()
     let menuItems = [
         {id:1,name:'World',path:'/',icon:<Globe color={router.pathname === '/' ?'#89b35a' :'#2f3336'} />},
-        {id:2,name:'Gift Trees',path:'/gift',icon:<Gift color={router.pathname === '/gift' ?'#89b35a' :'#2f3336'}/>},
         {id:3,name:'Donate',path:'/donate',icon:<Donate color={router.pathname === '/donate' ?'#89b35a' :'#2f3336'} />},
-        {id:4,name:'Compete',path:'/compete',icon:<Compete color={router.pathname === '/compete' ?'#89b35a' :'#2f3336'}/>},
         {id:5,name:'Map',path:'/map',icon:<Map color={router.pathname === '/map' ?'#89b35a' :'#2f3336'} />},
     ]
     return (
