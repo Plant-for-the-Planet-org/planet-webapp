07cce7f6252c1888bb07243da619893df17aceb8
Harsh Vitra <jainharsh44@gmail.com>
2020-07-26 17:29:33 +0530

    Reduce loading time

--- src/features/public/Donations/components/ProjectSnippet.tsx
+++ src/features/public/Donations/components/ProjectSnippet.tsx
@@ -1,68 +1,85 @@
-import React, { ReactElement } from 'react'
-import styles from './../styles/Projects.module.scss'
-import { getImageUrl } from '../../../../utils/getImageURL'
-import Sugar from 'sugar'
-import Link from 'next/link'
+import Link from 'next/link';
+import React, { ReactElement } from 'react';
+import Sugar from 'sugar';
+import { getImageUrl } from '../../../../utils/getImageURL';
+import styles from './../styles/Projects.module.scss';
 
 interface Props {
-    project:any
+  project: any;
 }
 
-export default function ProjectSnippet({project}: Props): ReactElement {
-    const ImageSource = project.properties.image ? getImageUrl('project', 'large',project.properties.image) : '';
-    const progressPercentage = (project.properties.countPlanted / project.properties.countTarget)*100+'%';
-    return (
-        <div className={styles.singleProject}>
-            <Link prefetch={false} href="/donate/[id]" as={`/donate/${project.properties.id}`}>
-                <a>
-                    <div className={styles.projectImage}>
-                        {project.properties.image && typeof(project.properties.image) !== "undefined" ?
-                            <div className={styles.projectImageFile} style={{backgroundImage:`linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.2), rgba(0,0,0,0), rgba(0,0,0,0)),url(${ImageSource})`}}></div>
-                        : null }
-                        {project.properties.classification ? 
-                            <div className={styles.projectType}>
-                                {project.properties.classification}
-                            </div>:null
-                        }
-                        
-                        <div className={styles.projectName}>
-                        {Sugar.String.truncate(project.properties.name,34)}
-                        </div>
-                    </div>
-                </a>
-            </Link>
-                    
-                    <div className={styles.progressBar}>
-                        <div className={styles.progressBarHighlight} style={{width:progressPercentage}} />
-                    </div>
-                    <div className={styles.projectInfo}>
-                        <div className={styles.projectData}>
-                            <div className={styles.targetLocation}>
-                                <div className={styles.target}>
-                                    
-                                    {project.properties.countPlanted} planted •
-                                </div>
-                                <div className={styles.location}>
-                                {project.properties.location}
-                                </div>   
-                            </div>
-                            <div className={styles.projectTPOName}>
-                                By {project.properties.tpoName}
-                            </div>
-                        </div>
-                        <div className={styles.projectCost}>
-                            {project.properties.treeCost ? (
-                                <>
-                                    <div className={styles.costButton}>
-                                            {project.properties.currency === 'USD' ? '$' : project.properties.currency === 'EUR' ? '€' : project.properties.currency} {project.properties.treeCost.toFixed(2)}
-                                    </div>
-                                    <div className={styles.perTree}>
-                                        per tree
-                                    </div>
-                                </>
-                            ) : null}
-                        </div>
-                    </div>
-                </div>
-    )
+export default function ProjectSnippet({ project }: Props): ReactElement {
+  const ImageSource = project.properties.image
+    ? getImageUrl('project', 'medium', project.properties.image)
+    : '';
+  const progressPercentage =
+    (project.properties.countPlanted / project.properties.countTarget) * 100 +
+    '%';
+  return (
+    <div className={styles.singleProject}>
+      <Link
+        prefetch={false}
+        href="/donate/[id]"
+        as={`/donate/${project.properties.id}`}
+      >
+        <a>
+          <div className={styles.projectImage}>
+            {project.properties.image &&
+            typeof project.properties.image !== 'undefined' ? (
+              <div
+                className={styles.projectImageFile}
+                style={{
+                  backgroundImage: `linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.2), rgba(0,0,0,0), rgba(0,0,0,0)),url(${ImageSource})`,
+                }}
+              ></div>
+            ) : null}
+            {project.properties.classification ? (
+              <div className={styles.projectType}>
+                {project.properties.classification}
+              </div>
+            ) : null}
+
+            <div className={styles.projectName}>
+              {Sugar.String.truncate(project.properties.name, 34)}
+            </div>
+          </div>
+        </a>
+      </Link>
+
+      <div className={styles.progressBar}>
+        <div
+          className={styles.progressBarHighlight}
+          style={{ width: progressPercentage }}
+        />
+      </div>
+      <div className={styles.projectInfo}>
+        <div className={styles.projectData}>
+          <div className={styles.targetLocation}>
+            <div className={styles.target}>
+              {project.properties.countPlanted} planted •
+            </div>
+            <div className={styles.location}>{project.properties.location}</div>
+          </div>
+          <div className={styles.projectTPOName}>
+            By {project.properties.tpoName}
+          </div>
+        </div>
+        <div className={styles.projectCost}>
+          {project.properties.treeCost ? (
+            <>
+              <div className={styles.costButton}>
+                {project.properties.currency === 'USD'
+                  ? '$'
+                  : project.properties.currency === 'EUR'
+                  ? '€'
+                  : project.properties.currency}{' '}
+                {project.properties.treeCost.toFixed(2)}
+              </div>
+              <div className={styles.perTree}>per tree</div>
+            </>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
