658cbaa286648f98398cc4cc9e1b411ed079f681
Harsh Vitra <jainharsh44@gmail.com>
2020-07-26 17:34:45 +0530

    Commenting Pay

--- pages/donate/[id]/pay.tsx
+++ pages/donate/[id]/pay.tsx
@@ -1,38 +1,38 @@
-import dynamic from 'next/dynamic';
-import Layout from '../../../src/features/common/Layout';
-import { context } from '../../../src/utils/config';
+// import dynamic from 'next/dynamic';
+// import Layout from '../../../src/features/common/Layout';
+// import { context } from '../../../src/utils/config';
 
-const TreeDonation = dynamic(
-  () => import('./../../../src/features/public/Donations/screens/TreeDonation'),
-  { ssr: false, loading: () => <p>Loading...</p> }
-);
-export default function Donate({ project }: any) {
-  const DonateProps = {
-    project: project,
-  };
-  return (
-    <Layout>
-      <TreeDonation {...DonateProps} />
-    </Layout>
-  );
-}
+// const TreeDonation = dynamic(
+//   () => import('./../../../src/features/public/Donations/screens/TreeDonation'),
+//   { ssr: false, loading: () => <p>Loading...</p> }
+// );
+// export default function Donate({ project }: any) {
+//   const DonateProps = {
+//     project: project,
+//   };
+//   return (
+//     <Layout>
+//       <TreeDonation onClose={handleClose} {...DonateProps} />
+//     </Layout>
+//   );
+// }
 
-export async function getStaticProps({ params }: any) {
-  const res = await fetch(
-    `${context.api_url}/app/projects/${params.id}?_scope=extended`
-  );
-  const project = await res.json();
-  return {
-    props: { project }, // will be passed to the page component as props
-  };
-}
+// export async function getStaticProps({ params }: any) {
+//   const res = await fetch(
+//     `${context.api_url}/app/projects/${params.id}?_scope=extended`
+//   );
+//   const project = await res.json();
+//   return {
+//     props: { project }, // will be passed to the page component as props
+//   };
+// }
 
-// This function gets called at build time
-export async function getStaticPaths() {
-  const res = await fetch(`${context.api_url}/app/projects?_scope=extended`);
-  const projects = await res.json();
-  const paths = projects.map((project: any) => ({
-    params: { id: project.id },
-  }));
-  return { paths, fallback: false };
-}
+// // This function gets called at build time
+// export async function getStaticPaths() {
+//   const res = await fetch(`${context.api_url}/app/projects?_scope=extended`);
+//   const projects = await res.json();
+//   const paths = projects.map((project: any) => ({
+//     params: { id: project.id },
+//   }));
+//   return { paths, fallback: false };
+// }
