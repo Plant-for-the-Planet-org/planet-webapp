ab689812de96da60827db0765afdb75920c0c439
Harsh Vitra <jainharsh44@gmail.com>
2020-07-28 16:02:06 +0530

    Reduce the load time

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import '../src/features/public/Donations/styles/Maps.css';
-import { context } from '../src/utils/config';
 import ThemeProvider from '../src/utils/themeContext';
-function PlanetWeb({ Component, pageProps, config }: any) {
-  React.useEffect(() => {
-    localStorage.setItem('config', JSON.stringify(config));
-  }, [config]);
-
+function PlanetWeb({ Component, pageProps }: any) {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -17,22 +12,12 @@ function PlanetWeb({ Component, pageProps, config }: any) {
     }
   }, []);
 
-  let storedConfig;
-  if (typeof Storage !== 'undefined') {
-    storedConfig = localStorage.getItem('config');
-  }
-  return storedConfig ? (
+  return (
     <ThemeProvider>
       <CssBaseline />
-      <Component {...pageProps} config={config} />
+      <Component {...pageProps} />
     </ThemeProvider>
-  ) : null;
+  );
 }
-
-PlanetWeb.getInitialProps = async () => {
-  const res = await fetch(`${context.api_url}/public/v1.2/en/config`);
-  const config = await res.json();
-  return { config: config };
-};
 
 export default PlanetWeb;
