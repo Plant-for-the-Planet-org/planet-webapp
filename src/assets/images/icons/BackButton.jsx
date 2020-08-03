bb2597ca65484fdbd092ff6ec4054dc021385ea0
Nirali Parekh <nirali25parekh@gmail.com>
2020-08-03 14:18:00 +0530

    backButton implemented

---
+++ src/assets/images/icons/BackButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function BackButton(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style={{backgroundColor:'white', borderRadius:30, marginTop: 10, marginLeft: 10}}
+      width="30"
+      height="30"
+      enableBackground="new 0 0 219.151 219.151"
+      version="1.1"
+      viewBox="0 0 219.151 219.151"
+      xmlSpace="preserve"
+    >
+     <path 
+      fill={props.color ? props.color: "black"}
+      d="M94.861 156.507a7.502 7.502 0 0010.606 0 7.499 7.499 0 00-.001-10.608l-28.82-28.819 83.457-.008a7.5 7.5 0 00-.001-15l-83.46.008 28.827-28.825a7.5 7.5 0 00-10.607-10.608l-41.629 41.628a7.495 7.495 0 00-2.197 5.303 7.51 7.51 0 002.198 5.305l41.627 41.624z"></path>
+    </svg>
+  );
+}
+
+export default BackButton;
--- src/features/public/Donations/screens/ProjectDetails.tsx
+++ src/features/public/Donations/screens/ProjectDetails.tsx
@@ -1,11 +1,13 @@
 import Modal from '@material-ui/core/Modal';
 import React, { ReactElement } from 'react';
 import LazyLoad from 'react-lazyload';
+import Link from 'next/link';
 import Sugar from 'sugar';
 import BlackTree from '../../../../assets/images/icons/project/BlackTree';
 import Email from '../../../../assets/images/icons/project/Email';
 import Location from '../../../../assets/images/icons/project/Location';
 import WorldWeb from '../../../../assets/images/icons/project/WorldWeb';
+import BackButton from '../../../../assets/images/icons/BackButton';
 import { getCountryDataBy } from '../../../../utils/countryUtils';
 import { getImageUrl } from '../../../../utils/getImageURL';
 import GetProjectClassification from '../components/projectDetails/getProjectClassification';
@@ -18,26 +20,36 @@ interface Props {
 }
 
 function ProjectDetails({ project }: Props): ReactElement {
-
-
   const [rating, setRating] = React.useState<number | null>(2);
 
   const progressPercentage =
     (project.countPlanted / project.countTarget) * 100 + '%';
   const ImageSource = project.image
     ? getImageUrl('project', 'large', project.image)
-    : ''
+    : '';
 
   const contactDetails = [
     { id: 1, icon: <BlackTree />, text: 'View Profile', link: null },
-    { id: 2, icon: <WorldWeb />, text: project.website ?  project.website : 'unavailable', link: project.website },
+    {
+      id: 2,
+      icon: <WorldWeb />,
+      text: project.website ? project.website : 'unavailable',
+      link: project.website,
+    },
     {
       id: 3,
       icon: <Location />,
-      text: project.tpo.address ? project.tpo.address: 'unavailable',
-      link: project.coordinates ? `https://maps.google.com/?q=${project.coordinates.lat},${project.coordinates.lon}` : null,
+      text: project.tpo.address ? project.tpo.address : 'unavailable',
+      link: project.coordinates
+        ? `https://maps.google.com/?q=${project.coordinates.lat},${project.coordinates.lon}`
+        : null,
     },
-    { id: 4, icon: <Email />, text: project.tpo.email ? project.tpo.email : 'unavailable', link: project.tpo.email ? `mailto:${project.tpo.email}` : null },
+    {
+      id: 4,
+      icon: <Email />,
+      text: project.tpo.email ? project.tpo.email : 'unavailable',
+      link: project.tpo.email ? `mailto:${project.tpo.email}` : null,
+    },
   ];
 
   const loadImageSource = (image: any) => {
@@ -73,7 +85,15 @@ function ProjectDetails({ project }: Props): ReactElement {
                   style={{
                     backgroundImage: `linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.2), rgba(0,0,0,0), rgba(0,0,0,0)),url(${ImageSource})`,
                   }}
-                ></div>
+                >
+                  <Link prefetch={false} href="/donate/" as={`/donate/`}>
+                    <a>
+                      
+                        <BackButton />
+                      
+                    </a>
+                  </Link>
+                </div>
               </LazyLoad>
             ) : null}
 
