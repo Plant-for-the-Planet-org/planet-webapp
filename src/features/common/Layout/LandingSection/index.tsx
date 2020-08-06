aae915f0bf298ee110ace10660f308f8128a5761
Harsh Vitra <jainharsh44@gmail.com>
2020-08-06 21:51:41 +0530

    Changed image

--- src/features/common/Layout/LandingSection/index.tsx
+++ src/features/common/Layout/LandingSection/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import LazyLoad from 'react-lazyload';
 import styles from './LandingSection.module.scss';
 
-export default function LandingSection({ props }: any) {
+export default function LandingSection(props: any) {
   return (
     <section className={styles.landingSection}>
       <div className={styles.backgroundImage}>
         <LazyLoad>
-          <img src={'/static/images/home/BGHome.jpg'} />
+          <img
+            style={{ maxHeight: '100vh', minWidth: '100vw' }}
+            src={'/static/images/home/BackgroundImage.png'}
+          />
         </LazyLoad>
       </div>
 
