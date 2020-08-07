f9d43f8558acaf3a1ff53100f2680087dfe1cbfc
Jackson Garner <jackson.garner@salesforce.com>
2020-08-07 12:58:44 -0600

    Try providing signin Url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -22,6 +22,7 @@ const options = {
     Providers.Auth0({
       clientId: process.env.AUTH0_CLIENT_ID,
       domain: process.env.AUTH0_CUSTOM_DOMAIN,
+      signinUrl: '/',
     }),
     // Providers.Facebook({
     //   clientId: process.env.FACEBOOK_ID,
