c0bd0524ea9f78b0b2cf6a2f55733c3b6c07861e
Nirali Parekh <nirali25parekh@gmail.com>
2020-08-07 13:15:48 +0530

    view tpo profile linked

--- src/features/public/Donations/components/projectDetails/ProjectContactDetails.tsx
+++ src/features/public/Donations/components/projectDetails/ProjectContactDetails.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import styles from './../../styles/ProjectDetails.module.scss';
+import Link from 'next/link';
 
 interface Props {
   contactDetails: Array<{
@@ -14,7 +15,28 @@ function ProjectContactDetails({ contactDetails }: Props): ReactElement {
   return (
     <div className={styles.projectMoreInfo}>
       <div className={styles.infoTitle}>Contact Details</div>
-      {contactDetails.map((contactDetails) => (
+
+      {/* contactDetails tpo profile page link */}
+      <div key={contactDetails[0].id}>
+        <Link
+          prefetch={false}
+          href="/tpo/[id]"
+          as={`/tpo/${contactDetails[0].link}`}
+        >
+          <a>
+            <div className={styles.infoText + ' ' + styles.contactDetailsRow}>
+              {contactDetails[0].icon}
+              <span style={{ marginLeft: '16px', flexGrow: 1 }}>
+                {contactDetails[0].text}
+              </span>
+            </div>
+          </a>
+        </Link>
+      </div>
+
+
+    {/* contactDetails website, maps, mail links */}
+      {contactDetails.slice(1).map((contactDetails) => (
         <div key={contactDetails.id}>
           {contactDetails.link ? (
             <a href={contactDetails.link} target="_blank">
