77e98762cfe943b0ef88c645cd21bbd597dd438e
Harsh Vitra <jainharsh44@gmail.com>
2020-08-18 23:39:51 +0530

    Reset env var

--- src/utils/getStripe.tsx
+++ src/utils/getStripe.tsx
@@ -6,7 +6,7 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 let stripePromise: Promise<Stripe | null>;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.STRIPE_CONNECT_PUBLISHABLE_KEY!);
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
   }
   return stripePromise;
 };
