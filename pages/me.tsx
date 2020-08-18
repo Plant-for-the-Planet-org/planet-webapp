e73c718daa568b13ab2b4ddb19cd2850fa64f56e
Nirali Parekh <nirali25parekh@gmail.com>
2020-08-18 15:43:57 +0530

    top part ME page designed

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,9 +1,35 @@
-import Layout from '../src/features/common/Layout'
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import Footer from '../src/features/common/Footer';
+import Layout from '../src/features/common/Layout';
+import UserPage from '../src/features/public/UserProfile';
 
-export default function Me() {
+export default function UserProfile() {
+  const [userprofile, setUserprofile] = React.useState({});
+
+  const UserProps = {
+    userprofile: userprofile,
+  };
+  useEffect(() => {
+    async function loadUserData() {
+      // dummy data
+      let userprofile = {
+        countTarget : 200,
+        countPlanted: 50,
+        displayName: 'John Doe',
+        description: "I grew up planting trees with Plant-for-the-Planet and since 2013, we've planted over 6 Million trees near my hometown in Yucantan. Join the app and plant some more!",
+        isMe : true
+      }
+      setUserprofile(userprofile);
+    }
+    loadUserData();
+  }, []);
+
   return (
     <Layout>
-      <p>ME</p>
+      <UserPage {...UserProps} />
+
+      <Footer />
     </Layout>
-  )
+  ) ;
 }
--- src/features/public/UserProfile/index.tsx
+++ src/features/public/UserProfile/index.tsx
@@ -3,17 +3,68 @@ import { Col, Container, Row } from 'react-bootstrap';
 import LandingSection from '../../common/Layout/LandingSection';
 import TreeCounter from './../../common/TreeCounter/TreeCounter';
 import styles from './UserProfile.module.scss';
+import Redeem from '../../../assets/images/icons/userProfileIcons/Redeem';
+import Share from '../../../assets/images/icons/userProfileIcons/Share';
+import Shovel from '../../../assets/images/icons/userProfileIcons/Shovel';
+import Settings from '../../../assets/images/icons/userProfileIcons/Settings';
+import ScrollDown from '../../../assets/images/icons/userProfileIcons/ScrollDown'
 
 export default function UserProfile({ userprofile }: any) {
+  // console.log(DownArrow)
   return (
     <main>
+      {/* render only if it is ME page */}
+      {userprofile.isMe && (
+        <div className={styles.settingsIcon}>
+          <Settings color="white" />
+        </div>
+      )}
+      {userprofile.isMe && (
+        <div className={styles.downIcon}>
+          <ScrollDown color="white" />
+        </div>
+      )}
       <LandingSection>
         <div className={styles.landingContent}>
           <TreeCounter
             target={userprofile.countTarget}
             planted={userprofile.countPlanted}
           />
+
           <h2 className={styles.treeCounterName}>{userprofile.displayName}</h2>
+
+          {/* will render only if it is ME page */}
+          {userprofile.isMe && (
+            <React.Fragment>
+              <p className={styles.treeCounterDescription}>
+                {userprofile.description}{' '}
+              </p>
+
+              <Row className={styles.bottomIconsRow}>
+                <Col className={styles.iconTextColumn}>
+                  <div className={styles.bottomIconBg}>
+                    <Redeem color="white" />
+                  </div>
+                  <p className={styles.bottomRowText}> Redeem</p>
+                </Col>
+
+                <Col className={styles.iconTextColumn}>
+                  <div className={styles.bottomIconBg}>
+                    <Shovel color="white" />
+                  </div>
+
+                  <p className={styles.bottomRowText}> Register Trees</p>
+                </Col>
+
+                <Col className={styles.iconTextColumn}>
+                  <div className={styles.bottomIconBg}>
+                    <Share color="white" />
+                  </div>
+                  <p className={styles.bottomRowText}> Share </p>
+                </Col>
+              </Row>
+            </React.Fragment>
+          )}
         </div>
       </LandingSection>
       <Container fluid="md">
