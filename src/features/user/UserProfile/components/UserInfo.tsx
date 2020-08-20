5db7f1efcd0170e34b5baf388e21a589c2d90d9e
Nirali Parekh <nirali25parekh@gmail.com>
2020-08-20 19:32:48 +0530

    landing page fixed

---
+++ src/features/user/UserProfile/components/UserInfo.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import styles from '../UserProfile.module.scss';
+import Layout from '../../../common/Layout';
+import TreeCounter from './../../../common/TreeCounter/TreeCounter';
+import UserProfileOptions from './UserProfileOptions';
+
+export default function MyForestItem({ userprofile }: any) {
+  return (
+
+    
+
+
+    <div className={styles.landingContent}>
+        
+            <TreeCounter
+              target={userprofile.countTarget}
+              planted={userprofile.countPlanted}
+            />
+
+            <h2 className={styles.treeCounterName}>
+              {userprofile.displayName}
+            </h2>
+
+            {/* will render only if it is ME page */}
+            {userprofile.isMe && (
+              <React.Fragment>
+                {/* user description */}
+                <p className={styles.treeCounterDescription}>
+                  {userprofile.description}{' '}
+                </p>
+
+                {/* three icons in a row */}
+                <UserProfileOptions userprofile={userprofile} />
+              </React.Fragment>
+            )}
+          </div>
+  );
+}
+
+
