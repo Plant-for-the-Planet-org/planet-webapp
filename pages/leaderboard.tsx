8b8517e4093fb87f274354966b58aa2ce3880886
Ankit Gupta <agupta.ag19@gmail.com>
2020-08-24 23:10:14 +0530

    added pull to refresh in leaderboard and disabled in others

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,12 +1,38 @@
-import Layout from '../src/features/common/Layout';
 import dynamic from 'next/dynamic';
+import {
+  PullDownContent,
+  PullToRefresh,
+  RefreshContent,
+  ReleaseContent,
+} from 'react-js-pull-to-refresh';
+import Layout from '../src/features/common/Layout';
 
 const Tenant = process.env.TENANT ? process.env.TENANT : 'plantfortheplanet';
 const AboutPage = dynamic(() => import(`../src/tenants/${Tenant}/About/About`));
+
 export default function LeaderBoard() {
+  const isMobile = window.innerWidth <= 768;
+
+  function onRefresh() {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 2000);
+    });
+  }
+
   return (
-    <Layout>
-      <AboutPage />
-    </Layout>
+    <PullToRefresh
+      pullDownContent={<PullDownContent />}
+      releaseContent={<ReleaseContent />}
+      refreshContent={<RefreshContent />}
+      pullDownThreshold={150}
+      onRefresh={onRefresh}
+      triggerHeight={isMobile ? 100 : 0}
+      backgroundColor="white"
+      startInvisible={true}
+    >
+      <Layout>
+        <AboutPage />
+      </Layout>
+    </PullToRefresh>
   );
 }
