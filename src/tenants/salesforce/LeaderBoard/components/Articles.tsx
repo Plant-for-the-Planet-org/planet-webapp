d0061f3a661ac5af7ff9fc9d905c5058b58f29f2
Harsh Vitra <jainharsh44@gmail.com>
2020-08-31 18:34:43 +0200

    Article section

--- src/tenants/salesforce/LeaderBoard/components/Articles.tsx
+++ src/tenants/salesforce/LeaderBoard/components/Articles.tsx
@@ -4,7 +4,8 @@ export default function Articles() {
   const articles = [
     {
       id: 1,
-      treeCount: '1000000',
+      articleDescription:
+        'In Myanmar, only 16 percent of the original mangrove forest remains along the coastline. This project helps restore those forests while working with the local population to adapt to more sustainable practices. ',
       name:
         'Public policy and advocacy work - CA Climate Justice Corps proposal',
       imagePath: '/tenants/salesforce/images/Articles/1.png',
@@ -12,7 +13,8 @@ export default function Articles() {
     },
     {
       id: 2,
-      treeCount: '500000',
+      articleDescription:
+        'Salesforce advocates for this legislation that would remove the outdated cap on the Reforestation Trust Fund and nearly quadruple the amount of money available for reforestation across America’s national forests.',
       name: 'Planting trees in the Andes with Accion Andina ',
       imagePath: '/tenants/salesforce/images/Articles/1.png',
       link: '',
@@ -30,7 +32,7 @@ export default function Articles() {
             <Col key={article.id} sm={10} lg={6}>
               <div className={styles.articleSingleContainer}>
                 <img className={styles.articleImage} src={article.imagePath} />
-                <div className={styles.articleImageOverlay}></div>
+                {/* <div className={styles.articleImageOverlay}></div> */}
                 <div className={styles.articleInfoSection}>
                   <p className={styles.articletreeCount}>{article.treeCount}</p>
                   <p className={styles.articleTitle}>{article.name}</p>
