3be3a522bbf106b0ac22ed7b1a3dc1a3b04bd445
Sarvesh Warge <sarveshwarge@gmail.com>
2020-09-01 14:22:25 +0530

    load open sans in Layout

--- src/features/common/Header/index.tsx
+++ src/features/common/Header/index.tsx
@@ -11,6 +11,10 @@ export default function Layout() {
         href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap"
         rel="stylesheet"
       />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 }
