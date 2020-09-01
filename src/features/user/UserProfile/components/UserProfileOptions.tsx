f698b2dd1dc3405e88d1aa6ee9278ccc1589a990
Nirali Parekh <nirali25parekh@gmail.com>
2020-09-01 17:40:57 +0530

    web share api for share

--- src/features/user/UserProfile/components/UserProfileOptions.tsx
+++ src/features/user/UserProfile/components/UserProfileOptions.tsx
@@ -14,14 +14,25 @@ export default function UserProfileOptions({
   handleTextCopiedSnackbarOpen
  }: any) {
 
+  const webShareMobile = async() => {
+      try {
+        const response = await navigator.share({
+        text: 'Check out Plant for the Planet!',
+        })
+        console.log('Share complete', response)
+      } catch (error) {
+        console.error('Could not share at this time', error);
+      }
+  }
   
   const onShareClicked = () => {
-    if (!isMobileBrowser()) {
-      // desktop browser
+    if(navigator.share!== undefined) {
+      // if in phone and web share API supported
+      webShareMobile();
+    } else {
+      // in desktop
       navigator.clipboard.writeText('Dummy text copied to clipboard!');
       handleTextCopiedSnackbarOpen();
-    } else {
-      // mobile browser
     }
   };
 
