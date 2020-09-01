8523d745d37d0cb22c45c64e584747475895599e
Sarvesh Warge <sarveshwarge@gmail.com>
2020-09-01 19:41:03 +0530

    initialize react gtm in app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import React from 'react';
+import TagManager from 'react-gtm-module';
 import '../src/features/public/Donations/styles/Maps.scss';
 import '../src/theme/global.scss';
 import ThemeProvider from '../src/utils/themeContext';
 
 export default function PlanetWeb({ Component, pageProps }: any) {
+  const tagManagerArgs = {
+    gtmId: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
+  };
+  React.useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -17,17 +24,17 @@ export default function PlanetWeb({ Component, pageProps }: any) {
 
   React.useEffect(() => {
     async function loadConfig() {
-
-      const res = await fetch(`${process.env.API_ENDPOINT}/public/v1.2/en/config`, {
-        headers: { 'tenant-key': `${process.env.TENANTID}` }
-      })
-        .then(async (res) => {
-          const config = await res.json();
-          localStorage.setItem('config', JSON.stringify(config));
-          localStorage.setItem('countryCode', config.country);
-          localStorage.setItem('currencyCode', config.currency);
-        });
-
+      const res = await fetch(
+        `${process.env.API_ENDPOINT}/public/v1.2/en/config`,
+        {
+          headers: { 'tenant-key': `${process.env.TENANTID}` },
+        }
+      ).then(async (res) => {
+        const config = await res.json();
+        localStorage.setItem('config', JSON.stringify(config));
+        localStorage.setItem('countryCode', config.country);
+        localStorage.setItem('currencyCode', config.currency);
+      });
     }
     loadConfig();
   }, []);
