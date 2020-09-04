7e06f58ab3da4227c7e43e82d10d7060fed7f886
Nirali Parekh <nirali25parekh@gmail.com>
2020-09-04 16:35:02 +0530

    bg change

--- src/features/public/TpoProfile/index.tsx
+++ src/features/public/TpoProfile/index.tsx
@@ -14,7 +14,7 @@ export default function TpoProfile({ tpoprofile }: any) {
     <main>
       {/* tpoinfo section */}
  
-      <LandingSection fixedBg noFixedHeight>
+      <LandingSection fixedBg noFixedHeight imageSrc={`${process.env.CDN_URL}/media/images/app/bg_layer.jpg`}>
           <div className={styles.tpoProfileDiv}>
           <TpoInfo 
           tpoprofile={tpoprofile} 
