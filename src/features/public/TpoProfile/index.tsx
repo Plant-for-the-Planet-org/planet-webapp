10e82e7c992bb13c4555766948ddb5f4c845e065
Nirali Parekh <nirali25parekh@gmail.com>
2020-09-04 17:10:42 +0530

    cdn url env condition

--- src/features/public/TpoProfile/index.tsx
+++ src/features/public/TpoProfile/index.tsx
@@ -14,7 +14,7 @@ export default function TpoProfile({ tpoprofile }: any) {
     <main>
       {/* tpoinfo section */}
  
-      <LandingSection fixedBg noFixedHeight imageSrc={`${process.env.CDN_URL}/media/images/app/bg_layer.jpg`}>
+      <LandingSection fixedBg noFixedHeight imageSrc={process.env.CDN_URL ? `${process.env.CDN_URL}/media/images/app/bg_layer.jpg` : `https://cdn.plant-for-the-planet.org/media/images/app/bg_layer.jpg` }>
           <div className={styles.tpoProfileDiv}>
           <TpoInfo 
           tpoprofile={tpoprofile} 
