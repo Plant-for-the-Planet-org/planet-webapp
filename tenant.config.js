2bdbb611ae92adcc716c114799d4802110addf8b
Ankit Gupta <agupta.ag19@gmail.com>
2020-09-07 00:37:41 +0530

    removed tenant config from gitignore

---
+++ tenant.config.js
@@ -0,0 +1,14 @@
+export default function tenantConfig() {
+  return {
+    // name of tenant
+    tenantName: 'plantfortheplanet',
+    // tenant id
+    tenantID: 'ten_a7hDszU9',
+    // url of tenant home page
+    tenantURL: '',
+    // font family and it's property particular to tenant
+    font: {
+      fontFamily: '"Raleway", sans-serif',
+    },
+  };
+}
