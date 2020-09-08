665f59c32744c399cd30cadc2df5fb5655f79c2f
Sarvesh Warge <sarveshwarge@gmail.com>
2020-09-08 17:56:25 +0530

    articles and blogs

--- src/tenants/salesforce/LeaderBoard/components/LearnMore.tsx
+++ src/tenants/salesforce/LeaderBoard/components/LearnMore.tsx
@@ -7,11 +7,18 @@ export default function LearnMore() {
       <Col xs={12} md={6} className={styles.learnMoreSection}></Col>
       <Col xs={12} md={6} className={styles.learnMoreSectionText}>
         <p className={styles.learnMoreSectionTextHeader}>
-          Plant a Tree and Take Climate Action
+          We’re committed to the planet.
         </p>
         <p className={styles.learnMoreSectionTextPara}>
-          It's easy to get involved – choose a tree project to support, or give
-          the gift of a tree donation to a friend!
+          We believe business is one of the greatest platforms for change. We
+          commit to creating a sustainable, low-carbon future for all. That
+          means reducing emissions, as well as protecting and improving carbon
+          sinks, like forests.
+          <br />
+          <br />
+          Climate change disproportionately affects the most disadvantaged and
+          vulnerable populations, exacerbating existing economic and racial
+          inequalities. It’s critical that we take bold steps now.
         </p>
       </Col>
     </Row>
