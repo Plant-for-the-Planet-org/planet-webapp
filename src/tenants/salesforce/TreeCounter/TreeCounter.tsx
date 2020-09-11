b8458f107ec5710f4a9c3195e17596fb38c61524
Sarvesh Warge <sarveshwarge@gmail.com>
2020-09-11 14:27:00 +0530

    make it h2

--- src/tenants/salesforce/TreeCounter/TreeCounter.tsx
+++ src/tenants/salesforce/TreeCounter/TreeCounter.tsx
@@ -6,12 +6,12 @@ export default function TpoProfile(props: any) {
     <div className={treeCounterStyles.treeCounter}>
       <div className={treeCounterStyles.backgroundCircle}>
         <div className={treeCounterStyles.treeCounterDataField}>
-          <h3 className={treeCounterStyles.countNumber}>
+          <h2 className={treeCounterStyles.countNumber}>
             {Number(props.planted)} Million
-          </h3>
-          <h3 className={treeCounterStyles.countLabel}>
+          </h2>
+          <h2 className={treeCounterStyles.countLabel}>
             Trees Supported by the Salesforce Community
-          </h3>
+          </h2>
         </div>
       </div>
     </div>
