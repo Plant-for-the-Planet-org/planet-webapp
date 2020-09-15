090fb4f40a91fcd51b2de78fc98b77c53edcbd30
Harsh Vitra <jainharsh44@gmail.com>
2020-09-15 22:36:31 +0200

    Removed comments

--- src/features/public/Donations/components/ThankYou.tsx
+++ src/features/public/Donations/components/ThankYou.tsx
@@ -1,16 +1,13 @@
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
-import React, { ReactElement, useRef } from 'react';
+import React, { ReactElement } from 'react';
 import Sugar from 'sugar';
+import tenantConfig from '../../../../../tenant.config';
 import Close from '../../../../assets/images/icons/headerIcons/close';
 import { ThankYouProps } from '../../../common/types/donations';
 import styles from './../styles/ThankYou.module.scss';
-<<<<<<< HEAD
 import ShareOptions from './ShareOptions';
-import tenantConfig from '../../../../../tenant.config';
-=======
 import { getPaymentType } from './treeDonation/PaymentFunctions';
->>>>>>> develop
 
 function ThankYou({
   project,
@@ -23,33 +20,10 @@ function ThankYou({
   onClose,
   paymentType,
 }: ThankYouProps): ReactElement {
-<<<<<<< HEAD
   const config = tenantConfig();
   const imageRef = React.createRef();
 
-  let paymentTypeUsed;
-  switch (paymentType) {
-    case 'CARD':
-      paymentTypeUsed = 'Credit Card';
-      break;
-    case 'SEPA':
-      paymentTypeUsed = 'SEPA Direct Debit';
-      break;
-    case 'GOOGLE_PAY':
-      paymentTypeUsed = 'Google Pay';
-      break;
-    case 'APPLE_PAY':
-      paymentTypeUsed = 'Apple Pay';
-      break;
-    case 'BROWSER':
-      paymentTypeUsed = 'Browser';
-      break;
-    default:
-      paymentTypeUsed = 'Credit Card';
-  }
-=======
   let paymentTypeUsed = getPaymentType(paymentType);
->>>>>>> develop
 
   function Alert(props: AlertProps) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -60,8 +34,8 @@ function ThankYou({
   );
 
   const sendRef = () => {
-    return imageRef
-  }
+    return imageRef;
+  };
 
   const handleTextCopiedSnackbarOpen = () => {
     setTextCopiedSnackbarOpen(true);
@@ -85,36 +59,23 @@ function ThankYou({
         <div className={styles.headerTitle}>Thank You!</div>
       </div>
 
-<<<<<<< HEAD
-=======
-      <div className={styles.contributionAmount}>
-        Your {currency} {Sugar.Number.format(Number(treeCount * treeCost), 2)}{' '}
-        donation was successfully paid with {paymentTypeUsed}.
-      </div>
-
->>>>>>> develop
       <div className={styles.contributionMessage}>
         Your {currency} {Sugar.Number.format(Number(treeCount * treeCost), 2)}{' '}
         donation was successfully paid with {paymentTypeUsed}.
         {isGift &&
           `We've sent an email to ${giftDetails.recipientName} about the gift.`}{' '}
         Your {treeCount} trees will be planted by {project.name} in{' '}
-<<<<<<< HEAD
         {project.location}.
       </div>
 
       <div className={styles.contributionMessage}>
-        Maybe you'll visit them some day? In the mean time,
-=======
-        {project.location}. Maybe you'll visit them some day? In the mean time,
->>>>>>> develop
-        maybe hook up your friends with some trees of their own by telling them
-        our yours?
+        Maybe you'll visit them some day? In the mean time, maybe hook up your
+        friends with some trees of their own by telling them our yours?
       </div>
 
       <div className={styles.horizontalLine} />
 
-      <div className={styles.thankyouImageContainer} >
+      <div className={styles.thankyouImageContainer}>
         <div className={styles.thankyouImage} ref={imageRef}>
           <div className={styles.donationCount}>
             My {treeCount} trees are being planted in {project.location}
@@ -127,7 +88,7 @@ function ThankYou({
 
       <ShareOptions
         treeCount={treeCount}
-        sendRef = {sendRef}
+        sendRef={sendRef}
         handleTextCopiedSnackbarOpen={handleTextCopiedSnackbarOpen}
       />
 
@@ -146,10 +107,3 @@ function ThankYou({
 }
 
 export default ThankYou;
-
-// Thank you, Marc!
-// Your €11.84 donation was successful. We thank you for helping us fulfil our mission of bringing back the world’s forests. Your 10 trees will be planted by Yucatan Reforestation in Mexico. Maybe you’ll be able to visit them some day?
-// Want to do even more? Maybe hook up your friends with some trees of their own by telling them our yours?
-
-// Title: Planting trees against the climate crisis
-// Text: Preventing the climate crisis requires drastically reducing carbon emissions and planting trees. That’s why I just planted some. Check out salesforce.com/trees if you want to plant some too!
