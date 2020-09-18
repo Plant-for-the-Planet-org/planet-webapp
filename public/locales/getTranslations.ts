509cf3f558849f882e49b6fd2ba7c3879c3d2944
Harsh Vitra <jainharsh44@gmail.com>
2020-09-19 03:21:57 +0530

    solve local storage issue

--- public/locales/getTranslations.ts
+++ public/locales/getTranslations.ts
@@ -1,19 +1,31 @@
+/* eslint-disable camelcase */
 import de_me from './de/me.json';
 import me from './en/me.json';
 import es_me from './es/me.json';
 
 let language = 'en';
-
-let localLanguage = localStorage.getItem('language')
+let localLanguage;
+if (typeof Storage !== 'undefined') {
+  localLanguage = localStorage.getItem('language');
+}
 if (localLanguage) {
-    language = localLanguage
+  language = localLanguage;
 }
 
 export function getMe() {
-    switch (language) {
-        case 'de': return de_me;
-        case 'es': return es_me;
-        case 'en': return me;
-        default: return me
-    }
+  switch (language) {
+    case 'de': return de_me;
+    case 'es': return es_me;
+    case 'en': return me;
+    default: return me;
+  }
+}
+
+export function getHome() {
+  switch (language) {
+    case 'de': return de_me;
+    case 'es': return es_me;
+    case 'en': return me;
+    default: return me;
+  }
 }
