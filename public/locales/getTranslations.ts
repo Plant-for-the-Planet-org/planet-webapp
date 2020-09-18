ab91d43951798d9784592a1cff5fb432c1086ff4
Harsh Vitra <jainharsh44@gmail.com>
2020-09-18 18:18:16 +0530

    Get language from local storage

--- public/locales/getTranslations.ts
+++ public/locales/getTranslations.ts
@@ -1,10 +1,18 @@
 import de_me from './de/me.json';
 import me from './en/me.json';
+import es_me from './es/me.json';
 
-const language = 'de'
+let language = 'en';
+
+let localLanguage = localStorage.getItem('language')
+if (localLanguage) {
+    language = localLanguage
+}
+
 export function getMe() {
     switch (language) {
         case 'de': return de_me;
+        case 'es': return es_me;
         case 'en': return me;
         default: return me
     }
