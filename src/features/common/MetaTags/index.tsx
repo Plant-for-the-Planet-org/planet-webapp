6cb30d79d284bb7d69cc5f01cba25447c40a76d4
Harsh Vitra <jainharsh44@gmail.com>
2020-09-18 18:36:44 +0530

    Changing URLs

--- src/features/common/MetaTags/index.tsx
+++ src/features/common/MetaTags/index.tsx
@@ -21,7 +21,7 @@ export default function MetaTags({ title, desc, imageURL, ogType }: Props) {
       <meta property="og:description" content={desc} />
       <meta name="description" content={desc} />
       <meta property="og:type" content={ogType} />
-      <meta property="og:image" content={imageURL} />
+      {/* <meta property="og:image" content={imageURL} /> */}
       {config.tenantName === 'planet' ? (
         <link rel="alternate" href="android-app://org.pftp/projects" />
       ) : null}
@@ -31,16 +31,16 @@ export default function MetaTags({ title, desc, imageURL, ogType }: Props) {
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="theme-color" content="#4d5153" />
-      <link rel="shortcut icon" href="/public/tenants/planet/favicons/favicon.ico" type="image/x-icon" />
-      <link rel="apple-touch-icon" href="/public/tenants/planet/favicons/apple-touch-icon.png" />
-      <link rel="apple-touch-icon" sizes="57x57" href="/public/tenants/planet/favicons/apple-touch-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/public/tenants/planet/favicons/apple-touch-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/public/tenants/planet/favicons/apple-touch-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/public/tenants/planet/favicons/apple-touch-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/public/tenants/planet/favicons/apple-touch-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/public/tenants/planet/favicons/apple-touch-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/public/tenants/planet/favicons/apple-touch-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/public/tenants/planet/favicons/apple-touch-icon-180x180.png" />
+      <link rel="shortcut icon" href={`/tenants/${config.tenantName}/favicons/favicon.ico`} type="image/x-icon" />
+      <link rel="apple-touch-icon" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon.png`} />
+      <link rel="apple-touch-icon" sizes="57x57" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-57x57.png`} />
+      <link rel="apple-touch-icon" sizes="72x72" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-72x72.png`} />
+      <link rel="apple-touch-icon" sizes="76x76" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-76x76.png`} />
+      <link rel="apple-touch-icon" sizes="114x114" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-114x114.png`} />
+      <link rel="apple-touch-icon" sizes="120x120" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-120x120.png`} />
+      <link rel="apple-touch-icon" sizes="144x144" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-144x144.png`} />
+      <link rel="apple-touch-icon" sizes="152x152" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-152x152.png`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`/tenants/${config.tenantName}/favicons/apple-touch-icon-180x180.png`} />
     </Head>
   );
 }
