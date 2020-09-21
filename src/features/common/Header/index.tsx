4e8cbd38e293a3b087edf36add0f68b3e143d7c8
Sagar Aryal <sagar@aryal.me>
2020-09-21 16:22:30 +0200

    Merge branch 'develop' into tenant/salesforce

--- src/features/common/Header/index.tsx
+++ src/features/common/Header/index.tsx
@@ -14,10 +14,7 @@ export default function Layout() {
       {config.font.secondaryFontURL && (
         <link href={config.font.secondaryFontURL} rel="stylesheet" />
       )}
-      <meta
-        name="viewport"
-        content="width=device-width; width = 320; initial-scale=1.0; maximum-scale=1.0; user-scalable=yes; target-densitydpi=160dpi"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="application-name" content={config.meta.title} />
