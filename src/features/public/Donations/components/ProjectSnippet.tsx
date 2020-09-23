c071deb0e9ca7813f954ace7567f7d7ae468829e
Ankit Gupta <agupta.ag19@gmail.com>
2020-09-24 00:31:37 +0530

    changed donate button and added on tpo profile

--- src/features/public/Donations/components/ProjectSnippet.tsx
+++ src/features/public/Donations/components/ProjectSnippet.tsx
@@ -124,22 +124,17 @@ export default function ProjectSnippet({ project, key }: Props): ReactElement {
           <div className={styles.projectCost}>
             {project.properties.treeCost ? (
               <>
-                <div onClick={handleOpen} className={styles.costButton}>
-                  {/* {project.properties.currency === 'USD'
-                    ? '$'
-                    : project.properties.currency === 'EUR'
-                    ? '€'
-                    : project.properties.currency} */}
-                  {/* {project.properties.treeCost % 1 !== 0
-                    ? project.properties.treeCost.toFixed(2)
-                  : project.properties.treeCost} */}
+                <div onClick={handleOpen} className={styles.donateButton}>
+                  {t('common:donate')}
+                </div>
+                <div className={styles.perTree}>
                   {getFormatedCurrency(
                     i18n.language,
                     project.properties.currency,
                     project.properties.treeCost
-                  )}
+                  )}{' '}
+                  {t('donate:perTree')}
                 </div>
-                <div className={styles.perTree}>{t('donate:perTree')}</div>
               </>
             ) : null}
           </div>
