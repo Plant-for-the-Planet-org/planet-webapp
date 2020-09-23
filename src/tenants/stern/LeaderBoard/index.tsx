5da2a63df2ae70e6624233efed8ca8dd7839bfd0
Harsh Vitra <jainharsh44@gmail.com>
2020-09-23 12:55:51 +0530

    Common leaderboard for tenants

--- src/tenants/stern/LeaderBoard/index.tsx
+++ src/tenants/stern/LeaderBoard/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Leaderboard.module.scss';
 import LandingSection from '../../../features/common/Layout/LandingSection';
-import LeaderBoard from '../../../features/public/LeaderBoard';
+import LeaderBoard from '../../common/LeaderBoard';
 import TreeCounter from '../../../features/common/TreeCounter/TreeCounter';
 import Footer from '../../../features/common/Footer';
 
@@ -10,26 +10,27 @@ interface Props {
 }
 
 export default function About({ leaderboard, tenantScore }: Props) {
-  console.log('tenantScore', tenantScore);
-  
   return (
     <main>
       <LandingSection
         imageSrc={
           process.env.CDN_URL
             ? `${process.env.CDN_URL}/media/images/app/bg_layer.jpg`
-            : `https://cdn.plant-for-the-planet.org/media/images/app/bg_layer.jpg`
+            : 'https://cdn.plant-for-the-planet.org/media/images/app/bg_layer.jpg'
         }
       >
-        {tenantScore && tenantScore.total && 
-        <TreeCounter
-          target={tenantScore.total}
-          planted={tenantScore.total}
-          hideTarget
-        /> }
-        
-        <p className={styles.publicUserDescription} style={{fontWeight:'bold',marginBottom:'0px'}}>Baum pflanzen – Zeit gewinnen</p>
-        <p className={styles.publicUserDescription} style={{marginTop:'8px'}}>Mit Plant-for-the-Planet pflanzen wir weltweit Bäume und lassen somit unseren globalen STERN-Wald entstehen. Pro verkauftem Exemplar der Kliamwoche-Ausgabe spendet die Redaktion einen Baum. Jeder Baum, den wir pflanzen, bindet CO2 und schenkt uns Menschen wertvolle Zeit. Diese Zeit werden wir nutzen, um unsere CO2-Emissionen massiv zu reduzieren. Versprochen! Für die Menschen!
+        {tenantScore && tenantScore.total
+          && (
+          <TreeCounter
+            target={tenantScore.total}
+            planted={tenantScore.total}
+            hideTarget
+          />
+          ) }
+
+        <p className={styles.publicUserDescription} style={{ fontWeight: 'bold', marginBottom: '0px' }}>Baum pflanzen – Zeit gewinnen</p>
+        <p className={styles.publicUserDescription} style={{ marginTop: '8px' }}>
+Mit Plant-for-the-Planet pflanzen wir weltweit Bäume und lassen somit unseren globalen STERN-Wald entstehen. Pro verkauftem Exemplar der Kliamwoche-Ausgabe spendet die Redaktion einen Baum. Jeder Baum, den wir pflanzen, bindet CO2 und schenkt uns Menschen wertvolle Zeit. Diese Zeit werden wir nutzen, um unsere CO2-Emissionen massiv zu reduzieren. Versprochen! Für die Menschen!
         </p>
       </LandingSection>
       <LeaderBoard leaderboard={leaderboard} />
