1e5255e0b645b9712560f8f79d0829d87d22d79c
Harsh Vitra <jainharsh44@gmail.com>
2020-09-25 14:27:49 +0530

    Removed config from server

--- i18n/server.js
+++ i18n/server.js
@@ -1,12 +1,9 @@
 const NextI18Next = require('next-i18next').default;
 const path = require('path');
-const getConfig = require('../tenant.config');
-
-const config = getConfig();
 
 module.exports = new NextI18Next({
-  defaultLanguage: config.languages[0] ? config.languages[0] : 'en',
-  fallbackLng: config.languages[0] ? config.languages[0] : 'en',
-  otherLanguages: config.languages.splice(0, 1),
+  defaultLanguage: 'en',
+  fallbackLng: 'en',
+  otherLanguages: ['de'],
   localePath: path.resolve('./public/static/locales'),
 });
