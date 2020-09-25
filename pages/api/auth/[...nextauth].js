715ef6b488d62f22300e46a6da978c9f71de22b8
Nirali Parekh <nirali25parekh@gmail.com>
2020-09-25 13:52:55 +0530

    jwt encryption

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,10 +9,14 @@ const options = {
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.SECRET,
+  secret: process.env.NEXT_AUTH_SECRET,
   session: {
     jwt: true,
   },
+  jwt:{
+  secret: process.env.JWT_SECRET,
+  encryption: true,
+  }
   // pages: {
     // signIn: '/api/auth/signin',  // Displays signin buttons
     // signOut: '/api/auth/signout', // Displays form with sign out button
--- pages/me.tsx
+++ pages/me.tsx
@@ -6,8 +6,65 @@ import Layout from '../src/features/common/Layout';
 import UserPage from '../src/features/user/UserProfile';
 import tenantConfig from '../tenant.config';
 
+const dummyProfile = {
+  countTarget: 200,
+  countPlanted: 50,
+  displayName: 'John Doe',
+  description:
+    "I grew up planting trees with Plant-for-the-Planet and since 2013, we've planted over 6 Million trees near my hometown in Yucantan. Join the app and plant some more!",
+  isMe: true,
+  myForest: [
+    {
+      id: 1,
+      name: 'Eden Reforestration Projects',
+      country: 'Nepal',
+      gift: 'Gift to Sam Williams',
+      date: '10 February 2018',
+    },
+    {
+      id: 2,
+      name: 'Global Forest Generation',
+      country: 'Chile',
+      date: '10 April 2019',
+    },
+    {
+      id: 3,
+      name: 'Yucantan Reforestration',
+      country: 'Mexico',
+      gift: 'Gift to Sam Williams',
+    },
+    {
+      id: 4,
+      name: 'Global Forest Generation',
+      country: 'Chile',
+      date: '10 April 2019',
+    },
+    {
+      id: 5,
+      name: 'Yucantan Reforestration',
+      country: 'Mexico',
+      gift: 'Gift to Sam Williams',
+      date: '10 April 2019',
+    },
+    {
+      id: 6,
+      name: 'Eden Reforestration Projects',
+      country: 'Nepal',
+      gift: 'Gift to Sam Williams',
+      date: '10 February 2018',
+    },
+    {
+      id: 7,
+      name: 'Yucantan Reforestration',
+      country: 'Mexico',
+      gift: 'Gift to Sam Williams',
+      date: '10 April 2019',
+    },
+  ],
+};
 const config = tenantConfig();
 export default function UserProfile() {
+  
   const router = useRouter();
   const [userprofile, setUserprofile] = React.useState({});
   const [ session, loading ] = useSession()
@@ -16,63 +73,7 @@ export default function UserProfile() {
   };
 
   const texts = getMe();
-  // dummy data
-  const dummyProfile = {
-    countTarget: 200,
-    countPlanted: 50,
-    displayName: 'John Doe',
-    description:
-      "I grew up planting trees with Plant-for-the-Planet and since 2013, we've planted over 6 Million trees near my hometown in Yucantan. Join the app and plant some more!",
-    isMe: true,
-    myForest: [
-      {
-        id: 1,
-        name: 'Eden Reforestration Projects',
-        country: 'Nepal',
-        gift: 'Gift to Sam Williams',
-        date: '10 February 2018',
-      },
-      {
-        id: 2,
-        name: 'Global Forest Generation',
-        country: 'Chile',
-        date: '10 April 2019',
-      },
-      {
-        id: 3,
-        name: 'Yucantan Reforestration',
-        country: 'Mexico',
-        gift: 'Gift to Sam Williams',
-      },
-      {
-        id: 4,
-        name: 'Global Forest Generation',
-        country: 'Chile',
-        date: '10 April 2019',
-      },
-      {
-        id: 5,
-        name: 'Yucantan Reforestration',
-        country: 'Mexico',
-        gift: 'Gift to Sam Williams',
-        date: '10 April 2019',
-      },
-      {
-        id: 6,
-        name: 'Eden Reforestration Projects',
-        country: 'Nepal',
-        gift: 'Gift to Sam Williams',
-        date: '10 February 2018',
-      },
-      {
-        id: 7,
-        name: 'Yucantan Reforestration',
-        country: 'Mexico',
-        gift: 'Gift to Sam Williams',
-        date: '10 April 2019',
-      },
-    ],
-  };
+  
   useEffect(() => {
     async function loadUserData() {
       // here, API call
