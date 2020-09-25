1e59700609473d61ea0c95b9727fcb80373b4fef
Harsh Vitra <jainharsh44@gmail.com>
2020-09-25 13:01:20 +0530

    Added tenant config in server

--- i18n/server.js
+++ i18n/server.js
@@ -1,9 +1,12 @@
 const NextI18Next = require('next-i18next').default;
 const path = require('path');
+const getConfig = require('../tenant.config');
+
+const config = getConfig();
 
 module.exports = new NextI18Next({
-  defaultLanguage: 'en',
-  fallbackLng: 'en',
-  otherLanguages: ['de'],
+  defaultLanguage: config.languages[0] ? config.languages[0] : 'en',
+  fallbackLng: config.languages[0] ? config.languages[0] : 'en',
+  otherLanguages: config.languages.splice(0, 1),
   localePath: path.resolve('./public/static/locales'),
 });
