704abc127ec0c7f44ecf7167e6241f7fbc7beab9
Nirali Parekh <nirali25parekh@gmail.com>
2020-10-03 01:52:28 +0530

    remove redundant import

--- pages/complete-signup.tsx
+++ pages/complete-signup.tsx
@@ -2,7 +2,6 @@ import { signIn, signOut, useSession, getSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Layout from '../src/features/common/Layout';
-import styles from '../src/features/user/UserProfile/styles/EditProfileModal.module.scss';
 import CompleteSignup from '../src/features/user/CompleteSignup'
 
 export default function UserProfile() {
