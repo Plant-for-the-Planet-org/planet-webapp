27aac36eea3b5f8ab58afdde1dca74c1480eff81
Nirali Parekh <nirali25parekh@gmail.com>
2020-10-05 20:29:13 +0530

    removed redundant userId

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -46,7 +46,7 @@ const options = {
     session: async (session, token) => {
       // now, session has accessToken. can be accessed by next-auth/client - useSession()
       session.accessToken = token.accessToken
-      session.userID = token.userID
+      // /session.userID = token.userID
       return session
     },    
   },
