e3e4331e41829d33b27b8296cdc537901b28fe90
Nirali Parekh <nirali25parekh@gmail.com>
2020-10-07 17:41:37 +0530

    Complete Signup document tile

--- pages/complete-signup.tsx
+++ pages/complete-signup.tsx
@@ -3,6 +3,10 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Layout from '../src/features/common/Layout';
 import CompleteSignup from '../src/features/user/CompleteSignup'
+import tenantConfig from '../tenant.config';
+import Head from 'next/head';
+
+const config = tenantConfig();
 
 export default function UserProfile() {
   const [session, loading] = useSession();
@@ -13,10 +17,15 @@ export default function UserProfile() {
       }
   }
   return(
+    <>
+    <Head>
+        <title>{`${config.meta.title} - Complete SignUp`}</title>
+    </Head>
     <Layout>
        <CompleteSignup
           style={{ height: '100vh', overflowX: 'hidden' }}
         />
     </Layout>
+    </>
   );
 }
