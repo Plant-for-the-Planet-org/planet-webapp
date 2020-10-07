81130d7055d812df05f235ec64ac622eb29895e6
Harsh Vitra <jainharsh44@gmail.com>
2020-10-07 20:25:44 +0530

    User profile meta and layout

--- pages/t/[id].tsx
+++ pages/t/[id].tsx
@@ -5,9 +5,10 @@ import Footer from '../../src/features/common/Footer';
 import Layout from '../../src/features/common/Layout';
 import PublicUserPage from '../../src/features/public/PublicUserProfile';
 import UserNotFound from '../../src/features/common/ErrorComponents/UserProfile/UserNotFound';
-import Head from 'next/head';
 import tenantConfig from '../../tenant.config';
 import getsessionId from '../../src/utils/getSessionId';
+import GetPublicUserProfileMeta from '../../src/utils/getMetaTags/GetPublicUserProfileMeta';
+
 const config = tenantConfig();
 
 interface Props {
@@ -39,7 +40,6 @@ export default function PublicUser(initialized: Props) {
           headers: { 'tenant-key': `${process.env.TENANTID}`, 'X-SESSION-ID': await getsessionId()  },
         }
       );
-
       if (res.ok === false) {
         setPublicUserprofile(null);
       } else {
@@ -58,53 +58,15 @@ export default function PublicUser(initialized: Props) {
 
   return (
     <>
-      <Head>
-        <title>{`${
-          publicUserprofile !== undefined
-            ? publicUserprofile.displayName
-            : config.meta.title
-        }`}</title>
-        <meta
-          property="og:site_name"
-          content={
-            publicUserprofile !== undefined
-              ? publicUserprofile.displayName
-              : config.meta.title
-          }
-        />
-        <meta
-          property="og:url"
-          content={`${process.env.SCHEME}://${config.tenantURL}`}
-        />
-        <meta
-          property="og:title"
-          content={`${
-            publicUserprofile !== undefined
-              ? publicUserprofile.displayName
-              : config.meta.title
-          }`}
-        />
-        <meta property="og:description" content={config.meta.description} />
-        <meta name="description" content={config.meta.description} />
-        <meta property="og:type" content="profile" />
-        <meta property="og:image" content={config.meta.image} />
-        {config.tenantName === 'planet' ? (
-          <link rel="alternate" href="android-app://org.pftp/projects" />
-        ) : null}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={config.meta.title} />
-        <meta name="twitter:site" content={config.meta.twitterHandle} />
-        <meta name="twitter:url" content={config.tenantURL} />
-        <meta name="twitter:description" content={config.meta.description} />
-      </Head>
-      {initialized && publicUserprofile ? (
-        <Layout>
-          <PublicUserPage {...PublicUserProps} />
-          <Footer />
-        </Layout>
-      ) : (
-        <UserProfleLoader />
-      )}
+      <GetPublicUserProfileMeta publicUserprofile={publicUserprofile} />
+      <Layout>
+        {initialized && publicUserprofile ? (
+            <PublicUserPage {...PublicUserProps} />
+          ) : (
+            <UserProfleLoader />
+        )}
+        <Footer />
+      </Layout>
     </>
   );
 }
