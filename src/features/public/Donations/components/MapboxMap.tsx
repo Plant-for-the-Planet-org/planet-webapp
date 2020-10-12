e7c3648020740528611f19f166686511df6367fb
Sarvesh Warge <sarveshwarge@gmail.com>
2020-10-12 07:47:57 +0530

    set default project marker at project coordinated

--- src/features/public/Donations/components/MapboxMap.tsx
+++ src/features/public/Donations/components/MapboxMap.tsx
@@ -65,6 +65,10 @@ export default function MapboxMap({
   React.useEffect(() => {
     if (showSingleProject) {
       if (project !== null) {
+        setSingleProjectLatLong([
+          project.coordinates.lat,
+          project.coordinates.lon,
+        ]);
         if (typeof project.sites !== 'undefined' && project.sites.length > 0) {
           if (project.sites[0].geometry !== null) {
             setCurrentSite(0);
@@ -77,7 +81,7 @@ export default function MapboxMap({
               setsiteExists(true);
             }, 300);
           } else {
-            setsiteExists(false); 
+            setsiteExists(false);
             setGeoJson(null);
             setSingleProjectLatLong([
               project.coordinates.lat,
@@ -99,34 +103,50 @@ export default function MapboxMap({
 
   React.useEffect(() => {
     if (showSingleProject) {
-      if(project) {
-      if (siteExists) {
-        if (geoJson !== null) {
-          const bbox = turf.bbox(geoJson.features[currentSite]);
-          const { longitude, latitude, zoom } = new WebMercatorViewport(
-            viewport
-          ).fitBounds(
-            [
-              [bbox[0], bbox[1]],
-              [bbox[2], bbox[3]],
-            ],
-            {
-              padding: {
-                top: 50,
-                bottom: isMobile ? 120 : 50,
-                left: isMobile ? 50 : 400,
-                right: isMobile ? 50 : 100,
-              },
-            }
-          );
+      if (project) {
+        if (siteExists) {
+          if (geoJson !== null) {
+            const bbox = turf.bbox(geoJson.features[currentSite]);
+            const { longitude, latitude, zoom } = new WebMercatorViewport(
+              viewport
+            ).fitBounds(
+              [
+                [bbox[0], bbox[1]],
+                [bbox[2], bbox[3]],
+              ],
+              {
+                padding: {
+                  top: 50,
+                  bottom: isMobile ? 120 : 50,
+                  left: isMobile ? 50 : 400,
+                  right: isMobile ? 50 : 100,
+                },
+              }
+            );
+            const newMapState = {
+              mapStyle: 'mapbox://styles/mapbox/satellite-v9',
+            };
+            const newViewport = {
+              ...viewport,
+              longitude,
+              latitude,
+              zoom,
+              transitionDuration: 4000,
+              transitionInterpolator: new FlyToInterpolator(),
+              transitionEasing: d3.easeCubic,
+            };
+            setViewPort(newViewport);
+            setMapState(newMapState);
+          }
+        } else {
           const newMapState = {
-            mapStyle: 'mapbox://styles/mapbox/satellite-v9',
+            mapStyle: 'mapbox://styles/sagararl/ckdfyrsw80y3a1il9eqpecoc7',
           };
           const newViewport = {
             ...viewport,
-            longitude,
-            latitude,
-            zoom,
+            longitude: singleProjectLatLong[1],
+            latitude: singleProjectLatLong[0],
+            zoom: 5,
             transitionDuration: 4000,
             transitionInterpolator: new FlyToInterpolator(),
             transitionEasing: d3.easeCubic,
@@ -140,33 +160,17 @@ export default function MapboxMap({
         };
         const newViewport = {
           ...viewport,
-          longitude: singleProjectLatLong[1],
-          latitude: singleProjectLatLong[0],
-          zoom: 5,
-          transitionDuration: 4000,
+          latitude: defaultMapCenter[0],
+          longitude: defaultMapCenter[1],
+          zoom: 1.4,
+          transitionDuration: 2400,
           transitionInterpolator: new FlyToInterpolator(),
           transitionEasing: d3.easeCubic,
         };
-        setViewPort(newViewport);
         setMapState(newMapState);
+        setViewPort(newViewport);
       }
-    } else {
-      const newMapState = {
-        mapStyle: 'mapbox://styles/sagararl/ckdfyrsw80y3a1il9eqpecoc7',
-      };
-      const newViewport = {
-        ...viewport,
-        latitude: defaultMapCenter[0],
-        longitude: defaultMapCenter[1],
-        zoom: 1.4,
-        transitionDuration: 2400,
-        transitionInterpolator: new FlyToInterpolator(),
-        transitionEasing: d3.easeCubic,
-      };
-      setMapState(newMapState);
-      setViewPort(newViewport);
     }
-  }
   }, [
     project,
     showSingleProject,
@@ -219,7 +223,7 @@ export default function MapboxMap({
         onClick={() => setPopupData({ ...popupData, show: false })}
       >
         {showSingleProject ? (
-          !siteExists ? (   
+          !siteExists ? (
             <Marker
               latitude={singleProjectLatLong[0]}
               longitude={singleProjectLatLong[1]}
@@ -378,7 +382,8 @@ export default function MapboxMap({
 
               <p className={styles.projectControlText}>
                 &nbsp;&nbsp;
-                {project!==null && siteExists &&
+                {project !== null &&
+                siteExists &&
                 project.sites.length !== 0 &&
                 geoJson.features[currentSite]
                   ? geoJson.features[currentSite].properties.name
