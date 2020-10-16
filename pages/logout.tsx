3bbe03d236c3374d1df10139cea982d43c1b193e
Nirali Parekh <nirali25parekh@gmail.com>
2020-10-16 22:23:22 +0530

    logout auth0 session

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -9,24 +9,42 @@ export default function Logout() {
   const [session, loading] = useSession();
   const router = useRouter();
 
-  // no session
-  if (!loading && !session) {
-    if (typeof window !== 'undefined') {
+  React.useEffect(() => {
+    const logoutAllSessions = async () => {
+      try {
+        // clear auth0 session
+        const logoutEndpoint = `https://${process.env.AUTH0_CUSTOM_DOMAIN}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${process.env.NEXTAUTH_URL}`;
+        console.log('auth0 logout endpoint:', logoutEndpoint);
+        if (typeof window !== 'undefined') {
+          router.push(logoutEndpoint);
+        }
+        console.log('auth0 session cleared.....')
+        
+        // clear application session
+        localStorage.removeItem('userExistsInDB');
+        localStorage.removeItem('userprofile');
+        signOut();
+        console.log('application session cleared.....');
+
+      } catch (e) {
+        console.log('error logout', e);
+      }
+    };
+
+    // no session
+    if (!loading && !session) {
+      if (typeof window !== 'undefined') {
         router.push('/');
       }
-  }
-
-  // session present
-  if (!loading && session) {
-    if (typeof Storage !== 'undefined'){
-      localStorage.removeItem('userExistsInDB')
-      localStorage.removeItem('userprofile')
-      signOut({ callbackUrl: '/' })
     }
-  }
- 
-  return (
-    <>
-    </>
-  );
+
+    // session present
+    if (!loading && session) {
+      if (typeof Storage !== 'undefined') {
+        logoutAllSessions();
+      }
+    }
+  }, [loading]);
+
+  return <></>;
 }
