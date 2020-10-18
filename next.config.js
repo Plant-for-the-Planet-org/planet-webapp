9979d9fea8bcb6aaa497226acc3c56090fc6e21a
Nirali Parekh <nirali25parekh@gmail.com>
2020-10-18 22:32:57 +0530

    nextauth url in next.config

--- next.config.js
+++ next.config.js
@@ -27,6 +27,8 @@ const scheme = process.env.SCHEME === 'http' || process.env.SCHEME === 'https'
   ? process.env.SCHEME
   : 'https';
 
+const nextAuthUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://auth0.pp.eco'; 
+
 const hasAssetPrefix = process.env.ASSET_PREFIX !== '' && process.env.ASSET_PREFIX !== undefined;
 
 module.exports = withSourceMaps({
@@ -91,7 +93,7 @@ module.exports = withSourceMaps({
     SCHEME: scheme,
     API_ENDPOINT: `${scheme}://${process.env.API_ENDPOINT}`,
     CDN_URL: `${scheme}://${process.env.CDN_URL}`,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: nextAuthUrl,
   },
   trailingSlash: false,
   reactStrictMode: true,
