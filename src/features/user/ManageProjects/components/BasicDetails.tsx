ab48fa4a8063872bc86a1bdce67473ee5c1bc197
Harsh Vitra <jainharsh44@gmail.com>
2020-10-22 12:52:58 +0530

    Data preparation for submitting basic details

--- src/features/user/ManageProjects/components/BasicDetails.tsx
+++ src/features/user/ManageProjects/components/BasicDetails.tsx
@@ -5,7 +5,6 @@ import i18next from './../../../../../i18n';
 import ToggleSwitch from '../../../common/InputTypes/ToggleSwitch';
 import styles from './../styles/StepForm.module.scss';
 import MapGL, { Marker } from 'react-map-gl';
-import { Editor, DrawPointMode, EditingMode } from 'react-map-gl-draw';
 import { MenuItem } from '@material-ui/core';
 
 const { useTranslation } = i18next;
@@ -16,51 +15,20 @@ interface Props {
 
 export default function BasicDetails({ handleNext }: Props): ReactElement {
   const { t, i18n } = useTranslation(['manageProjects']);
-  const defaultMapCenter = [36.96, -28.5];
+
+  // Map setup
+  const defaultMapCenter = [0, 0];
   const defaultZoom = 1.4;
-  const edit = new EditingMode();
-  const draw = new DrawPointMode();
-  const [receiveDonations, setReceiveDonations] = React.useState(true);
-  const [projectAnalysis, setProjectAnalysis] = React.useState(true);
-  const [reviewerExpense, setReviewerExpense] = React.useState(true);
-  const [publishProject, setPublishProject] = React.useState(true);
-  const [mode, setMode] = React.useState(draw);
-  const [editing, setEditing] = React.useState(false);
-  const [projectCoords, setProjectCoords] = React.useState(null);
-  const [features, setFeatures] = React.useState(null);
-
   const mapRef = React.useRef(null);
-
   const [viewport, setViewPort] = React.useState({
-    width: 700,
+    width: '100%',
     height: 400,
     latitude: defaultMapCenter[0],
     longitude: defaultMapCenter[1],
     zoom: defaultZoom,
   });
 
-  // In future if the details are present, we will feed default values here
-  const defaultBasicDetails = {
-    projectName: '',
-    projectURL: '',
-    projectType: '',
-    treeTarget: 0,
-    website: '',
-    aboutProject: '',
-    costPerTree: 0,
-  };
-
-  const [basicDetails, setBasicDetails] = React.useState(defaultBasicDetails);
-
-  const changeBasicDetails = (e: any) => {
-    setBasicDetails({ ...basicDetails, [e.target.name]: e.target.value });
-  };
-
-  const { register, handleSubmit, errors } = useForm({ mode: 'onChange' });
-  const _onViewportChange = (view: any) => setViewPort({ ...view });
-  const onSubmit = (data: any) => {
-    handleNext();
-  };
+  const [projectCoords, setProjectCoords] = React.useState([0, 0]);
 
   const changeLat = (e: any) => {
     setProjectCoords([
@@ -75,8 +43,34 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
       projectCoords ? projectCoords[1] : 0,
     ]);
   };
+  const _onViewportChange = (view: any) => setViewPort({ ...view });
 
-  const projectType = [
+
+  // Form Fields
+  // In future if the details are present, we will feed default values here
+  const defaultBasicDetails = {
+    projectName: '',
+    projectURL: '',
+    projectType: '',
+    treeTarget: 0,
+    website: '',
+    description: '',
+    costPerTree: 0,
+  };
+  const [receiveDonations, setReceiveDonations] = React.useState(true);
+  const [reviewerExpense, setReviewerExpense] = React.useState(true);
+  const [publishProject, setPublishProject] = React.useState(true);
+  const [projectAnalysis, setProjectAnalysis] = React.useState(true);
+
+  const [basicDetails, setBasicDetails] = React.useState(defaultBasicDetails);
+
+  const changeBasicDetails = (e: any) => {
+    setBasicDetails({ ...basicDetails, [e.target.name]: e.target.value });
+  };
+
+  const { register, handleSubmit, errors } = useForm({ mode: 'onChange' });
+
+  const projectTypes = [
     { label: 'Large scale planting', value: 'large-scale-planting' },
     { label: 'Agroforestry', value: 'agroforestry' },
     { label: 'Natural Regeneration', value: 'natural-regeneration' },
@@ -84,6 +78,42 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
     { label: 'Urban Planting', value: 'urban-planting' },
     { label: 'Other Planting', value: 'other-planting' },
   ];
+
+  const onSubmit = (data: any) => {
+
+    console.log('Data', data);
+    // Project type
+    // Receive donations
+    // Lodging support
+
+    let submitData = {
+      name: data.projectName,
+      desiredSlug: data.projectURL,
+      classification: data.projectType,
+      geoLocation: {
+        type: 'Point',
+        coordinates: [
+          data.longitude,
+          data.latitude
+        ]
+      },
+      countTarget: Number(data.treeTarget),
+      webSite: data.website,
+      description: data.description,
+      acceptDonations: receiveDonations,
+      treeCost: data.costPerTree ? Number(data.costPerTree) : 0,
+      currency: 'EUR',
+      vistorAssitance: reviewerExpense,
+      publish: publishProject, 
+      enablePlantLocations: projectAnalysis 
+    }
+
+    console.log('Submit Data', submitData);
+
+
+    // handleNext();
+
+  };
 
   return (
     <div className={styles.stepContainer}>
@@ -150,7 +180,7 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
               value={basicDetails.projectType}
 
             >
-              {projectType.map((option) => (
+              {projectTypes.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
                   {option.label}
                 </MenuItem>
@@ -219,7 +249,7 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
           <MaterialTextField
             label={t('manageProjects:aboutProject')}
             variant="outlined"
-            name="aboutProject"
+            name="description"
             onChange={changeBasicDetails}
             multiline
             inputRef={register({
@@ -229,9 +259,9 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
               },
             })}
           />
-          {errors.aboutProject && (
+          {errors.description && (
             <span className={styles.formErrors}>
-              {errors.aboutProject.message}
+              {errors.description.message}
             </span>
           )}
         </div>
@@ -288,7 +318,7 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
 
         </div>
 
-        <div className={styles.formFieldLarge}>
+        <div className={`${styles.formFieldLarge} ${styles.mapboxContainer}`}>
           <MapGL
             {...viewport}
             ref={mapRef}
@@ -301,19 +331,8 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
                 setProjectCoords(event.lngLat);
               }
             }}
+
           >
-            {/* <Editor
-              clickRadius={12}
-              mode={mode}
-              features={features}
-              onUpdate={(data: any) => {
-                console.log(data.data[0].geometry.coordinates);
-                setProjectCoords(data.data[0].geometry.coordinates);
-                setMode(edit);
-                setEditing(true);
-                setFeatures(data.data);
-              }}
-            /> */}
             {projectCoords !== null ? (
               <Marker
                 latitude={projectCoords[1]}
@@ -325,26 +344,7 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
                 <div className={styles.marker}></div>
               </Marker>
             ) : null}
-            {/* {projectCoords !== null ? (
-              <div
-                onClick={() => {
-                  setProjectCoords(null);
-                  setFeatures([]);
-                  setEditing(false);
-                  setMode(draw);
-                }}
-                style={{
-                  height: 50,
-                  width: 50,
-                  backgroundColor: 'white',
-                  position: 'absolute',
-                  top: 0,
-                  right: 0,
-                }}
-              >
-                clear
-              </div>
-            ) : null} */}
+
           </MapGL>
           <div
             className={styles.formField}
@@ -362,8 +362,6 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
                 onInput={(e) => {
                   e.target.value = e.target.value.replace(/[^0-9.-]/g, '');
                 }}
-
-              // defaultValue={}
               />
             </div>
             <div className={styles.formFieldHalf}>
@@ -378,8 +376,6 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
                 onInput={(e) => {
                   e.target.value = e.target.value.replace(/[^0-9.-]/g, '');
                 }}
-
-              // defaultValue={}
               />
             </div>
           </div>
@@ -401,28 +397,38 @@ export default function BasicDetails({ handleNext }: Props): ReactElement {
           </div>
         </div>
 
-        {/* <div className={styles.formField}>
-          <ToggleSwitch
-            checked={publishProject}
-            onChange={() => setPublishProject(!publishProject)}
-            name="publishProject"
-            inputProps={{ 'aria-label': 'secondary checkbox' }}
-          />
-          <p>Publish Project (if projectstatus=Approved)</p>
+        <div className={styles.formField}>
+          <div className={styles.formFieldHalf}>
+            <div className={`${styles.formFieldRadio}`}>
+              <label htmlFor={'publishProject'}>Publish Project (if projectstatus=Approved)</label>
+              <ToggleSwitch
+                checked={publishProject}
+                onChange={() => setPublishProject(!publishProject)}
+                name="publishProject"
+                id="publishProject"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+              />
+
+            </div>
+          </div>
+          <div className={styles.formFieldHalf}>
+            <div className={`${styles.formFieldRadio}`}>
+              <label htmlFor={'projectAnalysis'}>
+                Detailed Project Analysis if projectstatus=Approved
+                Activate once all relevant data is submitted via Tree Mapper.
+                    </label>
+              <ToggleSwitch
+                checked={projectAnalysis}
+                onChange={() => setProjectAnalysis(!projectAnalysis)}
+                name="projectAnalysis"
+                id="projectAnalysis"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+              />
+
+            </div>
+          </div>
         </div>
 
-        <div className={styles.formField}>
-          <ToggleSwitch
-            checked={projectAnalysis}
-            onChange={() => setProjectAnalysis(!projectAnalysis)}
-            name="projectAnalysis"
-            inputProps={{ 'aria-label': 'secondary checkbox' }}
-          />
-          <p>
-            Detailed Project Analysis if projectstatus=Approved
-            Activate once all relevant data is submitted via Tree Mapper.
-                    </p>
-        </div> */}
 
         <div className={styles.formField} style={{ marginTop: '48px' }}>
           <div className={`${styles.formFieldHalf}`}>
