da685cb4f9da47a0531e85169cfe243e57ba69b8
Harsh Vitra <jainharsh44@gmail.com>
2020-10-22 21:25:16 +0530

    Mapbox icons

--- public/assets/images/icons/manageProjects/Trash.tsx
+++ public/assets/images/icons/manageProjects/Trash.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 function TrashIcon(props: any) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      // ariaHidden="true"
+      className="svg-inline--fa fa-trash-alt fa-w-14"
+      data-icon="trash-alt"
+      data-prefix="fas"
+      viewBox="0 0 448 512"
+    >
       <path
-        fill={props.color}
-        d="M296 432h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zm-160 0h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zM440 64H336l-33.6-44.8A48 48 0 0 0 264 0h-80a48 48 0 0 0-38.4 19.2L112 64H8a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h24v368a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V96h24a8 8 0 0 0 8-8V72a8 8 0 0 0-8-8zM171.2 38.4A16.1 16.1 0 0 1 184 32h80a16.1 16.1 0 0 1 12.8 6.4L296 64H152zM384 464a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16V96h320zm-168-32h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8z"
-      />
+        fill="currentColor"
+        d="M32 464a48 48 0 0048 48h288a48 48 0 0048-48V128H32zm272-256a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zM432 32H312l-9.4-18.7A24 24 0 00281.1 0H166.8a23.72 23.72 0 00-21.4 13.3L136 32H16A16 16 0 000 48v32a16 16 0 0016 16h416a16 16 0 0016-16V48a16 16 0 00-16-16z"
+      ></path>
     </svg>
   );
 }
