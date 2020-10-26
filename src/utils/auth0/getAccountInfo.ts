8e916b1da32003643f18cd020a672f1c5e56fa61
Nirali Parekh <nirali25parekh@gmail.com>
2020-10-26 18:32:03 +0530

    change treemapper->app endpoint

--- src/utils/auth0/getAccountInfo.ts
+++ src/utils/auth0/getAccountInfo.ts
@@ -2,7 +2,7 @@
 export async function getAccountInfo(session:any) {
     console.log('------API CALL TO THE BACKEND------');
     const response = await fetch(
-        `${process.env.API_ENDPOINT}/treemapper/profile`,
+        `${process.env.API_ENDPOINT}/app/profile`,
         {
           headers: {
             Authorization: `OAuth ${session.accessToken}`,
