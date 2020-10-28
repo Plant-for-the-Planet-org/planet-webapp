2054b56cfec6743d9b256699b635e28f6c3492e1
Harsh Vitra <jainharsh44@gmail.com>
2020-10-28 15:34:12 +0530

    No projects found

--- src/features/user/UserProfile/components/ProjectsContainer.tsx
+++ src/features/user/UserProfile/components/ProjectsContainer.tsx
@@ -6,7 +6,9 @@ import ProjectLoader from '../../../common/ContentLoaders/Projects/ProjectLoader
 import i18next from '../../../../../i18n';
 import styles from '../styles/ProjectsContainer.module.scss';
 import { getAuthenticatedRequest, getRequest } from '../../../../utils/apiRequests/api';
-import {  useSession } from 'next-auth/client';
+import { useSession } from 'next-auth/client';
+import AddProject from '../../../../../public/assets/images/icons/manageProjects/AddProject';
+import Link from 'next/link';
 
 const { useTranslation } = i18next;
 
@@ -14,56 +16,65 @@ const ProjectSnippet = dynamic(() => import('./../../../public/Donations/compone
   loading: () => <ProjectLoader />,
 });
 
-export default function ProjectsContainer({authenticatedType,userprofile }: any) {
+export default function ProjectsContainer({ authenticatedType, userprofile }: any) {
   const { t } = useTranslation(['donate']);
   const [session, loading] = useSession();
-  const [projects,setProjects] =React.useState([])
+  const [projects, setProjects] = React.useState([])
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     async function loadProjects() {
       // const currencyCode = getStoredCurrency();
       const privateURL = '/app/profile/projects';
       const publicURL = `/app/profiles/${userprofile.id}/projects`;
 
-      if(authenticatedType === 'private'){
+      if (authenticatedType === 'private') {
         await getAuthenticatedRequest(
-          privateURL,session
+          privateURL, session
         ).then(projects => {
           setProjects(projects);
         })
-      }else{
+      } else {
         await getRequest(
           publicURL,
         ).then(projects => {
           setProjects(projects);
         })
-      }      
+      }
     }
     loadProjects();
-  },[])
-  
+  }, [])
+
   return (
     <div style={{ margin: 'auto', maxWidth: '950px' }}>
-      {projects.length < 1 ? (
-        <div className={styles.projectNotFound}>
-          <LazyLoad>
-            <NotFound className={styles.projectNotFoundImage} />
-            <h5>{t('donate:noProjectsFound')}</h5>
-          </LazyLoad>
-        </div>
-      ) : (
-        <div className={styles.listProjects}>
-          <h6 className={styles.projectsTitleText}> {t('donate:PROJECTS')} </h6>
+      {projects.length < 1 ?
+        authenticatedType === 'private' ?
+          (
+            <Link href='/manage-projects/add-project'>
+                <div className={styles.projectNotFound}>
+                  <AddProject />
+                  <h2>Add Project</h2>
+                </div>
+            </Link>
+          ) : (
+            <div className={styles.projectNotFound}>
+              <LazyLoad>
+                <NotFound className={styles.projectNotFoundImage} />
+                <h5>{t('donate:noProjectsFound')}</h5>
+              </LazyLoad>
+            </div>
+          ) : (
+          <div className={styles.listProjects}>
+            <h6 className={styles.projectsTitleText}> {t('donate:PROJECTS')} </h6>
 
-          {projects.map((project: any) => {
-            return (
-              <div className={styles.singleProject} key={project.id}>
-                <ProjectSnippet key={project.id} project={project} directGift={null} setDirectGift={null} />
-              </div>
-            );
-          })}
-        </div>
-      )}
+            {projects.map((project: any) => {
+              return (
+                <div className={styles.singleProject} key={project.id}>
+                  <ProjectSnippet key={project.id} project={project} directGift={null} setDirectGift={null} />
+                </div>
+              );
+            })}
+          </div>
+        )}
     </div>
   );
 }
