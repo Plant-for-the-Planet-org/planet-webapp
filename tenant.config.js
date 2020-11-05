5f6d660edcde9f0ec552006ec6f4315d59987148
Sagar Aryal <sagar.aryal@plant-for-the-planet.org>
2020-11-05 15:52:27 +0100

    Reference config for tenant

--- tenant.config.js
+++ tenant.config.js
@@ -2,6 +2,7 @@ import sternConfig from './public/tenants/stern/config';
 import planetConfig from './public/tenants/planet/config';
 import salesforceConfig from './public/tenants/salesforce/config';
 import planetConfigBeta from './public/tenants/planet/configBeta';
+import lacoquetaConfig from './public/tenants/lacoqueta/config';
 
 export default function tenantConfig() {
   switch (process.env.TENANT) {
@@ -9,6 +10,7 @@ export default function tenantConfig() {
     case 'stern': return sternConfig;
     case 'salesforce': return salesforceConfig;
     case 'planetbeta': return planetConfigBeta;
+    case 'lacoqueta': return lacoquetaConfig;
     default: return planetConfig;
   }
 }
