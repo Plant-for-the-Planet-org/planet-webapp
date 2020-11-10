dc9c3e35b01d30b9783e0c00f1ff59a0d6961beb
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-11-10 13:52:58 +0100

    support and share buttonn

---
+++ src/features/user/UserProfile/components/UserShareAndSupport.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Share from '../../../../../public/assets/images/icons/userProfileIcons/Share';
+import Support from '../../../../../public/assets/images/icons/userProfileIcons/Support';
+import styles from '../styles/UserInfo.module.scss';
+
+export default function UserShareAndSupport({ userprofile }) {
+  return (
+    <div className={styles.bottomIconsRow}>
+      {userprofile.type !== 'tpo' && (
+        <div className={styles.iconTextColumnSupport}>
+          <div className={styles.bottomIconBgSupport}>
+            <Support width="37px" paddingLeft="10px" />
+            <p className={styles.bottomRowTextSupport}>Support</p>
+          </div>
+        </div>
+      )}
+      <div className={styles.iconTextColumnSupport}>
+        <div className={styles.bottomIconBgSupport}>
+          <Share width="39px" paddingLeft="10px" color="white" solid />
+          <p className={styles.bottomRowTextSupport}>Share</p>
+        </div>
+      </div>
+    </div>
+  );
+}
