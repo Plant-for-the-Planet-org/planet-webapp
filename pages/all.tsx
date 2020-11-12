5f212234958c604d6b019115af227e0f6fd32ce1
Harsh Vitra <jainharsh44@gmail.com>
2020-11-12 11:23:23 +0530

    Meta tags for leaderboard

--- pages/all.tsx
+++ pages/all.tsx
@@ -1,21 +1,14 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import Head from 'next/head';
 import Layout from '../src/features/common/Layout';
 import LeaderBoard from '../src/tenants/planet/LeaderBoard'
 import tenantConfig from '../tenant.config';
 import { getRequest } from '../src/utils/apiRequests/api';
-
+import GetLeaderboardMeta from './../src/utils/getMetaTags/GetLeaderboardMeta'
 const config = tenantConfig();
 
 export default function Home() {
   const router = useRouter();
-  // stores whether device is mobile or not;
-  const [isMobile, setIsMobile] = React.useState<boolean>(false);
-
-  React.useEffect(() => {
-    setIsMobile(window.innerWidth <= 768);
-  }, []);
 
   const [leaderboard, setLeaderboard] = React.useState(null);
   console.log(leaderboard);
@@ -27,12 +20,6 @@ export default function Home() {
     }
     loadLeaderboard();
   }, []);
-
-  function onRefresh() {
-    return new Promise((resolve) => {
-      setTimeout(resolve, 2000);
-    });
-  }
 
   let AllPage;
   function getAllPage() {
@@ -48,28 +35,7 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>{`Home | ${config.meta.title}`}</title>
-        <meta property="og:site_name" content={config.meta.title} />
-        <meta
-          property="og:url"
-          content={`${process.env.SCHEME}://${config.tenantURL}`}
-        />
-        <meta property="og:title" content={`Home | ${config.meta.title}`} />
-        <meta property="og:description" content={config.meta.description} />
-        <meta name="description" content={config.meta.description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={config.meta.image} />
-        {config.tenantName === 'planet' ? (
-          <link rel="alternate" href="android-app://org.pftp/projects" />
-        ) : null}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={config.meta.title} />
-        <meta name="twitter:site" content={config.meta.twitterHandle} />
-        <meta name="twitter:url" content={config.tenantURL} />
-        <meta name="twitter:description" content={config.meta.description} />
-      </Head>
-
+      <GetLeaderboardMeta/>
       <Layout>{getAllPage()}</Layout>
     </>
   );
