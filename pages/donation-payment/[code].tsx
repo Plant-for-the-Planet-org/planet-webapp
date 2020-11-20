df1f64d625a2bdc3d9fa9c5e45c6ff02228983b8
Harsh Vitra <jainharsh44@gmail.com>
2020-11-20 15:29:28 +0530

    Temp save

--- pages/donation-payment/[code].tsx
+++ pages/donation-payment/[code].tsx
@@ -1,6 +1,12 @@
 import Footer from '../../src/features/common/Layout/Footer';
 import { useRouter } from 'next/router';
 import React from 'react';
+import Modal from '@material-ui/core/Modal';
+import { Elements } from '@stripe/react-stripe-js';
+import { ThemeContext } from '../../src/theme/themeContext';
+import getStripe from '../../src/utils/stripe/getStripe';
+import DonationsPopup from '../../src/features/donations';
+
 interface Props {
     initialized: Boolean;
 }
@@ -32,8 +38,40 @@ function PaymentPage({ initialized }: Props) {
         flexDirection: 'column'
     }
 
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+  const { theme } = React.useContext(ThemeContext);
+
+  let projectDetails = {};
+
+  // Need to pass - 
+  // treeCost
+  // currency
+  // project id (GUID)
+  // taxDeductionCountries Array
+  // project name
+  // project tpo name
+  // country
+  
     return (
         <>
+        <Modal
+        className={`modal ${theme}`}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <Elements stripe={getStripe()}>
+          <DonationsPopup project={projectDetails} onClose={handleClose} />
+        </Elements>
+      </Modal>
             {initialized && (
                 <div style={styles}>
                     <h2>{router.query.code}</h2>
