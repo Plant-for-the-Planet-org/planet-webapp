3d9ea85300329c2ee591b723dbad225aabb252c9
Sarvesh Warge <sarveshwarge@gmail.com>
2020-11-22 16:11:17 +0530

    no support urls for TPOs

--- pages/s/[id].tsx
+++ pages/s/[id].tsx
@@ -2,24 +2,25 @@ import React, { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 import { getRequest } from '../../src/utils/apiRequests/api';
 
-interface Props {}
+interface Props { }
 
-export default function DirectGift({}: Props): ReactElement {
+export default function DirectGift({ }: Props): ReactElement {
   const router = useRouter();
   //   const [profile, setProfile] = React.useState(null);
   React.useEffect(() => {
     if (router && router.query.id) {
       async function loadPublicUserData() {
         const newProfile = await getRequest(`/app/profiles/${router.query.id}`);
-        // console.log(newProfile);
-        localStorage.setItem(
-          'directGift',
-          JSON.stringify({
-            id: newProfile.slug,
-            displayName: newProfile.displayName,
-            show: true,
-          })
-        );
+        if (newProfile.type !== 'tpo') {
+          localStorage.setItem(
+            'directGift',
+            JSON.stringify({
+              id: newProfile.slug,
+              displayName: newProfile.displayName,
+              show: true,
+            })
+          );
+        }
         router.push('/', undefined, {
           shallow: true,
         });
