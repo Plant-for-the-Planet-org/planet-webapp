a449c2949c2e980402ff4af4673b78f7ab812b47
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-11-26 15:04:40 +0100

    fix add target issue

--- src/features/user/UserProfile/components/AddTargetModal.tsx
+++ src/features/user/UserProfile/components/AddTargetModal.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useSession } from 'next-auth/client';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
+import { useForm } from 'react-hook-form';
 import Fade from '@material-ui/core/Fade';
 import styles from '../styles/RedeemModal.module.scss';
 import MaterialTextField from '../../../common/InputTypes/MaterialTextField';
 import { editProfile } from '../../../../utils/auth0/apiRequests';
 import i18next from '../../../../../i18n';
-import { useForm } from 'react-hook-form';
+import formStyles from '../styles/EditProfileModal.module.scss';
+import spinnerStyle from '../../ManageProjects/styles/StepForm.module.scss';
 
 const { useTranslation } = i18next;
 
@@ -22,8 +24,10 @@ export default function AddTargetModal({
   const [session, loading] = useSession();
   const { t } = useTranslation(['target']);
   const { register, handleSubmit, errors } = useForm({ mode: 'onBlur' });
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const apiCallChangeTarget = async () => {
+    setIsLoading(true);
     if (!loading && session) {
       const bodyToSend = {
         target: !target ? userprofile.score.target : target,
@@ -32,9 +36,11 @@ export default function AddTargetModal({
       if (res.status === 200) {
         handleAddTargetModalClose();
         changeForceReload(!forceReload);
+        setIsLoading(false);
       } else {
         console.log('edit target failed');
         handleAddTargetModalClose();
+        setIsLoading(false);
       }
     }
   };
@@ -54,29 +60,48 @@ export default function AddTargetModal({
       <Fade in={addTargetModalOpen}>
         <div className={styles.modal}>
           <h4>
-            <b>
-{' '}
-{t('target:setTarget')}
-{' '}
-            </b>
+            <b> {t('target:setTarget')} </b>
           </h4>
           <div className={styles.inputField}>
             <MaterialTextField
-              placeholder={userprofile.score.target ? userprofile.score.target : '10000'}
+              placeholder={
+                userprofile.score.target ? userprofile.score.target : '10000'
+              }
               InputProps={{ inputProps: { min: 0 } }}
               label=""
               type="number"
-              defaultValue={userprofile.score.target ? userprofile.score.target : null}
+              defaultValue={
+                userprofile.score.target ? userprofile.score.target : null
+              }
               onChange={(e) => setTarget(e.target.value)}
               variant="outlined"
               inputRef={register({
-                min: 0
+                min: 1,
               })}
               name="addTarget"
             />
           </div>
-          {errors.addTarget && <span>invalid</span>}
-          <div className={styles.continueButton} onClick={() => handleSubmit(apiCallChangeTarget())}>{t('target:targetSave')}</div>
+          {errors.addTarget && (
+            <span className={formStyles.formErrors}>
+              {t('target:targetErrorMessage')}
+            </span>
+          )}
+          {errors.addTarget ? (
+            <div className={styles.continueButton}>
+              {t('target:targetSave')}
+            </div>
+          ) : (
+            <div
+              className={styles.continueButton}
+              onClick={() => handleSubmit(apiCallChangeTarget())}
+            >
+              {isLoading ? (
+                <div className={spinnerStyle.spinner}></div>
+              ) : (
+                t('target:targetSave')
+              )}
+            </div>
+          )}
         </div>
       </Fade>
     </Modal>
--- src/features/user/UserProfile/components/Settings.tsx
+++ src/features/user/UserProfile/components/Settings.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/SettingsModal.module.scss';
 import i18next from '../../../../../i18n/server';
@@ -15,49 +15,55 @@ export default function SettingsContainer({
   changeForceReload,
   forceReload,
 }) {
-    const {useTranslation} = i18next;
-    const {t} = useTranslation(['me', 'common', 'editProfile']);
-    const router = useRouter();
-    return (
-        <div className={styles.modal}
-        style={{top: window.screen.width <= 768 && userType=='tpo' ? '10px' : null}}
+  const { useTranslation } = i18next;
+  const { t } = useTranslation(['me', 'common', 'editProfile']);
+  const router = useRouter();
+  return (
+    <div
+      className={styles.modal}
+      style={{
+        top: window.screen.width <= 768 && userType == 'tpo' ? '10px' : null,
+      }}
+    >
+      {userType == 'tpo' && (
+        <a
+          href={`#projectsContainer`}
+          onClick={handleSettingsModalClose}
+          className={styles.settingsItem}
         >
-            {
-              userType == 'tpo' && 
-              <a href={`#projectsContainer`} onClick={handleSettingsModalClose} className={styles.settingsItem}> {t('me:settingManageProject')} </a>
-            }
-            <div className={styles.settingsItem} 
-            onClick={handleEditProfileModalOpen}
-            > {t('editProfile:edit')} </div>
-            {/*  <div className={styles.settingsItem}> Change Password </div>
+          {' '}
+          {t('me:settingManageProject')}{' '}
+        </a>
+      )}
+      <div className={styles.settingsItem} onClick={handleEditProfileModalOpen}>
+        {' '}
+        {t('editProfile:edit')}{' '}
+      </div>
+      {/*  <div className={styles.settingsItem}> Change Password </div>
             <div className={styles.settingsItem}> Change Email </div>
             <div className={styles.settingsItem}> Embed Widget </div> */}
-            <div
-              className={styles.settingsItem}
-              onClick={() => {
-                if (typeof window !== 'undefined') {
-                  router.push(`/logout`);
-                }
-              }
-              }
-              >
-              <b>{t('me:logout')} </b>
-            </div>
-            <div
-              className={styles.settingsItem}
-              onClick={handleSettingsModalClose}
-            >
-              <div className={styles.cancelText}> {t('common:cancel')}</div>
-            </div>
-            <EditProfileModal
-            userprofile={userprofile}
-            editProfileModalOpen={editProfileModalOpen}
-            handleEditProfileModalClose={handleEditProfileModalClose}
-            handleEditProfileModalOpen={handleEditProfileModalOpen}
-            changeForceReload={changeForceReload}
-            forceReload={forceReload}
-            handleSettingsModalClose={handleSettingsModalClose}
-          />
-        </div>
-    )
+      <div
+        className={styles.settingsItem}
+        onClick={() => {
+          if (typeof window !== 'undefined') {
+            router.push(`/logout`);
+          }
+        }}
+      >
+        <b>{t('me:logout')} </b>
+      </div>
+      <div className={styles.settingsItem} onClick={handleSettingsModalClose}>
+        <div className={styles.cancelText}> {t('common:cancel')}</div>
+      </div>
+      <EditProfileModal
+        userprofile={userprofile}
+        editProfileModalOpen={editProfileModalOpen}
+        handleEditProfileModalClose={handleEditProfileModalClose}
+        handleEditProfileModalOpen={handleEditProfileModalOpen}
+        changeForceReload={changeForceReload}
+        forceReload={forceReload}
+        handleSettingsModalClose={handleSettingsModalClose}
+      />
+    </div>
+  );
 }
