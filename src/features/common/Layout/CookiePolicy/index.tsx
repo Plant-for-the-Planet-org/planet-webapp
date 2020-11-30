a664e196d4fead0750ca2a1b48407449133faae2
Sarvesh Warge <sarveshwarge@gmail.com>
2020-11-30 08:17:42 +0530

    hide cookie note after login

--- src/features/common/Layout/CookiePolicy/index.tsx
+++ src/features/common/Layout/CookiePolicy/index.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import CloseIcon from '../../../../../public/assets/images/icons/CloseIcon';
 import styles from './CookiePolicy.module.scss';
 import i18next from '../../../../../i18n';
+import { useSession } from 'next-auth/client';
 
 export default function CookiePolicy() {
+  const [session, loading] = useSession();
   const [showCookieNotice, setShowCookieNotice] = useState(true);
   const { useTranslation } = i18next;
   const { t, ready } = useTranslation(['leaderboard']);
@@ -15,6 +17,12 @@ export default function CookiePolicy() {
     }
     localStorage.setItem('cookieNotice', showCookieNotice.toString());
   }, [showCookieNotice]);
+
+  React.useEffect(() => {
+    if (!loading && session) {
+      setShowCookieNotice(false);
+    }
+  }, [loading, session]);
 
   return showCookieNotice ? (
     <div className={styles.cookieContainer}>
