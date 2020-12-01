6ab7b865432f0f9351f9b17a93ae41deb43d4127
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-01 06:31:49 +0100

    tree donation part testing

--- __test__/TreeDonation.test.js
+++ __test__/TreeDonation.test.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import TreeDonation from '../src/features/donations/screens/TreeDonation';
+import {project} from './mockData/projectData'
 
 describe('tree donation', () => {
-  it('it expects component not to render without props', () => {
-    const { getByText } = render(<TreeDonation />);
-    expect(getByText('trees')).toBeInTheDocument();
+  it('it expects component to have some text', () => {
+    const setIsTaxDeductible = jest.fn();
+    const { getByText } = render(<TreeDonation project={project} country="NG" setIsTaxDeductible={setIsTaxDeductible} treeCost="3" currency="NGN"  />);
+    expect(getByText('donate:treeDonation')).toBeInTheDocument();
   });
 });
