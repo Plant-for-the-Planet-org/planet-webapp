28762275e7725a4e9e457741504ea2580f7ec19c
Harsh Vitra <jainharsh44@gmail.com>
2020-12-03 22:36:49 +0530

    Complete functionality

--- pages/claim/[type]/[code].tsx
+++ pages/claim/[type]/[code].tsx
@@ -2,24 +2,25 @@ import React, { ReactElement } from 'react'
 import { useRouter } from 'next/router';
 import { useAuth0 } from '@auth0/auth0-react';
 import { postAuthenticatedRequest } from '../../../src/utils/apiRequests/api';
+import styles from './../../../src/features/user/UserProfile/styles/RedeemModal.module.scss';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import ShareOptions from '../../../src/features/donations/components/ShareOptions';
+import Close from '../../../public/assets/images/icons/headerIcons/close';
+import i18next from './../../../i18n'
+import tenantConfig from './../../../tenant.config'
+import LandingSection from '../../../src/features/common/Layout/LandingSection';
+
+const { useTranslation } = i18next;
 
 interface Props {
 
 }
 
 function ClaimDonation({ }: Props): ReactElement {
+    const { t } = useTranslation(['me', 'common', 'donate', 'redeem']);
 
-    // Check if the user is logged in or not. 
-
-    // If the user is not logged in - send the user to log in page, store the claim redirect link in the localstorage.
-    // When the user logs in, redirect user to the claim link from the localstorage and clear the localstorage. 
-
-    // For this create a separate function which fetches the link from the storage, clears the storage and then redirects the user using the link
-
-    // If the user is logged in - 
-    // Validate the code automatically 
-    // Once validated ask user to claim their donation
-    // Once claimed redirect them to their profile
+    const config = tenantConfig();
 
     const router = useRouter();
 
@@ -39,21 +40,51 @@ function ClaimDonation({ }: Props): ReactElement {
 
     const [ready, setReady] = React.useState(false);
 
-    console.log('type',type);
-    
+    const imageRef = React.createRef();
+    const sendRef = () => imageRef;
+
+    const [textCopiedsnackbarOpen, setTextCopiedSnackbarOpen] = React.useState(
+        false,
+    );
+    const handleTextCopiedSnackbarOpen = () => {
+        setTextCopiedSnackbarOpen(true);
+    };
+
+    const handleTextCopiedSnackbarClose = (
+        event?: React.SyntheticEvent,
+        reason?: string,
+    ) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setTextCopiedSnackbarOpen(false);
+    };
+
+    function Alert(props: AlertProps) {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+    }
+
+
+    const [codeRedeemed, setCodeRedeemed] = React.useState(false);
+
 
     React.useEffect(() => {
         if (router && router.query.type && router.query.code) {
-            setCode(router.query.code);
-            setType(router.query.type)
-            setReady(true);
+            if (type !== 'donation' || type !== 'donor' || type !== 'gift') {
+                setErrorMessage(t('redeem:invalidType'))
+            }
+            else{
+                setCode(router.query.code);
+                setType(router.query.type)
+                setReady(true);
+            }
         }
     }, [router]);
 
-    async function validateCode(code: any) {
+    async function validateCode(code: any, type: any) {
         setIsUploadingData(true)
         const submitData = {
-            type: 'donation',
+            type: type,
             code: code
         }
         if (!isLoading && isAuthenticated) {
@@ -80,31 +111,179 @@ function ClaimDonation({ }: Props): ReactElement {
     React.useEffect(() => {
         if (!isLoading && isAuthenticated) {
             // validate code
-            if (ready && code){
-                validateCode(code)
+            if (ready && code && type) {
+                validateCode(code, type)
             }
         }
         else if (!isLoading && !isAuthenticated) {
             // store the claim link in localstorage
             if (ready && typeof window !== 'undefined') {
-                localStorage.setItem('redirectLink',window.location.href);
+                localStorage.setItem('redirectLink', window.location.href);
                 loginWithRedirect({ redirectUri: `${process.env.NEXTAUTH_URL}/login` });
             }
         }
-    }, [isAuthenticated, isLoading,code])
+    }, [isAuthenticated, isLoading, code])
 
-    
+    async function redeemCode(code: any, type: any) {
+        setIsUploadingData(true)
+        const submitData = {
+            type: type,
+            code: code
+        }
+        if (!isLoading && isAuthenticated) {
+            let token = await getAccessTokenSilently();
+            let userLang = localStorage.getItem('language') || 'en';
+            postAuthenticatedRequest(`/api/v1.3/${userLang}/convertCode`, submitData, token).then((res) => {
+                if (res.code === 401) {
+                    setErrorMessage(res.message);
+                    setIsUploadingData(false)
+                }
+                else if (res.response.status === 'error') {
+                    setErrorMessage(res.errorText);
+                    setIsUploadingData(false)
+                }
+                else if (res.response.status === 'success') {
+                    setCodeRedeemed(true);
+                    setIsUploadingData(false);
+                    setCodeValidated(false);
+                }
+            })
+        }
+    }
+
+    const closeRedeem = () => {
+        setCodeValidated(false);
+        setCodeRedeemed(false);
+        if (typeof window !== 'undefined') {
+            router.push(`/`);
+        }
+    }
+
     return ready ? (
-        <div>
-            <h2>Claim</h2>
-            <h2>
-            {codeValidated && 'Code validated'} 
-            {router.query.code}
-            </h2>
-        </div>
-    ): (
-        <></>
-    )
+        <LandingSection>
+            {codeRedeemed && validCodeData ? (
+                <>
+                    <div className={styles.modalFinal}>
+                        <div className={styles.header}>
+                            <div onClick={() => closeRedeem()} className={styles.headerCloseIcon}>
+                                <Close />
+                            </div>
+                            <div className={styles.headerTitle}>
+                                {t('redeem:congratulations')}
+                            </div>
+                        </div>
+
+                        <div className={styles.thankyouImageContainer}>
+                            <div className={styles.thankyouImage}>
+                                <div className={styles.thankyouImageHeader}>
+                                    <p dangerouslySetInnerHTML={{ __html: t('donate:thankyouHeaderText') }} />
+                                </div>
+                                <div className={styles.donationCount}>
+                                    {t('redeem:myPlantedTreesByOrg', { count: validCodeData.treeCount, tpoName: validCodeData.tpos[0].tpoName })}
+                                    <p className={styles.donationTenant}>
+                                        {t('donate:plantTreesAtURL', { url: config.tenantURL })}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* hidden div for image download */}
+                        <div style={{ width: '0px', height: '0px', overflow: 'hidden' }}>
+                            <div className={styles.tempThankYouImage} ref={imageRef}>
+                                <div className={styles.tempthankyouImageHeader}>
+                                    <p dangerouslySetInnerHTML={{ __html: t('donate:thankyouHeaderText') }} />
+                                </div>
+                                <p className={styles.tempDonationCount}>
+                                    {t('redeem:myPlantedTreesByOrg', { count: validCodeData.treeCount, tpoName: validCodeData.tpos[0].tpoName })}
+                                </p>
+                                <p className={styles.tempDonationTenant}>
+                                    {t('donate:plantTreesAtURL', { url: config.tenantURL })}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={styles.shareOptions}>
+                            <ShareOptions
+                                treeCount={validCodeData.treeCount}
+                                sendRef={sendRef}
+                                handleTextCopiedSnackbarOpen={handleTextCopiedSnackbarOpen}
+                            />
+                        </div>
+
+                        <Snackbar
+                            open={textCopiedsnackbarOpen}
+                            autoHideDuration={4000}
+                            onClose={handleTextCopiedSnackbarClose}
+                        >
+                            <Alert onClose={handleTextCopiedSnackbarClose} severity="success">
+                                {t('donate:copiedToClipboard')}
+                            </Alert>
+                        </Snackbar>
+
+                    </div>
+                </>
+            ) : (
+                    <div className={styles.modal}>
+                        {codeValidated && validCodeData ? (
+                            <>
+
+                                {errorMessage && (
+                                    <span className={styles.formErrors}>{errorMessage}</span>
+                                )}
+
+                                <div className={styles.codeTreeCount}>
+                                    {validCodeData.treeCount}
+                                    <span>{t('common:trees')}</span>
+                                </div>
+
+                                <div className={styles.plantedBy}>
+                                    <span>{t('common:plantedBy')}</span>
+                                    <p>{validCodeData.tpos[0].tpoName}</p>
+                                </div>
+
+                                <div onClick={() => redeemCode(code, type)} className={styles.continueButton}>
+                                    {isUploadingData ? (
+                                        <div className={styles.spinner}></div>
+                                    ) : (t('redeem:addToMyTrees'))}
+                                </div>
+                            </>
+                        ) : (
+                                <>
+                                    <div>
+                                        {t('redeem:validating')} {code}
+                                    </div>
+
+                                    {errorMessage && (
+                                        <span className={styles.formErrors}>{errorMessage}</span>
+                                    )}
+                                    <div onClick={() => validateCode(code, type)} className={styles.continueButton}>
+                                        {isUploadingData ? (
+                                            <div className={styles.spinner}></div>
+                                        ) : (t('redeem:validateCode'))}
+                                    </div>
+                                </>
+                            )}
+
+                    </div>
+                )}
+        </LandingSection>
+    ) : (
+            <LandingSection>
+                <div className={styles.modalFinal}>
+                <div className={styles.header}>
+                            <div onClick={() => closeRedeem()} className={styles.headerCloseIcon}>
+                                <Close />
+                            </div>
+                            <div className={styles.headerTitle}>
+                            {errorMessage && (
+                                    <span className={styles.formErrors}>{errorMessage}</span>
+                                )}
+                            </div>
+                        </div>
+                    
+                </div>
+            </LandingSection>
+        )
 }
 
 export default ClaimDonation
