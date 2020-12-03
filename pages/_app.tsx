3ab098500317e8b68e13a70446f9d05c361bdb15
Harsh Vitra <jainharsh44@gmail.com>
2020-12-04 02:41:21 +0530

    Commneted logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -54,15 +54,14 @@ export default function PlanetWeb({ Component, pageProps, err }: any) {
 
 
   if (process.env.VERCEL_URL && typeof window !== 'undefined') {
-    if (process.env.VERCEL_URL !== window.location.hostname) {
-      router.replace(process.env.VERCEL_URL);
-    }
-  }
-
-  
-  if(typeof window !== 'undefined'){
     console.log('Vercel URL',process.env.VERCEL_URL);
     console.log('Host URL',window.location.hostname);
+    if (process.env.VERCEL_URL !== window.location.hostname) {
+      console.log('Replace');
+      
+      // router.replace(process.env.VERCEL_URL);
+      
+    }
   }
   
 
