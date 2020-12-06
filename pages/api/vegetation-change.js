48330d9ec0d3e904f382509a6e8c4d00e2905ed2
Sarvesh Warge <sarveshwarge@gmail.com>
2020-12-06 10:32:35 +0530

    edit vegetation change

--- pages/api/vegetation-change.js
+++ pages/api/vegetation-change.js
@@ -49,30 +49,33 @@ var runAnalysis = function (req, res) {
         // ... run analysis ...
         const sitePolygon = ee.FeatureCollection(req.body);
         var evi_2015 = ee
-          .ImageCollection('LANDSAT/LC08/C01/T1_ANNUAL_EVI')
-          .filterDate('2015-01-01', '2015-10-31');
+          .ImageCollection('LANDSAT/LC08/C01/T1_32DAY_EVI')
+          .filterDate('2015-03-01', '2015-05-31');
         var evi_2020 = ee
-          .ImageCollection('LANDSAT/LC08/C01/T1_ANNUAL_EVI')
-          .filterDate('2020-01-01', '2020-10-31');
+          .ImageCollection('LANDSAT/LC08/C01/T1_32DAY_EVI')
+          .filterDate('2020-03-01', '2020-05-31');
 
         var evi_2015_clipped = evi_2015
           .filterBounds(sitePolygon)
-          .mean()
+          .max()
           .clip(sitePolygon)
           .rename('EVI');
         var evi_2020_clipped = evi_2020
           .filterBounds(sitePolygon)
-          .mean()
+          .max()
           .clip(sitePolygon)
           .rename('EVI');
 
-        //un masked
-        var evi_difference = evi_2015_clipped.subtract(evi_2020_clipped);
+        var evi_difference = evi_2020_clipped
+          .subtract(evi_2015_clipped)
+          .gt(0.05)
+          .selfMask();
+        // var evi_difference = evi_2020_clipped.subtract(evi_2015_clipped);
 
         const mapId = await evi_difference.getMap({
           min: -1,
           max: 1,
-          palette: ['FFFFFF', 'FFFFFF', '00FF00'],
+          palette: ['FF0000', 'FFFFFF', '00FF00'],
         });
         console.log('response sent - ' + mapId.urlFormat);
         return res.status(200).json({
