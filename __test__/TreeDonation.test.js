a124837f2d8ab79ea5189a3d494e44b825cca189
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-11 11:20:45 +0100

    fix test issues

---
+++ __test__/TreeDonation.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import TreeDonation from '../src/features/donations/screens/TreeDonation';
+import { project } from './mockData/projectData';
+
+
+describe('tree donation', () => {
+  const setIsTaxDeductible = jest.fn();
+  it('it expects component to have some text', async() => {
+    const { findByText } = render(<TreeDonation project={project} country="NG" setIsTaxDeductible={setIsTaxDeductible} treeCost="3" currency="NGN" />);
+    const value = await findByText('Tree Donation');
+    expect(value).toBeInTheDocument();
+  });
+  it('it expects component to be click', async() => {
+    const setOpenTaxDeductionModal = jest.fn();
+    const { findByTestId } = render(<TreeDonation project={project} country="NG" setIsTaxDeductible={setIsTaxDeductible} treeCost="3" currency="NGN" onClick={setOpenTaxDeductionModal()} />);
+    const value = await findByTestId('taxDeductionId')
+    expect(value).toBeTruthy();
+    fireEvent.click(value);
+    expect(setOpenTaxDeductionModal).toBeCalledTimes(1);
+  });
+});
+
--- __test__/homePage.test.js
+++ __test__/homePage.test.js
@@ -1,7 +1,9 @@
-import { driver } from './helper';
+import {
+  driver
+} from './helper';
 
 describe('hompage', () => {
-    let testStatus = false;
+  let testStatus = false;
   afterEach(() => {
     if (testStatus) {
       driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Yaay! my sample test passed"}}');
@@ -10,16 +12,15 @@ describe('hompage', () => {
     }
     testStatus = false;
   });
-//   });
+  //   });
   it('should load homepage', () => {
     driver.get('https://www.trilliontreecampaign.org/').then(function () {
-  driver.getTitle().then(function (title) {
-    console.log(title);
-    expect(title).toBe("Together let's plant a Trillion Trees!");
-    testStatus = true
-    driver.quit();
-  });
-});
+      driver.getTitle().then(function (title) {
+        expect(title).toBe("Together let's plant a Trillion Trees!");
+        testStatus = true
+        // driver.quit();
+      });
+    });
   })
 })
 // HTTP Server should be running on 8099 port of GitHub runner
