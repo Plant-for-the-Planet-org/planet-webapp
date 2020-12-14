78e776e7af7c0a1a82e23ae0a563bd06825fa336
Harsh Vitra <jainharsh44@gmail.com>
2020-12-14 19:14:01 +0530

    Section 2

--- src/tenants/andalusia/Anilloverdegranada.tsx
+++ src/tenants/andalusia/Anilloverdegranada.tsx
@@ -43,6 +43,10 @@ function LandingSection() {
 function Section2() {
     return (
         <div className={styles.section2MainContainer}>
+            <div className={styles.section2Overlay}/>
+            <p>OBJETIVO (2020-2030)  - 200.000 ÁRBOLES</p>
+            <h2>Stop Talking, Start Planting </h2>
+            <span>Fase inicial (2020-2021) 6.500 árboles.</span>
 
         </div>
     )
