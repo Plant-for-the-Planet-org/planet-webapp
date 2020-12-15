0f7f95ba53f5b43122e2e6258fd1257462bef30f
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-15 17:23:38 +0100

    fix zip code for us

--- __test__/homePage.test.js
+++ __test__/homePage.test.js
@@ -40,7 +40,7 @@ describe('hompage', () => {
     await driver.findElement(By.name('email')).sendKeys('captainamiedi1@gmail.com');
     await driver.findElement(By.name('address')).sendKeys('43 block');
     await driver.findElement(By.name('city')).sendKeys('surulere');
-    await driver.findElement(By.name('zipCode')).sendKeys('6176 ZG');
+    await driver.findElement(By.name('zipCode')).sendKeys('99501');
     const val1 = await driver.findElement(By.xpath("//*[text()='Continue']"));
     await val1.click();
     await driver.findElement(By.xpath("//div[@id='cardNumber']/div/input")).sendKeys('4242424242424242');
