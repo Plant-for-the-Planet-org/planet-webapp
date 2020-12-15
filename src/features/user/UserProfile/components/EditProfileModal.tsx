26fffbe6e3d4e79929ea4471f0ea7ef31592d61f
Norbert Schuler <norbert.schuler@epublica.de>
2020-12-15 15:52:15 +0100

    put token in dependencies instead

--- src/features/user/UserProfile/components/EditProfileModal.tsx
+++ src/features/user/UserProfile/components/EditProfileModal.tsx
@@ -135,7 +135,7 @@ export default function EditProfileModal({
         }
       };
     });
-  }, []);
+  }, [token]);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: 'image/*',
