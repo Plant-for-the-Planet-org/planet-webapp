29bca36df14d8ce0a455dec3020738c6bf87d61c
Harsh Vitra <jainharsh44@gmail.com>
2020-12-17 20:06:48 +0530

    Project Map section

--- src/tenants/andalusia/Anilloverdegranada.tsx
+++ src/tenants/andalusia/Anilloverdegranada.tsx
@@ -4,6 +4,7 @@ import FeaturesSection from './components/FeaturesSection'
 import LandingSection from './components/LandingSection'
 import Objective from './components/Objective'
 import ProjectBy from './components/ProjectBy'
+import ProjectMap from './components/ProjectMap'
 import SustainableCity from './components/SustainableCity'
 import styles from './styles/anilloverdegranada.module.scss'
 
@@ -12,13 +13,16 @@ interface Props {
 }
 
 function Anilloverdegranada({ }: Props): ReactElement {
+    const projectID = 'proj_0CgTQJMm5zX624sdpVDet93O';
     return (
         <div className={styles.pageContainer}>
             <LandingSection />
             <Objective />
             <FeaturesSection/>
+            <ProjectMap projectID={projectID} />
             <SustainableCity/>
             <ProjectBy/>
+            
             <Footer/>
         </div>
     )
