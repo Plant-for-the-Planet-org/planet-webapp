275710914a17ff2699ea3c8f6c6c1bfcea83b15f
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-17 20:23:43 +0100

    trying some fix

--- __test__/RemoveDirectGift.test.js
+++ __test__/RemoveDirectGift.test.js
@@ -9,14 +9,20 @@ describe('remove', () => {
   });
   it('should remove gift direct', async () => {
     const url = await driver.getCurrentUrl();
-    await driver.navigate().to(`${url}s/sagar-aryal`);
-    const val = await donateButton();
-    await val.click();
-    await driver.switchTo().activeElement();
-    await driver.findElement(By.xpath("//*[text()='Remove']")).click();
-    await driver.findElement(By.name('checkedA')).click();
+    driver.navigate().to(`${url}s/sagar-aryal`).then(() => {
+      const val = donateButton().then(() => {
+        val.click().then(() => {
+          driver.switchTo().activeElement().then(() =>{
+            driver.findElement(By.xpath("//*[text()='Remove']")).click().then(() => {
+              driver.findElement(By.name('checkedA')).click().then(() => {
+                driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Action successful"}}');
+              })
+            })
+          })
+        })
+      })
+    })
     // await driver.quit();
-    driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Action successful"}}');
     // await val;
     // console.log(val1);
   });
