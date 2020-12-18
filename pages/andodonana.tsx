fd4a3152ff0b31da13beebb0ee691761f45f0aab
Rishabh <rishabhshilpasingh@gmail.com>
2020-12-18 22:18:47 +0530

    Added Andodonana

---
+++ pages/andodonana.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react'
+import Andodonana from '../src/tenants/andalusia/Andodonana'
+
+interface Props {
+    
+}
+
+function AnilloverdegranadaPage({}: Props): ReactElement {
+    return (
+        <Andodonana/>
+    )
+}
+
+export default AnilloverdegranadaPage
