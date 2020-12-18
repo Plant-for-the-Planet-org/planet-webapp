ec96538623c302c3cbafa5f700d6477c46a540ee
Harsh Vitra <jainharsh44@gmail.com>
2020-12-18 16:08:40 +0530

    Client ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,9 @@ import ProjectPropsProvider, {
   ProjectPropsContext,
 } from '../src/features/common/Layout/ProjectPropsContext';
 import UserPropsProvider from '../src/features/common/Layout/UserPropsContext';
+import tenantConfig from '../tenant.config';
+
+const configTenant = tenantConfig();
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   const config = getConfig();
@@ -131,7 +134,7 @@ export default function PlanetWeb({ Component, pageProps, err }: any) {
     return (
       <Auth0Provider
         domain={process.env.AUTH0_CUSTOM_DOMAIN}
-        clientId={process.env.AUTH0_CLIENT_ID}
+        clientId={process.env.AUTH0_CLIENT_ID ? process.env.AUTH0_CLIENT_ID : configTenant.AUTH0_CLIENT_ID}
         redirectUri={process.env.NEXTAUTH_URL}
         audience={'urn:plant-for-the-planet'}
         cacheLocation={'localstorage'}
