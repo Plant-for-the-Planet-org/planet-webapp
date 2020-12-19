02b06a70a5776896d4ee97178fd46d5d100985b6
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-19 17:15:37 +0100

    fix testing

--- __test__/homePage.test.js
+++ __test__/homePage.test.js
@@ -106,4 +106,46 @@ describe('hompage', () => {
       });
     });
   });
+  it ('should gift form', async () => {
+    await load();
+    const val = await donateButton();
+    await val.click();
+    await driver.switchTo().activeElement();
+    const val1 = await driver.findElement(By.name('checkedA'));
+    await val1.click();
+    await driver.findElement(By.name('recipientName')).sendKeys('Gift simon');
+    await driver.findElement(By.name('email')).sendKeys('bright.amidiagbe@plant-for-the-planet.org');
+    await driver.findElement(By.name('giftMessage')).sendKeys('Special gift to you my friend');
+    await driver.wait(until.elementLocated(By.id('treeDonateContinue')), 10000).click();
+    await driver.findElement(By.name('firstName')).sendKeys('Bright');
+    await driver.findElement(By.name('lastName')).sendKeys('Amidiagbe');
+    await driver.findElement(By.name('email')).sendKeys('captainamiedi1@gmail.com');
+    await driver.findElement(By.name('address')).sendKeys('43 block');
+    await driver.findElement(By.name('city')).sendKeys('surulere');
+    await driver.findElement(By.name('zipCode')).sendKeys('94203'); //for netherland 6176 ZG
+    const val3 = await driver.findElement(By.xpath("//*[text()='Continue']"));
+    await val3.click();
+    await driver.switchTo().activeElement();
+    driver.wait(until.elementLocated(By.xpath("//div[@id='cardNumber']/div/input"))).isEnabled().then((value) => {
+      value.sendKeys('4242424242424242');
+    });
+    driver.wait(until.elementLocated(By.xpath("//div[@id='expiry']/div/input"))).isEnabled().then((value) => {
+      value.sendKeys('1123');
+    });
+    driver.wait(until.elementLocated(By.xpath("//div[@id='cvc']/div/input"))).isEnabled().then((value) => {
+      value.sendKeys('1111');
+    });
+    driver.wait(until.elementLocated(By.className('PaymentDetails_continueButton__2eFJF'))).isDisplayed().then((value) => {
+      value.click();
+    });
+    await driver.wait(until.elementLocated(By.xpath("//*[text()='Thank You']")), 50000).getText().then((title) => {
+      expect(title).toBe('Thank You');
+      if (title.includes('Thank You')) {
+        driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains header!"}}');
+      } else {
+        driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Title does not contain header!"}}');
+      }
+    });
+    // console.log(val3);
+  });
 });
