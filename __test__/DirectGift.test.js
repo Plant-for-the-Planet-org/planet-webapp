3382b6db3566f72e510a7388233073472cb90166
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-19 15:17:25 +0100

    fix testing

--- __test__/DirectGift.test.js
+++ __test__/DirectGift.test.js
@@ -19,19 +19,26 @@ describe('direct gift', () => {
     await driver.findElement(By.name('email')).sendKeys('captainamiedi1@gmail.com');
     await driver.findElement(By.name('address')).sendKeys('43 block');
     await driver.findElement(By.name('city')).sendKeys('surulere');
-    await driver.findElement(By.name('zipCode')).sendKeys('99501'); // for netherland 6176 ZG
+    await driver.findElement(By.name('zipCode')).sendKeys('94203'); // for netherland 6176 ZG
     const val3 = await driver.findElement(By.xpath("//*[text()='Continue']"));
     await val3.click();
+    await driver.switchTo().activeElement();
     // await driver.findElement(By.xpath("//div[@id='cardNumber']/div/input")).clear();
-    // await driver.findElement(By.xpath("//div[@id='cardNumber']/div/input")).click();
-    await driver.wait(until.elementLocated(By.xpath("//div[@id='cardNumber']/div/input[@name='cardnumber'"))).sendKeys('4242424242424242');
-    // await driver.findElement(By.xpath("//div[@id='cardNumber']/div/input")).sendKeys('4242424242424242');
-    await driver.wait(until.elementLocated(By.xpath("//div[@id='expiry']/div/input[@name='exp-date'"))).sendKeys('11234');
-    await driver.wait(until.elementLocated(By.xpath("//div[@id='cvc']/div/input[@name='cvc'"))).sendKeys('1111');
-    const val2 = await driver.wait(until.elementLocated(By.className('PaymentDetails_continueButton__2eFJF')), 10000);
-    // const val2 = await driver.findElement(By.className('PaymentDetails_continueButton__2eFJF'));
-    await val2.click();
-    await driver.wait(until.elementLocated(By.xpath("//*[text()='Thank You']")), 50000).getText().then((title) => {
+    driver.wait(until.elementLocated(By.xpath("//div[@id='cardNumber']/div/input"))).isEnabled().then((value) => {
+      value.sendKeys('4242424242424242');
+      // await driver.wait(until.elementLocated(By.xpath("//div[@id='cardNumber']/div/input"))).sendKeys('4242424242424242');
+
+    })
+    driver.wait(until.elementLocated(By.xpath("//div[@id='expiry']/div/input"))).isEnabled().then((value) => {
+      value.sendKeys('1123');
+    });
+    driver.wait(until.elementLocated(By.xpath("//div[@id='cvc']/div/input"))).isEnabled().then((value) => {
+      value.sendKeys('1111');
+    });
+    driver.wait(until.elementLocated(By.className('PaymentDetails_continueButton__2eFJF'))).isDisplayed().then((value) => {
+      value.click();
+    });
+    driver.wait(until.elementLocated(By.xpath("//*[text()='Thank You']")), 50000).getText().then((title) => {
       expect(title).toBe('Thank You');
       if (title.includes('Thank You')) {
         driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains header!"}}');
