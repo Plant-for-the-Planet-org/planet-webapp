f0b7c4bf646b75f1fa14eaae30ffe25b4b8f9d8a
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-19 17:35:32 +0100

    test fixes

--- __test__/homePage.test.js
+++ __test__/homePage.test.js
@@ -149,20 +149,16 @@ describe('hompage', () => {
     });
   });
   it('should remove gift direct', async () => {
-    load().then(() => {
-      const url = driver.getCurrentUrl().then(() => {
-        driver.navigate().to(`${url}s/sagar-aryal`).then(() => {
-          const val = donateButton().then(() => {
-            val.click().then(() => {
-              driver.switchTo().activeElement().then(() => {
-                driver.findElement(By.xpath("//*[text()='Remove']")).click().then(() => {
-                  driver.findElement(By.name('checkedA')).click().then(() => {
-                    driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Action successful"}}');
-                  });
-                });
-              });
-            });
-          });
+    load().then(async () => {
+      const url = await driver.getCurrentUrl();
+      await driver.navigate().to(`${url}s/sagar-aryal`);
+      const val = await donateButton();
+      await val.click();
+      await driver.switchTo().activeElement();
+      // await driver.wait(until.elementLocated(By.id('treeDonateContinue')), 10000).click();
+      driver.findElement(By.xpath("//*[text()='Remove']")).click().then(() => {
+        driver.findElement(By.name('checkedA')).click().then(() => {
+          driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Action successful"}}');
         });
       });
     });
