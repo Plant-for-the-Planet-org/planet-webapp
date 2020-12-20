b726d1b381c12eb846e472b3675d92423a394777
Harsh Vitra <jainharsh44@gmail.com>
2020-12-21 03:27:58 +0530

    Changes for Granada page

--- pages/andoalcala.tsx
+++ pages/andoalcala.tsx
@@ -1,11 +1,17 @@
 import React, { ReactElement } from 'react'
 import Andoalcala from '../src/tenants/andalucia/Andoalcala'
+import { useRouter } from 'next/router';
 
 interface Props {
     
 }
 
 function AnilloverdegranadaPage({}: Props): ReactElement {
+    const router = useRouter();
+
+    if(process.env.TENANT !== 'andalucia'){
+        router.push('/')
+    }
     return (
         <Andoalcala/>
     )
--- pages/andodonana.tsx
+++ pages/andodonana.tsx
@@ -1,11 +1,17 @@
 import React, { ReactElement } from 'react'
 import Andodonana from '../src/tenants/andalucia/Andodonana'
+import { useRouter } from 'next/router';
 
 interface Props {
     
 }
 
 function AnilloverdegranadaPage({}: Props): ReactElement {
+    const router = useRouter();
+
+    if(process.env.TENANT !== 'andalucia'){
+        router.push('/')
+    }
     return (
         <Andodonana/>
     )
