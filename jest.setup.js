1d4fa0596e021031b2627fd48d3b76f53ae26d01
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-21 16:36:14 +0100

    test fixes

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom/extend-expect";
 // import "@testing-library/react/cleanup-after-each";
 import dotenv from 'dotenv';
+import { driver } from "./__test__/helper";
 
 dotenv.config({ path: '.env' });
 jest.setTimeout(300000);
+
+afterAll(async () => {
+    await driver.quit()
+})
