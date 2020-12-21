ef39c2692cfa9f401dd202325c93fc735f3ffde8
Sagar Aryal <sagar@aryal.me>
2020-12-21 13:46:41 +0100

    Implement Dynamic Vercel URLs

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testPathIgnorePatterns: ['<rootDit>/.next/', '<rootDir>/node_modules/'],
-    "moduleNameMapper": { 
-      "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules" 
+    "moduleNameMapper": {
+      "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules"
     },
     "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$",
     "globals": {
-      "__baseUrl__": "https://planet-webapp-oriagzlkz.vercel.app"
+      "__baseUrl__": process.env.BROWSERSTACK_TEST_URL
     }
   }
