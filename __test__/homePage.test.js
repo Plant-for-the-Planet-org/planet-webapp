f0dc21bd6230a7af48e7bfbbc832987d3afe3a8a
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-21 16:17:23 +0100

    test fixes

--- __test__/homePage.test.js
+++ __test__/homePage.test.js
@@ -41,7 +41,7 @@ describe('hompage', () => {
       await driver.findElement(By.name('email')).sendKeys('captainamiedi1@gmail.com');
       await driver.findElement(By.name('address')).sendKeys('43 block');
       await driver.findElement(By.name('city')).sendKeys('surulere');
-      await driver.findElement(By.name('zipCode')).sendKeys('94203'); // for netherland 6176 ZG
+      await driver.findElement(By.name('zipCode')).sendKeys('6176 ZG'); // for netherland 6176 ZG
       const val1 = await driver.findElement(By.xpath("//*[text()='Continue']"));
       await val1.click();
       await driver.switchTo().activeElement();
