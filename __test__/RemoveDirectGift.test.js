29e6afb1061eefc03ea364ba28267231e7e9f896
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-12-22 08:37:14 +0100

    text fixes

--- __test__/RemoveDirectGift.test.js
+++ __test__/RemoveDirectGift.test.js
@@ -1,4 +1,4 @@
-const { By } = require('selenium-webdriver');
+const { By, until } = require('selenium-webdriver');
 const { driver } = require('./helper');
 const { load } = require('./Pages');
 const { donateButton } = require('./Pages/homePage');
@@ -8,22 +8,28 @@ describe('remove', () => {
     await load();
   });
   it('should remove gift direct', async () => {
+    const url = await driver.getCurrentUrl();
+    await driver.navigate().to(`${url}s/sagar-aryal`);
+    await driver.wait(until.elementLocated(By.className('donateButton'))).click();
+    await driver.switchTo().activeElement();
+    await driver.findElement(By.xpath("//*[text()='Remove']")).click();
+    // await driver.findElement(By.name('checkedA')).click();
     // load().then(() => {
-      const url = driver.getCurrentUrl().then(() => {
-        driver.navigate().to(`${url}s/sagar-aryal`).then(() => {
-          const val = donateButton().then(() => {
-            val.click().then(() => {
-              driver.switchTo().activeElement().then(() => {
-                driver.findElement(By.xpath("//*[text()='Remove']")).click().then(() => {
+      // const url = driver.getCurrentUrl().then(() => {
+      //   driver.navigate().to(`${url}s/sagar-aryal`).then(() => {
+      //     const val = donateButton().then(() => {
+      //       val.click().then(() => {
+      //         driver.switchTo().activeElement().then(() => {
+      //           driver.findElement(By.xpath("//*[text()='Remove']")).click().then(() => {
                   driver.findElement(By.name('checkedA')).click().then(() => {
                     driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Action successful"}}');
                   });
-                });
-              });
-            });
-          });
-        });
-      });
+      //           });
+      //         });
+      //       });
+      //     });
+      //   });
+      // });
     // });
   });
 });
