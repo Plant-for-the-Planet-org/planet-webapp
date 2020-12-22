30d6f7fb98e6c00225a33684c38f7b77aa1fc28e
Sarvesh Warge <sarveshwarge@gmail.com>
2020-12-22 13:49:18 +0530

    view contribution

--- src/features/user/UserProfile/components/MyTrees/MyTrees.tsx
+++ src/features/user/UserProfile/components/MyTrees/MyTrees.tsx
@@ -7,6 +7,8 @@ import TreesIcon from '../../../../../../public/assets/images/icons/TreesIcon';
 import formatDate from '../../../../../utils/countryCurrency/getFormattedDate';
 import { getFormattedNumber } from '../../../../../utils/getFormattedNumber';
 import i18next from '../../../../../../i18n';
+import BackButton from '../../../../../../public/assets/images/icons/BackButton';
+import ProjectContactDetails from '../../../../projects/components/projectDetails/ProjectContactDetails';
 
 const MyTreesMap = dynamic(() => import('./MyTreesMap'), {
   loading: () => <p>loading</p>,
@@ -19,9 +21,30 @@ interface Props {
   authenticatedType: any;
 }
 
+const project = {
+  id: 'proj_6D2v791pcqVIE6qYcBkdmaXu',
+  location: 'Moffat, Dumfriesshire',
+  name: 'Talla & Gameshope, Tweedsmuir, Scotland',
+  slug: 'talla-gameshope-tweedsmuir-scotland',
+  tpo: {
+    id: 'tpo_6CjTMsBqdVKNbZKq5hlGBLji',
+    name: 'Natural Capital Partners',
+    email: 'rfay@naturalcapitalpartners.com',
+    address: {
+      address: '300 West Coleman Boulevard',
+      city: 'Mount Pleasant',
+      zipCode: '29464',
+      country: 'US',
+    },
+    slug: 'natural-capital-partners',
+  },
+  website: 'naturalcapitalpartners.com',
+};
+
 export default function MyTrees({ profile, authenticatedType }: Props) {
-  const { t, i18n, ready } = useTranslation(['country', 'me']);
-  const [contributions, setContributions] = React.useState();
+  const { t, i18n, ready } = useTranslation(['country', 'me', 'donate']);
+  const [contributions, setContributions] = React.useState(null);
+  const [expandedContribution, setExpandedContribution] = React.useState();
   React.useEffect(() => {
     async function loadFunction() {
       getRequestWithoutRedirecting(`/app/profiles/${profile.id}/contributions`)
@@ -34,6 +57,10 @@ export default function MyTrees({ profile, authenticatedType }: Props) {
     }
     loadFunction();
   }, [profile]);
+
+  const handleViewContribution = (id: any) => {
+    setExpandedContribution(id);
+  };
 
   const MapProps = {
     contributions,
@@ -54,72 +81,162 @@ export default function MyTrees({ profile, authenticatedType }: Props) {
               {contributions.map((item: any) => {
                 return (
                   <div className={styles.tree}>
-                    <div className={styles.dateRow}>
-                      {formatDate(item.properties.plantDate)}
-                    </div>
-                    <div className={styles.treeRow}>
-                      <div className={styles.textCol}>
-                        <div className={styles.title}>
-                          {item.properties.type === 'registration'
-                            ? t('me:registered')
-                            : item.properties.project?.name}
+                    {expandedContribution &&
+                    expandedContribution === item.properties.id ? (
+                      <div className={styles.singleContribution}>
+                        <div
+                          onClick={() => setExpandedContribution(null)}
+                          className={styles.backButton}
+                        >
+                          <BackButton />
                         </div>
-                        <div className={styles.country}>
-                          {item.properties.country
-                            ? t(
-                                'country:' +
-                                  item.properties.country.toLowerCase()
-                              )
-                            : null}
+                        <div className={styles.singleDate}>
+                          {formatDate(item.properties.plantDate)}
+                        </div>
+                        <div className={styles.singleTreeCount}>
+                          {`${getFormattedNumber(
+                            i18n.language,
+                            Number(item.properties.treeCount)
+                          )} ${
+                            item.properties.type === 'gift' ? 'Tree Gift' : ''
+                          }${
+                            item.properties.type === 'donation'
+                              ? 'Tree Donation'
+                              : ''
+                          }${
+                            item.properties.type === 'registration'
+                              ? 'Tree Registration'
+                              : ''
+                          }`}
+                        </div>
+                        <div className={styles.plantedBy}>
+                          {`Planted by ${
+                            item.properties.type === 'registration'
+                              ? 'User'
+                              : item.properties.project.owner.name
+                          }`}
                         </div>
                         {item.properties.type === 'gift' ? (
-                          <div className={styles.source}>
-                            {t('me:receivedTrees')}
-                          </div>
-                        ) : null}
-                        {item.properties.type === 'redeem' ? (
-                          <div className={styles.source}>
-                            {t('me:redeemedTrees')}
-                          </div>
-                        ) : null}
-                      </div>
-                      <div className={styles.numberCol}>
-                        <div className={styles.treeIcon}>
-                          <div
-                            style={
-                              item.properties.type === 'registration'
-                                ? { color: '#3D67B1' }
-                                : {}
-                            }
-                            className={styles.number}
-                          >
-                            {getFormattedNumber(
-                              i18n.language,
-                              Number(item.properties.treeCount)
-                            )}
-                          </div>
-                          <div className={styles.icon}>
-                            {item.properties.treeCount > 1 ? (
-                              <TreesIcon
-                                color={
-                                  item.properties.type === 'registration'
-                                    ? '#3D67B1'
-                                    : null
-                                }
-                              />
-                            ) : (
-                              <TreeIcon
-                                color={
-                                  item.properties.type === 'registration'
-                                    ? '#3D67B1'
-                                    : null
-                                }
-                              />
-                            )}
-                          </div>
+                          item.properties.giver ? (
+                            <div className={styles.singleSource}>
+                              From <a href="#">{item.properties.giver.name}</a>
+                            </div>
+                          ) : (
+                            ''
+                          )
+                        ) : (
+                          ''
+                        )}
+                        {item.properties.type === 'donation' ? (
+                          item.properties.recipient ? (
+                            <div className={styles.singleSource}>
+                              To{' '}
+                              <a href="#">{item.properties.recipient.name}</a>
+                            </div>
+                          ) : (
+                            ''
+                          )
+                        ) : (
+                          ''
+                        )}
+
+                        <div className={styles.projectDetails}>
+                          <ProjectContactDetails project={project} />
+                        </div>
+                        <div className={styles.buttons}>
+                          <div className={styles.certificate}>Certificate</div>
+                          <div className={styles.share}>Share</div>
                         </div>
                       </div>
-                    </div>
+                    ) : (
+                      <>
+                        <div className={styles.dateRow}>
+                          {formatDate(item.properties.plantDate)}
+                        </div>
+                        <div
+                          onClick={() =>
+                            handleViewContribution(item.properties.id)
+                          }
+                          className={styles.treeRow}
+                        >
+                          <div className={styles.textCol}>
+                            <div className={styles.title}>
+                              {item.properties.type === 'registration'
+                                ? t('me:registered')
+                                : item.properties.project?.name}
+                            </div>
+                            <div className={styles.country}>
+                              {item.properties.country
+                                ? t(
+                                    'country:' +
+                                      item.properties.country.toLowerCase()
+                                  )
+                                : null}
+                            </div>
+                            {item.properties.type === 'gift' ? (
+                              <div className={styles.source}>
+                                {item.properties.giver.name
+                                  ? t('me:receivedFrom', {
+                                      name: item.properties.giver.name,
+                                    })
+                                  : t('me:receivedTrees')}
+                              </div>
+                            ) : null}
+                            {item.properties.type === 'redeem' ? (
+                              <div className={styles.source}>
+                                {t('me:redeemedTrees')}
+                              </div>
+                            ) : null}
+                            {item.properties.type === 'donation' ? (
+                              <div className={styles.source}>
+                                {item.properties.recipient
+                                  ? t('me:giftToGiftee', {
+                                      gifteeName:
+                                        item.properties.recipient.name,
+                                    })
+                                  : null}
+                              </div>
+                            ) : null}
+                          </div>
+                          <div className={styles.numberCol}>
+                            <div className={styles.treeIcon}>
+                              <div
+                                style={
+                                  item.properties.type === 'registration'
+                                    ? { color: '#3D67B1' }
+                                    : {}
+                                }
+                                className={styles.number}
+                              >
+                                {getFormattedNumber(
+                                  i18n.language,
+                                  Number(item.properties.treeCount)
+                                )}
+                              </div>
+                              <div className={styles.icon}>
+                                {item.properties.treeCount > 1 ? (
+                                  <TreesIcon
+                                    color={
+                                      item.properties.type === 'registration'
+                                        ? '#3D67B1'
+                                        : null
+                                    }
+                                  />
+                                ) : (
+                                  <TreeIcon
+                                    color={
+                                      item.properties.type === 'registration'
+                                        ? '#3D67B1'
+                                        : null
+                                    }
+                                  />
+                                )}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    )}
                   </div>
                 );
               })}
