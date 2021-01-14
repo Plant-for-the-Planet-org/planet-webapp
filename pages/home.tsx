b6a9c8b2bc53546007406331b67f22dfa5207549
Harsh Vitra <jainharsh44@gmail.com>
2021-01-15 02:20:47 +0530

    Tenant setup

--- pages/home.tsx
+++ pages/home.tsx
@@ -59,6 +59,7 @@ export default function Home(initialized: Props) {
         HomePage = NitroSBHome;
         return <HomePage leaderboard={leaderboard} tenantScore={tenantScore} />;
       case 'energizer':
+      case 'interactClub':
         HomePage = BasicHome;
         return <HomePage leaderboard={leaderboard} tenantScore={tenantScore} />;
       default:
