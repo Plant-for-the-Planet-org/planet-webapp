ec136df033d60f5d23568bbd5264b63ef335d00f
Sarvesh Warge <sarveshwarge@gmail.com>
2021-01-19 15:37:34 +0530

    fix imagery comparison slider

--- src/features/projects/components/CompareMaps.tsx
+++ src/features/projects/components/CompareMaps.tsx
@@ -82,7 +82,7 @@ export default function MapCompare({
       var container = '#comparison-container';
 
       new MapboxCompare(before, after, container, {
-        mousemove: true, // Optional. Set to true to enable swiping during cursor movement.
+        mousemove: false, // Optional. Set to true to enable swiping during cursor movement.
         orientation: 'vertical', // Optional. Sets the orientation of swiper to horizontal or vertical, defaults to vertical
       });
 
