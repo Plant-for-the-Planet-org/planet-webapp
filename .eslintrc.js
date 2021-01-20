a83e59e2ddd9217d4be8c1ca09461db7cdce0571
Norbert Schuler <norbert.schuler@epublica.de>
2021-01-20 10:49:45 +0100

    activated recommended eslint checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     es2020: true,
   },
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'airbnb',
   ],
