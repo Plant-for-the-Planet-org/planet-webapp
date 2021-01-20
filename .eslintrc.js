2818177f51bc60f3a52f40dbaf1100f3b696e38b
Norbert Schuler <norbert.schuler@epublica.de>
2021-01-20 11:47:52 +0100

    disable some rules for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,13 @@ module.exports = {
     'react/jsx-indent': 'off',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
-    //'react/jsx-no-literals': 2,
+    'react/jsx-no-literals': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-empty-pattern': 'off',
+    'react/display-name': 'off',
+    'react/no-find-dom-node': 'off'
   },
   globals: {
     React: 'writable',
