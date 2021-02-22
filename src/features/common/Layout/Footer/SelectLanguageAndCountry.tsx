844dc99139583595befc42f87f3ca29dcf25c18a
Norbert Schuler <norbert.schuler@epublica.de>
2021-02-22 10:44:01 +0100

    made setCurrencyCode function optional at selecting language and country

--- src/features/common/Layout/Footer/SelectLanguageAndCountry.tsx
+++ src/features/common/Layout/Footer/SelectLanguageAndCountry.tsx
@@ -32,7 +32,7 @@ interface TransitionsModalProps {
   setSelectedCurrency: Function,
   selectedCountry: any,
   setSelectedCountry: Function,
-  setCurrencyCode: Function,
+  setCurrencyCode?: Function,
 }
 export default function TransitionsModal({
     openModal,
@@ -74,7 +74,7 @@ export default function TransitionsModal({
     if (currencyCode) {
       window.localStorage.setItem('currencyCode', currencyCode);
       setSelectedCurrency(currencyCode);
-      setCurrencyCode(currencyCode)
+      if (setCurrencyCode) setCurrencyCode(currencyCode)
     }
     handleModalClose();
   }
