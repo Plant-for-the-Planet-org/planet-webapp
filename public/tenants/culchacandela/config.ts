2240d8efd3cbc1e9b12fb9491649b34b4338c2f7
Sarvesh Warge <sarveshwarge@gmail.com>
2021-02-22 13:45:04 +0530

    Update config.ts

--- public/tenants/culchacandela/config.ts
+++ public/tenants/culchacandela/config.ts
@@ -3,7 +3,7 @@ const config = {
   tenantURL: 'wald.culchacandela.de',
   languages: ['en', 'de'],
   fallbackCurrency: 'EUR',
-  tenantGoal: null,
+  tenantGoal: 77.777,
   font: {
     primaryFontFamily: '"Raleway",Helvetica,Arial,sans-serif',
     primaryFontURL:
