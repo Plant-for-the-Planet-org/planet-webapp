e06b034373e15acd18f37616891e3cb00f551307
Norbert Schuler <norbert.schuler@epublica.de>
2021-02-26 13:51:58 +0100

    type fix for variable name

--- src/utils/countryCurrency/getExchange.js
+++ src/utils/countryCurrency/getExchange.js
@@ -1,7 +1,7 @@
 import countryExchange from './countryExchangeForMinimumDonationAmount.json';
 
-export const getMinimumAmountForCurrency = (curreny) => {
-  const exchange = countryExchange.find((element) => element.currencyCode === curreny);
+export const getMinimumAmountForCurrency = (currency) => {
+  const exchange = countryExchange.find((element) => element.currencyCode === currency);
   if (exchange)
     return (exchange.value * 2);
   else
