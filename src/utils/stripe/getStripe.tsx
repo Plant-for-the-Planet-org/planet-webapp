488d87b345bbcbd475ebe09d0ecd776c9f692ed6
Rishabh <rishabhshilpasingh@gmail.com>
2021-03-03 14:47:54 +0530

    passing locale for error

--- src/utils/stripe/getStripe.tsx
+++ src/utils/stripe/getStripe.tsx
@@ -3,9 +3,10 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 let stripePromise: Promise<Stripe | null>;
 
 const getStripe = (paymentSetup:any) => {
+  let lang = localStorage.getItem('language') || 'en';
   const key = paymentSetup?.gateways?.stripe?.authorization.stripePublishableKey ? paymentSetup?.gateways?.stripe?.authorization.stripePublishableKey : paymentSetup?.gateways?.stripe?.stripePublishableKey;
   const account = paymentSetup?.gateways?.stripe?.authorization.accountId;
-  stripePromise = loadStripe(key,{stripeAccount:account});
+  stripePromise = loadStripe(key,{stripeAccount:account,locale:lang});
   return stripePromise;
 };
 
