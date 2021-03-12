d49edd9717e6a530d9379b2d68418f0737997a41
Harsh Vitra <jainharsh44@gmail.com>
2021-03-12 14:35:15 +0530

    Tenant ulmpflanzt

---
+++ public/tenants/ulmpflanzt/config.ts
@@ -0,0 +1,60 @@
+const config = {
+  tenantName: 'ulmpflanzt',
+  tenantURL: 'wald.ulm-pflanzt-bäume.de',
+  languages: ['de'],
+  fallbackCurrency: 'EUR',
+  tenantGoal:124781,
+  font: {
+    primaryFontFamily: '"Raleway",Helvetica,Arial,sans-serif',
+    primaryFontURL: "https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap",
+    secondaryFontFamily: '"Open Sans",Helvetica,Arial,sans-serif',
+    secondaryFontURL: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap",
+  },
+  meta: {
+    title: 'Ulm-pflanzt ❤️ Baeume',
+    description:
+      '124.781 Bäume – für jede/n Ulmer Bürger/in einen. Das ist das Ziel von „Ulm pflanzt Bäume“ – der Klima-Initiative von Plant-for-the-Planet , dem unw (Ulmer Initiativkreis nachhaltige Wirtschaftsentwicklung e. V.), den Ulmer Unternehmen pervormance international, Trivis und der Volksbank Ulm sowie der Stadt Ulm und der lokalen agenda ulm 21.',
+    image: `${process.env.CDN_URL}/media/images/app/bg_layer.jpg`,
+    twitterHandle: '',
+    locale: 'de_DE',
+  },
+  home: {
+    descriptionTitle:"",
+    image: `/tenants/pampers/background.jpeg`,
+  },
+  header: {
+    isSecondaryTenant: true,
+    tenantLogoURL: `/tenants/ulmpflanzt/logo.svg`,
+    tenantLogoLink: 'https://xn--ulm-pflanzt-bume-7nb.de/',
+    items: {
+      home: {
+        title: 'home',
+        onclick: '/home',
+        visible: true,
+      },
+      donate: {
+        title: 'plant',
+        onclick: '/',
+        visible: true,
+      },
+      about: {
+        title: 'aboutUs',
+        onclick: 'https://a.plant-for-the-planet.org/',
+        visible: false,
+      },
+      leaderboard: {
+        title: 'leaderboard',
+        onclick: '/all',
+        visible: false,
+      },
+      me:{
+        title: 'me',
+        onclick: '/me',
+        visible: false,
+      }
+    }
+  },
+  footerLinks:["privacyAndTerms","imprint","contact","supportUs"]
+};
+
+export default config;
