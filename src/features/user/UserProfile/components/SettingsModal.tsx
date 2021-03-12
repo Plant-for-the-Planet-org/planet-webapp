841e37bec5b29d73e64dad8d38a9fe2500793bd7
Harsh Vitra <jainharsh44@gmail.com>
2021-03-12 14:06:28 +0530

    Delete account modal UI

--- src/features/user/UserProfile/components/SettingsModal.tsx
+++ src/features/user/UserProfile/components/SettingsModal.tsx
@@ -9,6 +9,9 @@ import EditProfileModal from '../components/EditProfileModal';
 import i18next from '../../../../../i18n';
 import { useAuth0 } from '@auth0/auth0-react';
 import { removeLocalUserInfo } from '../../../../utils/auth0/localStorageUtils';
+import MaterialTextField from '../../../common/InputTypes/MaterialTextField';
+import AnimatedButton from '../../../common/InputTypes/AnimatedButton';
+import BackButton from '../../../../../public/assets/images/icons/BackButton';
 
 const { useTranslation } = i18next;
 export default function SettingsModal({
@@ -24,14 +27,20 @@ export default function SettingsModal({
 }: any) {
   const router = useRouter();
   const { t, ready } = useTranslation(['me', 'common', 'editProfile']);
-  const {
-    logout,
-  } = useAuth0();
+  const { logout } = useAuth0();
+
+  const [deleteModalOpen, setdeleteModalOpen] = React.useState(false);
+  const handledeleteModalClose = () => {
+    setdeleteModalOpen(false);
+  };
+  const handledeleteModalOpen = () => {
+    setdeleteModalOpen(!deleteModalOpen);
+  };
 
   const logoutUser = () => {
     removeLocalUserInfo();
     logout({ returnTo: `${process.env.NEXTAUTH_URL}/` });
-  }
+  };
   return ready ? (
     <>
       <Modal
@@ -48,20 +57,42 @@ export default function SettingsModal({
       >
         <Fade in={settingsModalOpen}>
           <div className={styles.modal}>
-            {
-              userType == 'tpo' &&
-              <a href={`#projectsContainer`} onClick={handleSettingsModalClose} className={styles.settingsItem}> {t('me:settingManageProject')} </a>
-            }
-            <div className={styles.settingsItem} onClick={handleEditProfileModalOpen}> {t('editProfile:edit')} </div>
+            {userType == 'tpo' && (
+              <a
+                href={`#projectsContainer`}
+                onClick={handleSettingsModalClose}
+                className={styles.settingsItem}
+              >
+                {' '}
+                {t('me:settingManageProject')}{' '}
+              </a>
+            )}
+            <div
+              className={styles.settingsItem}
+              onClick={handleEditProfileModalOpen}
+            >
+              {' '}
+              {t('editProfile:edit')}{' '}
+            </div>
             {/*  <div className={styles.settingsItem}> Change Password </div>
             <div className={styles.settingsItem}> Change Email </div>
             <div className={styles.settingsItem}> Embed Widget </div> */}
-            <button id={'settingsLogOut'}
+            <button
+              id={'settingsLogOut'}
               className={styles.settingsItem}
-              onClick={logoutUser}>
+              onClick={logoutUser}
+            >
               <b>{t('me:logout')} </b>
             </button>
-            <button id={'SettingsItem'}
+            <button
+              id={'settingsDeleteAccount'}
+              className={styles.settingsItem}
+              onClick={handledeleteModalOpen}
+            >
+              {t('me:deleteAccount')}
+            </button>
+            <button
+              id={'SettingsItem'}
               className={styles.settingsItem}
               onClick={handleSettingsModalClose}
             >
@@ -79,6 +110,71 @@ export default function SettingsModal({
         changeForceReload={changeForceReload}
         forceReload={forceReload}
       />
+      <DeleteModal
+        deleteModalOpen={deleteModalOpen}
+        handledeleteModalClose={handledeleteModalClose}
+      />
     </>
   ) : null;
+}
+
+function DeleteModal({ deleteModalOpen, handledeleteModalClose }) {
+  return (
+    <Modal
+      className={styles.modalContainer}
+      open={deleteModalOpen}
+      onClose={handledeleteModalClose}
+      closeAfterTransition
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={deleteModalOpen}>
+        <div className={styles.deleteModal}>
+          <p className={styles.deleteModalTitle}>Delete your account</p>
+          <p className={styles.deleteModalContent}>
+            By clicking delete, I am requesting Plant-for-the-Planet to delete
+            all data associated with my Plant-for-the-Planet account in
+            accordance with GDPR Act. I understand that donation data may be
+            kept for up to eight years. Trees I have registered will not be
+            removed from Trillion Tree Counter, however, will be anonymized and
+            cannot be associated with a profile again. Any additional data will
+            be deleted/anonymised in accordance to the Privacy Policy.
+            <br/>
+            <br/>
+            Alternatively you can mark your profile private by visiting Edit Profile.
+            <br/>
+            <br/>
+            To continue with deletion, please type “Delete My Account”
+          </p>
+
+          <MaterialTextField 
+              placeholder={'Delete My Account'}
+              label={'Delete My Account'}
+              type="text"
+              onChange={(e) => console.log(e.target.value)}
+              variant="outlined"
+              style={{marginTop:'20px'}}
+              name="addTarget" />
+
+          <div className={styles.deleteButtonContainer}>
+          <div onClick={()=>handledeleteModalClose()} className={styles.goBackContainer}>
+            <BackButton style={{}} />
+            Go Back
+          </div>
+          <AnimatedButton className={styles.deleteButton}>
+            Delete
+          </AnimatedButton>
+          </div>
+
+          <p className={styles.deleteModalWarning}>
+            I also understand that account deletion of example@email.com is irreversible.   
+          </p>
+        </div>
+      </Fade>
+    </Modal>
+  );
 }
