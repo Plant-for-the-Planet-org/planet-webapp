ef29eae94f70038da1adad5bd721086863d0fa4e
Harsh Vitra <jainharsh44@gmail.com>
2021-03-18 21:11:52 +0530

    Topbar text

--- src/features/common/Layout/Topbar/index.tsx
+++ src/features/common/Layout/Topbar/index.tsx
@@ -1,17 +1,23 @@
 import React, { ReactElement } from 'react'
 import Close from '../../../../../public/assets/images/icons/headerIcons/close'
+import i18next from './../../../../../i18n'
+const { useTranslation } = i18next;
 
 interface Props {
     
 }
 
 function Topbar({}: Props): ReactElement {
-    const [showTopBar, setshowTopBar] = React.useState(true)
+    const [showTopBar, setshowTopBar] = React.useState(true);
+    const { t, ready } = useTranslation(['common']);
+
     return (
         <div className={'topbarContainer'} style={{display:showTopBar ? 'flex' : 'none'}}>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. */}
+                {t('common:tobBarText')}
+                <a href="https://waldrekord.plant-for-the-planet.org/" target="_blank">
+                {t('common:clickHere')}
+                </a>
             </p>
             <button onClick={()=>setshowTopBar(false)}>
                 <Close color={'#fff'}/>
