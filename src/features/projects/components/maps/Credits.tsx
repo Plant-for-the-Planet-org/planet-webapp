182cd94e3707f9a1266b2ce19a54987f14ce112c
Sarvesh Warge <sarveshwarge@gmail.com>
2021-03-19 09:15:36 +0530

    add map credits

--- src/features/projects/components/maps/Credits.tsx
+++ src/features/projects/components/maps/Credits.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function Credits({ setCurrencyCode }: Props): ReactElement {
     const { useTranslation } = i18next;
-    const { i18n, t } = useTranslation(['common']);
+    const { i18n, t } = useTranslation(['common', 'maps']);
     const [language, setLanguage] = React.useState(i18n.language);
     const [selectedCurrency, setSelectedCurrency] = React.useState('EUR');
     const [selectedCountry, setSelectedCountry] = React.useState('DE');
@@ -72,16 +72,15 @@ export default function Credits({ setCurrencyCode }: Props): ReactElement {
                             {t('common:mapInfo')}
                             <div
                                 className={styles.popoverContent}
-                                style={{ left: '-270px', top: '-140px' }}
+                                style={{ left: '-270px', top: '-240px' }}
                             >
-                                <a>
-                                    Esri Community Maps Contributors, Esri, HERE, Garmin,
-                                    METI/NASA, USGS
-                    <br />
-                    Imagery: Esri, Maxar, Earthstar Geographics, CNES/Airbus DS,
-                    USDA FSA, USGS, Aerogrid, IGN, IGP, and the GIS User
-                    Community
-                  </a>
+                                <b>{t('maps:baseLayer')}</b>
+                                <a>Esri Community Maps Contributors, Esri, HERE, Garmin, METI/NASA, USGS </a>
+                                <a>World Imagery: Esri, Maxar, Earthstar Geographics, CNES/Airbus DS, USDA FSA, USGS, Aerogrid, IGN, IGP, and the GIS User Community</a>
+                                <b>{t('maps:satelliteImagery')}</b>
+                                <a>Image courtesy of Planet Labs, Inc</a>
+                                <a>Copernicus Sentinel data 2017-2021</a>
+                                <a>Landsat-8 image courtesy of the U.S. Geological Survey</a>
                             </div>
                         </div>
                     </div>
