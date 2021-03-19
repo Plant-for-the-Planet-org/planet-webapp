2496018edd3b9bc8cdbf2fcb8f12a9e5fefd7e9e
Harsh Vitra <jainharsh44@gmail.com>
2021-03-19 15:41:40 +0530

    Fix eslint

--- src/utils/stripe/getStripe.tsx
+++ src/utils/stripe/getStripe.tsx
@@ -3,7 +3,7 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 let stripePromise: Promise<Stripe | null>;
 
 const getStripe = (paymentSetup:any) => {
-  let lang = localStorage.getItem('language') || 'en';
+  const lang = localStorage.getItem('language') || 'en';
   const key = paymentSetup?.gateways?.stripe?.authorization.stripePublishableKey ? paymentSetup?.gateways?.stripe?.authorization.stripePublishableKey : paymentSetup?.gateways?.stripe?.stripePublishableKey;
   const account = paymentSetup?.gateways?.stripe?.authorization.accountId;
   stripePromise = loadStripe(key,{stripeAccount:account,locale:lang});
