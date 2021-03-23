f5e56efeec7c7377834b0a16901c9495b85dfe5e
Norbert Schuler <norbert.schuler@epublica.de>
2021-03-23 09:28:48 +0100

    added some Sentry ignore rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,6 +36,30 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
       }),
     ],
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    ignoreErrors: [
+      /^No error$/,
+      /__show__deepen/,
+      /_avast_submit/,
+      /Access is denied/,
+      /anonymous function: captureException/,
+      /Blocked a frame with origin/,
+      /console is not defined/,
+      /cordova/,
+      /DataCloneError/,
+      /Error: AccessDeny/,
+      /event is not defined/,
+      /feedConf/,
+      /ibFindAllVideos/,
+      /myGloFrameList/,
+      /SecurityError/,
+      /MyIPhoneApp/,
+      /snapchat.com/,
+      /vid_mate_check is not defined/,
+      /win\.document\.body/,
+      /window\._sharedData\.entry_data/,
+      /ztePageScrollModule/
+    ],
+    denyUrls: [],
   });
 }
 
