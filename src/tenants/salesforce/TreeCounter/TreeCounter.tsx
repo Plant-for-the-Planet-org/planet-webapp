6d32ddd87317eeb8f88537078c23d3f7c4610163
deadlinc <nick@launchxd.com>
2021-04-07 14:57:37 -0400

    Hide counter until number comes back from API;

--- src/tenants/salesforce/TreeCounter/TreeCounter.tsx
+++ src/tenants/salesforce/TreeCounter/TreeCounter.tsx
@@ -4,7 +4,7 @@ import { getFormattedNumber } from '../../../utils/getFormattedNumber';
 
 export default function TpoProfile(props: any) {
   return (
-    <div className={treeCounterStyles.treeCounter}>
+    <div className={`${treeCounterStyles.treeCounter} ${props.planted > 0 ? treeCounterStyles.treeCounterReady : ''}`}>
       <div className={treeCounterStyles.treeCounterDataField}>
         <span aria-label={props.planted} role="heading">
           {String(getFormattedNumber('en', Number(props.planted))).padStart(11, "0").split("").map(function(char, index){
