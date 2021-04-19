f98cec90cd860527e0073febccc6d3dc1d080930
deadlinc <nick@launchxd.com>
2021-04-19 10:01:28 -0400

    Remove Jira;

--- src/tenants/salesforce/Home/index.tsx
+++ src/tenants/salesforce/Home/index.tsx
@@ -21,7 +21,6 @@ export default function About({ tenantScore, leaderboard }: Props) {
     <>
       <Head>
         <title>{`Home | ${config.meta.title}`}</title>
-        <script type="text/javascript" src="https://launchxd.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/sb53l8/b/24/a44af77267a987a660377e5c46e0fb64/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=d3c90764" />
       </Head>
       <main style={{backgroundColor: 'white', paddingBottom: '60px'}}>
         <Landing tenantScore={tenantScore} />
