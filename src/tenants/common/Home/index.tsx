6079da26141dea783be9e666fb06d3ee1dbb949e
Norbert Schuler <norbert.schuler@epublica.de>
2021-04-21 10:12:50 +0200

    Merge branch 'main' into tenant/andalusia

--- src/tenants/common/Home/index.tsx
+++ src/tenants/common/Home/index.tsx
@@ -3,7 +3,6 @@ import LandingSection from '../../../features/common/Layout/LandingSection';
 import LeaderBoard from '../LeaderBoard';
 import TreeCounter from '../../../features/common/TreeCounter/TreeCounter';
 import Footer from '../../../features/common/Layout/Footer';
-import React from 'react';
 import tenantConfig from '../../../../tenant.config';
 import i18next from '../../../../i18n';
 
@@ -13,46 +12,42 @@ const { useTranslation } = i18next;
 
 interface Props {
   leaderboard: any;
-  tenantScore: any;
+  tenantScore:any;
 }
 
 export default function About({ leaderboard, tenantScore }: Props) {
   const { t, i18n, ready } = useTranslation(['tenants']);
 
-  const descriptionRef = React.useRef(null);
-  React.useEffect(() => {
-    if (descriptionRef.current !== null) {
-      descriptionRef.current.innerHTML = t(`tenants:${config.tenantName}.description`);
-    }
-  }, [ready]);
-
-  return ready ? (
+  return (
     <main>
       <LandingSection
         imageSrc={config.meta.image}>
-        <div style={{ marginTop: '120px' }} />
+        <div style={{ marginTop:'120px' }} />
         {tenantScore
           && (
-            <TreeCounter
-              target={config.tenantGoal}
-              planted={tenantScore.total}
-            />
-          )}
+          <TreeCounter
+            target={config.tenantGoal}
+            planted={tenantScore.total}
+          />
+          ) }
 
         <p className={styles.publicUserDescription} style={{ fontWeight: 'bold', marginBottom: '0px' }}>
-          {t(`tenants:${config.tenantName}.title`)}
+           {t(`tenants:${config.tenantName}.title`)}
         </p>
 
         {config.home.descriptionTitle && (
-          <p className={styles.publicUserDescription} style={{ fontWeight: 'bold', marginBottom: '0px' }}>
-            {t(`tenants:${config.tenantName}.descriptionTitle`)}
-          </p>
+        <p className={styles.publicUserDescription} style={{ fontWeight: 'bold', marginBottom: '0px' }}>
+          {t(`tenants:${config.tenantName}.descriptionTitle`)}
+        </p>
         )}
-        <p ref={descriptionRef} className={styles.publicUserDescription} style={{ marginTop: '8px' }}></p>
-        <div style={{ marginBottom: '60px' }} />
+
+        <p className={styles.publicUserDescription} style={{ marginTop: '8px' }}>
+          {t(`tenants:${config.tenantName}.description`)}
+        </p>
+        <div style={{ marginBottom:'60px' }} />
       </LandingSection>
-      {leaderboard && (leaderboard.mostDonated.length > 0 || leaderboard.mostRecent.length > 0) && <LeaderBoard leaderboard={leaderboard} />}
+      {leaderboard && (leaderboard.mostDonated.length > 0 || leaderboard.mostRecent.length > 0 ) && <LeaderBoard leaderboard={leaderboard} />}
       <Footer />
     </main>
-  ) : null;
+  );
 }
