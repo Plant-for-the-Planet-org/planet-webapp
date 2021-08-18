1330ef3e70be04ac6c5658298ce10e70d95e33c7
Rishabh <rishabhshilpasingh@gmail.com>
2021-08-18 23:52:22 +0530

    darkmode localstorage

--- src/features/common/Layout/DarkModeSwitch.tsx/index.tsx
+++ src/features/common/Layout/DarkModeSwitch.tsx/index.tsx
@@ -5,26 +5,61 @@ import { ThemeContext } from '../../../../theme/themeContext';
 import styles from './DarkModeSwitch.module.scss'
 
 function DarkModeSwitch() {
-    const { theme, toggleTheme } = React.useContext(ThemeContext);
+  const { theme, toggleTheme } = React.useContext(ThemeContext);
+  const now = new Date();
+  const [check, setCheck] = React.useState(false);
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      let tempcheck = document.getElementById("chk")
+      setCheck(tempcheck.checked)
+      console.log(tempcheck.checked,"temp")
+    }
+  }, [])
   
-    return (
-      <button style={{ position: "relative" }}>
-        <input
-          onClick={() =>
-            toggleTheme(theme === "theme-light" ? "theme-dark" : "theme-light")
-          }
-          defaultChecked={theme === "theme-dark" ? true : false}
-          type="checkbox"
-          className={styles.darkmodeCheckbox}
-          id="chk"
-        />
-        <label className={styles.darkmodeLabel} htmlFor="chk">
-          <MoonIcon />
-          <SunIcon />
-          <div className={styles.darkmodeBall}></div>
-        </label>
-      </button>
-    );
-  }
+  React.useEffect(() => {
+    let themeSetter = localStorage.getItem("darkTheme");
+    if (themeSetter) {
+      themeSetter = JSON.parse(themeSetter)
+      if (now.getTime() > themeSetter.expiry) {
+        localStorage.removeItem('darkTheme')
+        themeSetter = null;
+      }
+      else {
+        toggleTheme(themeSetter.value ? "theme-dark" : "theme-light");
+      }
+    }
+  }, [])
 
-  export default DarkModeSwitch;
+  function storeTheme() {
+    const expiryTime = now.getTime() + 86400000; //24hrs time span
+    const data = {
+      expiry: expiryTime,
+      value: check,
+    }
+    localStorage.setItem('darkTheme', JSON.stringify(data));
+  }
+console.log(theme === "theme-dark","theme")
+  return (
+    <button style={{ position: "relative" }}>
+      <input
+        onClick={(e) => {
+          toggleTheme(theme === "theme-light" ? "theme-dark" : "theme-light");
+          storeTheme();
+          setCheck(e.target.checked)
+        }
+        }
+        value={theme === "theme-dark" ? 1 : 0}
+        type="checkbox"
+        className={styles.darkmodeCheckbox}
+        id="chk"
+      />
+      <label className={styles.darkmodeLabel} htmlFor="chk">
+        <MoonIcon />
+        <SunIcon />
+        <div className={styles.darkmodeBall}></div>
+      </label>
+    </button>
+  );
+}
+
+export default DarkModeSwitch;
