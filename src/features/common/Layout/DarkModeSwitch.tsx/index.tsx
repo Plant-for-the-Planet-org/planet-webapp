dc4e14d927d48eff6c6fb278a4a9b30fb6d0e29b
Norbert Schuler <norbert.schuler@epublica.de>
2021-08-20 12:24:46 +0200

    copied dark mode switch from planet-donations

--- src/features/common/Layout/DarkModeSwitch.tsx/index.tsx
+++ src/features/common/Layout/DarkModeSwitch.tsx/index.tsx
@@ -5,50 +5,15 @@ import { ThemeContext } from '../../../../theme/themeContext';
 import styles from './DarkModeSwitch.module.scss'
 
 function DarkModeSwitch() {
-  const { theme, toggleTheme } = React.useContext(ThemeContext);
-  const now = new Date();
-  const [check, setCheck] = React.useState(false);
-  React.useEffect(() => {
-    if (typeof window !== 'undefined') {
-      let tempcheck = document.getElementById("chk")
-      setCheck(tempcheck.checked)
-      console.log(tempcheck.checked,"temp")
-    }
-  }, [])
-  
-  React.useEffect(() => {
-    let themeSetter = localStorage.getItem("darkTheme");
-    if (themeSetter) {
-      themeSetter = JSON.parse(themeSetter)
-      if (now.getTime() > themeSetter.expiry) {
-        localStorage.removeItem('darkTheme')
-        themeSetter = null;
-      }
-      else {
-        toggleTheme(themeSetter.value ? "theme-dark" : "theme-light");
-      }
-    }
-  }, [])
+  const { theme, setTheme } = React.useContext(ThemeContext);
 
-  function storeTheme() {
-    const expiryTime = now.getTime() + 86400000; //24hrs time span
-    const data = {
-      expiry: expiryTime,
-      value: check,
-    }
-    localStorage.setItem('darkTheme', JSON.stringify(data));
-  }
-console.log(theme === "theme-dark","theme")
   return (
     <button style={{ position: "relative" }}>
       <input
-        onClick={(e) => {
-          toggleTheme(theme === "theme-light" ? "theme-dark" : "theme-light");
-          storeTheme();
-          setCheck(e.target.checked)
+        onClick={() =>
+          setTheme(theme === "theme-light" ? "theme-dark" : "theme-light")
         }
-        }
-        value={theme === "theme-dark" ? 1 : 0}
+        defaultChecked={theme === "theme-dark" ? true : false}
         type="checkbox"
         className={styles.darkmodeCheckbox}
         id="chk"
@@ -61,5 +26,6 @@ console.log(theme === "theme-dark","theme")
     </button>
   );
 }
+
 
 export default DarkModeSwitch;
--- src/theme/themeContext.tsx
+++ src/theme/themeContext.tsx
@@ -1,24 +1,44 @@
-// const initialValue = { theme: "theme-light" };
 import React from 'react';
 
 export const ThemeContext = React.createContext({
-  theme: 'string',
-  toggleTheme: (theme:any) => theme,
+  theme: 'theme-light',
+  setTheme: (theme:any) => theme,
 });
 
 export default function ThemeProvider({ children }:any) {
-  const [theme, setTheme] = React.useState('theme-light');
+  const [theme, setTheme] = React.useState('theme-light');  
 
-  const toggleTheme = () => {
-    setTheme(theme === 'theme-light' ? 'theme-dark' : 'theme-light');
-  };
-React.useEffect(() => {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    setTheme("theme-dark")
-}
-}, [])
+  React.useEffect(()=>{
+    if(typeof window !== 'undefined'){
+      if(localStorage.getItem('theme')){
+        if(localStorage.getItem('theme')==='theme-light'){
+          setTheme('theme-light');
+        }else{
+          setTheme('theme-dark')
+        }
+      }
+      else{
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          setTheme('theme-dark');
+          localStorage.setItem('theme','theme-dark')
+        }
+        else{
+          setTheme('theme-light');
+          localStorage.setItem('theme','theme-light')
+        }
+      }
+      
+    }
+  },[]);
+
+  React.useEffect(()=>{
+    if(typeof window !== 'undefined'){
+      localStorage.setItem('theme',theme)
+    }
+  },[theme])
+
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme,setTheme }}>
       {children}
     </ThemeContext.Provider>
   );
