85830e91d9bd55172f9f1e2e5d26eaeb17e32f6f
Norbert Schuler <norbert.schuler@epublica.de>
2021-08-25 10:02:20 +0200

    fixed eslint errors

--- src/features/projects/components/maps/Sites.tsx
+++ src/features/projects/components/maps/Sites.tsx
@@ -30,7 +30,7 @@ export default function Sites({}: Props): ReactElement {
     if (screen.orientation) {
       isPortrait = screen.orientation.angle === 0 || screen.orientation.angle === 180;
     } else if (window.orientation) {
-      isPortrait = window.orientation === 0 || window.orientation === 180;
+      isPortrait = window.orientation === 0 || window.orientation === 180;
     }  
     const isMobileTemp = window.innerWidth <= 767 && isPortrait;
     //console.log("Projects-isMobileTemp", isMobileTemp, window.innerHeight, window.innerWidth, window.orientation, screen.orientation);
