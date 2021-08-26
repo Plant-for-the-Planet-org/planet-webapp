4126b82d1c6aaeab8e81be76fa3df530e77c5d7c
Sarvesh Warge <sarveshwarge@gmail.com>
2021-08-26 09:22:33 +0530

    add a tooltip to the button

--- src/features/common/LandingVideo/PlayButton.tsx
+++ src/features/common/LandingVideo/PlayButton.tsx
@@ -1,14 +1,18 @@
 import React, { ReactElement } from 'react';
 import PlayIcon from '../../../../public/assets/images/icons/PlayIcon';
 import styles from './styles.module.scss';
+import i18next from '../../../../i18n';
+
+const { useTranslation } = i18next;
 
 interface Props {
     setshowVideo: Function;
 }
 
 export default function PlayButton({setshowVideo}: Props): ReactElement {
+    const { t } = useTranslation(['common']);
     return (
-        <div onClick={()=>setshowVideo(true)} className={styles.playButton}>
+        <div title={t('howDoesThisWork')} onClick={()=>setshowVideo(true)} className={styles.playButton}>
             <PlayIcon/>
         </div>
     )
