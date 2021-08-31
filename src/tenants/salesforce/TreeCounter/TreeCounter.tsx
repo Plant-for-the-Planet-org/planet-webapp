712e9057bd050b90f9c5521a24c392af97c95118
Louis Alemar <louis@launchxd.com>
2021-08-31 13:26:46 -0400

    remove 0 and add padding top to card title

--- src/tenants/salesforce/TreeCounter/TreeCounter.tsx
+++ src/tenants/salesforce/TreeCounter/TreeCounter.tsx
@@ -7,7 +7,7 @@ export default function TpoProfile(props: any) {
     <div className={`${treeCounterStyles.treeCounter} ${props.planted > 0 ? treeCounterStyles.treeCounterReady : ''}`}>
       <div className={treeCounterStyles.treeCounterDataField}>
         <span aria-label={props.planted} role="heading">
-          {String(getFormattedNumber('en', Number(props.planted))).padStart(11, "0").split("").map(function(char, index){
+          {String(getFormattedNumber('en', Number(props.planted))).padStart(11).split("").map(function(char, index){
             return <span aria-hidden="true" key={`planted-${index}`} className={char === ',' ? treeCounterStyles.treeCounterComma : treeCounterStyles.treeCounterNumber}>{char}</span>;
           })}
         </span>
