13923560021b43c9e69a0498a7293cd5ebe8a68d
Tejas Sonar <tejassonar227@gmail.com>
2021-10-12 12:06:48 +0530

    fixed calendar UI

--- src/features/user/Account/PauseModal.tsx
+++ src/features/user/Account/PauseModal.tsx
@@ -118,7 +118,6 @@ export const PauseModal = ({
                       setdate(value);
                     }}
                     disablePast={true}
-                    color={'#68B030'}
                   />
                 </MuiPickersUtilsProvider>
               </ThemeProvider>
