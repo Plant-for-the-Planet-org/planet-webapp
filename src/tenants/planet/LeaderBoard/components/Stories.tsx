e329601ed788759dec8809a12410cd7a9c7844ba
Sarvesh Warge <sarveshwarge@gmail.com>
2021-11-11 01:34:31 +0530

    enable principles and standards section

--- src/tenants/planet/LeaderBoard/components/Stories.tsx
+++ src/tenants/planet/LeaderBoard/components/Stories.tsx
@@ -8,30 +8,40 @@ interface Props {}
 
 export default function Stories({}: Props): ReactElement {
   const { t, ready } = useTranslation(['planet']);
-  return ready ? (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.storyCard}>
-          <img src={'/tenants/planet/images/leaderboard/restoreTrees.svg'} />
-          <div className={styles.storyContent}>
-            <h2>{t('planet:restoreTrees')}</h2>
-            <div>
-              <img src={'/tenants/planet/images/leaderboard/playIcon.svg'} />
-              <p>{t('planet:watchVideo')}</p>
-            </div>
+  return (
+    <>
+      {ready ? (
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <a className={styles.storyCard} href="https://a.plant-for-the-planet.org/principles">
+              <img
+                src={'/tenants/planet/images/leaderboard/restoreTrees.svg'}
+              />
+              <div className={styles.storyContent}>
+                <h2>{t('planet:restoreTrees')}</h2>
+                <div>
+                  <img
+                    src={'/tenants/planet/images/leaderboard/mapIcon.svg'}
+                  />
+                  <p>{t('planet:learnMore')}</p>
+                </div>
+              </div>
+            </a>
+            <a className={styles.storyCard} href="https://a.plant-for-the-planet.org/standards">
+              <img
+                src={'/tenants/planet/images/leaderboard/selectProjects.svg'}
+              />
+              <div className={styles.storyContent}>
+                <h2>{t('planet:selectProjects')}</h2>
+                <div>
+                  <img src={'/tenants/planet/images/leaderboard/mapIcon.svg'} />
+                  <p>{t('planet:learnMore')}</p>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
-        <div className={styles.storyCard}>
-          <img src={'/tenants/planet/images/leaderboard/selectProjects.svg'} />
-          <div className={styles.storyContent}>
-            <h2>{t('planet:selectProjects')}</h2>
-            <div>
-              <img src={'/tenants/planet/images/leaderboard/mapIcon.svg'} />
-              <p>{t('planet:learnMore')}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ) : null;
+      ) : null}
+    </>
+  );
 }
