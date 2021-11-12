48d3678b6311703de07d4179759be8692a398d42
Sarvesh Warge <sarveshwarge@gmail.com>
2021-11-12 15:31:30 +0530

    support link translation

--- src/tenants/planet/LeaderBoard/components/Stories.tsx
+++ src/tenants/planet/LeaderBoard/components/Stories.tsx
@@ -1,19 +1,20 @@
 import React, { ReactElement } from 'react';
 import styles from './Stories.module.scss';
 import i18next from '../../../../../i18n';
+import { lang_path } from '../../../../utils/constants/wpLanguages';
 
 const { useTranslation } = i18next;
 
 interface Props {}
 
 export default function Stories({}: Props): ReactElement {
-  const { t, ready } = useTranslation(['planet']);
+  const { i18n, t, ready } = useTranslation(['planet']);
   return (
     <>
       {ready ? (
         <div className={styles.wrapper}>
           <div className={styles.container}>
-            <a className={styles.storyCard} href="https://a.plant-for-the-planet.org/principles">
+            <a className={styles.storyCard} href={`https://a.plant-for-the-planet.org/${lang_path[i18n.language]}/principles`}>
               <img
                 src={'/tenants/planet/images/leaderboard/restoreTrees.svg'}
               />
@@ -27,7 +28,7 @@ export default function Stories({}: Props): ReactElement {
                 </div>
               </div>
             </a>
-            <a className={styles.storyCard} href="https://a.plant-for-the-planet.org/standards">
+            <a className={styles.storyCard} href={`https://a.plant-for-the-planet.org/${lang_path[i18n.language]}/standards`}>
               <img
                 src={'/tenants/planet/images/leaderboard/selectProjects.svg'}
               />
