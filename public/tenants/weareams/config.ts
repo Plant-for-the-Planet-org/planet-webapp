9b293b3da511a6426cff3e3ce27b99826729da25
Sarvesh Warge <sarveshwarge@gmail.com>
2022-02-01 11:52:23 +0530

    ams tenant changes

--- public/tenants/weareams/config.ts
+++ public/tenants/weareams/config.ts
@@ -6,12 +6,14 @@ const config = {
   tenantGoal: null,
   font: {
     primaryFontFamily: '"Raleway",Helvetica,Arial,sans-serif',
-    primaryFontURL: "https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700;800&display=swap",
+    primaryFontURL:
+      'https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700;800&display=swap',
     secondaryFontFamily: '"Open Sans",Helvetica,Arial,sans-serif',
-    secondaryFontURL: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap",
+    secondaryFontURL:
+      'https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap',
   },
   meta: {
-    title: 'Alexander Mann Solutions ❤️ Trees',
+    title: 'AMS ❤️ Trees',
     description:
       'From 2021 on AMS joins the TrillionTreeCampaign and together with Plant-for-the-Planet we start giving back to nature. We will use the opportunities we get to promote sustainability in our supply chain and through our relationships with clients and business partners. Join us and #StopTalkingStartPlanting!',
     image: `${process.env.CDN_URL}/media/images/app/bg_layer.jpg`,
@@ -23,8 +25,8 @@ const config = {
   },
   header: {
     isSecondaryTenant: true,
-    tenantLogoURL: `/tenants/weareams/logo.png`,
-    // mobileLogoURL: `/tenants/weareams/logo-mobile.svg`,
+    tenantLogoURL: `/tenants/weareams/logo-desktop.jpg`,
+    mobileLogoURL: `/tenants/weareams/logo.png`,
     tenantLogoLink: 'https://forest.weareams.com',
     items: {
       home: {
