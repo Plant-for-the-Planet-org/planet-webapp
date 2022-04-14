5a170b2b6a9601d520a717987f07a79e72448ff8
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-04-14 17:47:36 +0530

    chore: set up storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+	'plugin:storybook/recommended',
     // 'airbnb',
   ],
   parser: '@typescript-eslint/parser',
