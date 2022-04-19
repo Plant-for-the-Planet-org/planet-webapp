a261743c7b5710d5991fd6bca85f066e94d430b4
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-04-19 14:54:49 +0530

    feat: updates UI for BulkMethodSelector
    - includes StyledButton (MUI based component)
    - add i18n translations

--- src/features/user/BulkCodes/components/BulkMethodSelector.tsx
+++ src/features/user/BulkCodes/components/BulkMethodSelector.tsx
@@ -1,62 +1,63 @@
 import React, { ReactElement, useState } from 'react';
+import i18next from '../../../../../i18n';
+
+import StyledButton from './StyledButton';
 import SelectorOption, { SelectorOptionProps } from './SelectorOption';
 
 import styles from '../BulkCodes.module.scss';
 
-const BulkMethodSelector = (): ReactElement => {
+const { useTranslation } = i18next;
+
+const BulkMethodSelector = (): ReactElement | null => {
   const [method, setMethod] = useState<string | null>(null);
+  const { t, ready } = useTranslation(['common', 'bulkCodes']);
 
-  const selectorOptions: SelectorOptionProps[] = [
-    {
-      method: 'import',
-      title: 'Import File',
-      subtitle:
-        'Use this method if one of the following criterias match your usecase:',
-      details: [
-        `I want to provide the Recipient's name or Email for each code`,
-        `I want Plant-for-the-Planet to automatically email the recipients once it has been generated (optional)`,
-        `I want to issue codes with different units of trees`,
-      ],
-    },
-    {
-      method: 'generic',
-      title: 'Create Generic Codes',
-      subtitle:
-        'Use this method if the following criterias match your usecase:',
-      details: [
-        'All codes will have the same value',
-        'I want to generate a number of code for arbitrary recipients',
-        'Names and Emails cannot be associated with the code',
-      ],
-    },
-  ];
+  if (ready) {
+    const selectorOptions: SelectorOptionProps[] = [
+      {
+        method: 'import',
+        title: t('bulkCodes:importMethodText.title'),
+        subtitle: t('bulkCodes:importMethodText.subtitle'),
+        details: t('bulkCodes:importMethodText.details', {
+          returnObjects: true,
+        }),
+      },
+      {
+        method: 'generic',
+        title: t('bulkCodes:genericMethodText.title'),
+        subtitle: t('bulkCodes:genericMethodText.subtitle'),
+        details: t('bulkCodes:genericMethodText.details', {
+          returnObjects: true,
+        }),
+      },
+    ];
 
-  const renderSelectorOptions = () => {
-    return selectorOptions.map((option, index) => (
-      <SelectorOption
-        method={option.method}
-        title={option.title}
-        subtitle={option.subtitle}
-        details={option.details}
-        handleMethodChange={handleMethodChange}
-        isSelected={option.method === method}
-        key={index}
-      />
-    ));
-  };
+    const renderSelectorOptions = () => {
+      return selectorOptions.map((option, index) => (
+        <SelectorOption
+          method={option.method}
+          title={option.title}
+          subtitle={option.subtitle}
+          details={option.details}
+          handleMethodChange={handleMethodChange}
+          isSelected={option.method === method}
+          key={index}
+        />
+      ));
+    };
 
-  const handleMethodChange = (method: string) => {
-    setMethod(method);
-  };
+    const handleMethodChange = (method: string) => {
+      setMethod(method);
+    };
 
-  return (
-    <div>
-      <form>
+    return (
+      <form className={styles.form}>
         <div className={styles.inputContainer}>{renderSelectorOptions()}</div>
-        <button onClick={(e) => e.preventDefault()}>Continue</button>
+        <StyledButton variant="contained">{t('common:continue')}</StyledButton>
       </form>
-    </div>
-  );
+    );
+  }
+  return null;
 };
 
 export default BulkMethodSelector;
