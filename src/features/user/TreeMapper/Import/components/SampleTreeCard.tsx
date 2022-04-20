921c11c3ce4f8faf2b40a3a458d2f0b5d754ccf5
Shreyas Chorge <shreyaschorge81@gmail.com>
2022-04-20 10:47:26 +0530

    Upgrade components in SampleTreeCard to v5

--- src/features/user/TreeMapper/Import/components/SampleTreeCard.tsx
+++ src/features/user/TreeMapper/Import/components/SampleTreeCard.tsx
@@ -9,7 +9,7 @@ import DateFnsUtils from '@date-io/date-fns';
 import { ThemeProvider } from '@material-ui/styles';
 import materialTheme from '../../../../../theme/themeStyles';
 import { localeMapForDate } from '../../../../../utils/language/getLanguageName';
-import { InputAdornment, MenuItem } from '@material-ui/core';
+import { InputAdornment, MenuItem } from '@mui/material';
 
 const { useTranslation } = i18next;
 
@@ -48,10 +48,11 @@ export default function SampleTreeCard({
       <div className={styles.sampleTreeName}>
         <div>
           {t('sampleTree', { number: index + 1 })}
-          {`${sampleTrees[index]?.treeTag
-            ? ` • ${t('tag')} ${sampleTrees[index]?.treeTag}`
-            : ''
-            }`}
+          {`${
+            sampleTrees[index]?.treeTag
+              ? ` • ${t('tag')} ${sampleTrees[index]?.treeTag}`
+              : ''
+          }`}
         </div>
         <div
           onClick={() => remove(index)}
@@ -63,14 +64,16 @@ export default function SampleTreeCard({
         </div>
       </div>
       <div className={styles.sampleTreeSummary}>
-        {`${sampleTrees[index]?.height
-          ? ` • ${t('height')} ${sampleTrees[index]?.height}`
-          : ''
-          }`}{' '}
-        {`${sampleTrees[index]?.diameter
-          ? ` • ${t('diameter')} ${sampleTrees[index]?.diameter}`
-          : ''
-          }`}
+        {`${
+          sampleTrees[index]?.height
+            ? ` • ${t('height')} ${sampleTrees[index]?.height}`
+            : ''
+        }`}{' '}
+        {`${
+          sampleTrees[index]?.diameter
+            ? ` • ${t('diameter')} ${sampleTrees[index]?.diameter}`
+            : ''
+        }`}
       </div>
       <div className={styles.sampleTreeDetails}>
         <div className={styles.formField}>
@@ -113,26 +116,25 @@ export default function SampleTreeCard({
               defaultValue={item.treeTag}
             />
           </div>
-
         </div>
         <div className={styles.formField}>
           <div className={styles.formFieldHalf}>
             <MaterialTextField
-              inputRef={register(
-                {
-                  pattern: {
-                    value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
-                    message: t('invalidHeight')
-                  }
-                }
-              )}
+              inputRef={register({
+                pattern: {
+                  value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
+                  message: t('invalidHeight'),
+                },
+              })}
               type="text"
               label={t('height')}
               variant="outlined"
               name={`sampleTrees[${index}].height`}
               defaultValue={item.height}
               InputProps={{
-                endAdornment: <InputAdornment position="end">{t('m')}</InputAdornment>,
+                endAdornment: (
+                  <InputAdornment position="end">{t('m')}</InputAdornment>
+                ),
               }}
             />
             {errors?.sampleTrees?.[index]?.height && (
@@ -143,21 +145,21 @@ export default function SampleTreeCard({
           </div>
           <div className={styles.formFieldHalf}>
             <MaterialTextField
-              inputRef={register(
-                {
-                  pattern: {
-                    value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
-                    message: t('invalidDiameter')
-                  }
-                }
-              )}
+              inputRef={register({
+                pattern: {
+                  value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
+                  message: t('invalidDiameter'),
+                },
+              })}
               type="text"
               label={t('diameter')}
               variant="outlined"
               name={`sampleTrees[${index}].diameter`}
               defaultValue={item.diameter}
               InputProps={{
-                endAdornment: <InputAdornment position="end">{t('cm')}</InputAdornment>,
+                endAdornment: (
+                  <InputAdornment position="end">{t('cm')}</InputAdornment>
+                ),
               }}
             />
             {errors?.sampleTrees?.[index]?.diameter && (
@@ -171,14 +173,12 @@ export default function SampleTreeCard({
         <div className={styles.formField}>
           <div className={styles.formFieldHalf}>
             <MaterialTextField
-              inputRef={register(
-                {
-                  pattern: {
-                    value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
-                    message: t('invalidLatitude')
-                  }
-                }
-              )}
+              inputRef={register({
+                pattern: {
+                  value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
+                  message: t('invalidLatitude'),
+                },
+              })}
               type="text"
               label={t('latitude')}
               variant="outlined"
@@ -193,14 +193,12 @@ export default function SampleTreeCard({
           </div>
           <div className={styles.formFieldHalf}>
             <MaterialTextField
-              inputRef={register(
-                {
-                  pattern: {
-                    value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
-                    message: t('invalidLongitude')
-                  }
-                }
-              )}
+              inputRef={register({
+                pattern: {
+                  value: /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/,
+                  message: t('invalidLongitude'),
+                },
+              })}
               type="text"
               label={t('longitude')}
               variant="outlined"
@@ -245,30 +243,37 @@ export default function SampleTreeCard({
                 select
                 inputRef={register}
               >
-                {plantLocation.plantedSpecies.map((species: Treemapper.PlantedSpecies, index: number) => {
-                  if (plantLocation.plantedSpecies.length === 1) {
-                    return (
-                      <MenuItem
-                        key={index} value={species.otherSpecies} selected={true}>
-                        {species.otherSpecies}
-                      </MenuItem>
-                    )
-                  } else if (species.otherSpecies === item.otherSpecies) {
-                    return (
-                      <MenuItem
-                        key={index} value={species.otherSpecies} selected={true}>
-                        {species.otherSpecies}
-                      </MenuItem>
-                    )
-                  } else {
-                    return (
-                      <MenuItem
-                        key={index} value={species.otherSpecies}>
-                        {species.otherSpecies}
-                      </MenuItem>
-                    )
+                {plantLocation.plantedSpecies.map(
+                  (species: Treemapper.PlantedSpecies, index: number) => {
+                    if (plantLocation.plantedSpecies.length === 1) {
+                      return (
+                        <MenuItem
+                          key={index}
+                          value={species.otherSpecies}
+                          selected={true}
+                        >
+                          {species.otherSpecies}
+                        </MenuItem>
+                      );
+                    } else if (species.otherSpecies === item.otherSpecies) {
+                      return (
+                        <MenuItem
+                          key={index}
+                          value={species.otherSpecies}
+                          selected={true}
+                        >
+                          {species.otherSpecies}
+                        </MenuItem>
+                      );
+                    } else {
+                      return (
+                        <MenuItem key={index} value={species.otherSpecies}>
+                          {species.otherSpecies}
+                        </MenuItem>
+                      );
+                    }
                   }
-                })}
+                )}
               </MaterialTextField>
             }
             name={`sampleTrees[${index}].otherSpecies`}
