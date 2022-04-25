9e9676706a5a43e1c04c5ece0ed55fb577eb83d3
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-04-25 12:13:10 +0530

    feat: add step navigation for Bulk Codes

--- src/features/user/BulkCodes/index.tsx
+++ src/features/user/BulkCodes/index.tsx
@@ -1,16 +1,35 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import i18next from '../../../../i18n';
 
 import DashboardView from '../../common/Layout/DashboardView';
 import TabbedView from './TabbedView';
 import BulkMethodSelector from './components/BulkMethodSelector';
+import ProjectSelector from './components/ProjectSelector';
 
-interface Props {}
+interface Props {
+  // step: 0 | 1 | 2;
+  // setStep: (step: 0 | 1 | 2) => void;
+}
 
 const { useTranslation } = i18next;
 
 export default function BulkCodes({}: Props): ReactElement | null {
   const { t, ready } = useTranslation(['bulkCodes']);
+  const [step, setStep] = useState<0 | 1 | 2>(0);
+
+  const renderStep = () => {
+    switch (step) {
+      case 0:
+        return <BulkMethodSelector setStep={setStep} />;
+      case 1:
+        return <ProjectSelector setStep={setStep} />;
+      case 2:
+        return <div>Step 3 will come here</div>;
+      default:
+        return <BulkMethodSelector setStep={setStep} />;
+    }
+  };
+
   return ready ? (
     <DashboardView
       title={t('bulkCodes:bulkCodes')}
@@ -22,9 +41,7 @@ export default function BulkCodes({}: Props): ReactElement | null {
         </p>
       }
     >
-      <TabbedView>
-        <BulkMethodSelector />
-      </TabbedView>
+      <TabbedView>{renderStep()}</TabbedView>
     </DashboardView>
   ) : null;
 }
