030855beeeee0400853fba470d5ea5664b59ef46
Shreyas Chorge <shreyaschorge81@gmail.com>
2022-04-29 11:00:58 +0530

    Fix Redirect Route Path

--- src/features/user/BulkCodes/components/CreationMethodForm.tsx
+++ src/features/user/BulkCodes/components/CreationMethodForm.tsx
@@ -65,13 +65,15 @@ CreationMethodFormProps): ReactElement | null => {
 
     const handleFormSubmit = () => {
       // Clear form data stored in context if method is changed
-      if (method !== bulkMethod) {
-        setProject(null);
-        setBulkGiftData(null);
-        setTotalUnits(null);
+      if (bulkMethod) {
+        if (method !== bulkMethod) {
+          setProject(null);
+          setBulkGiftData(null);
+          setTotalUnits(null);
+        }
       }
       setBulkMethod(method);
-      router.push(`/profile/bulk-codes/${bulkMethod}`);
+      router.push(`/profile/bulk-codes/${method}`);
     };
 
     return (
