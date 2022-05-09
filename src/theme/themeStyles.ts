899022f498edbef1e2c102fc2fae950fb381618a
Shreyas Chorge <shreyaschorge81@gmail.com>
2022-05-09 14:59:26 +0530

    Restore fontSize, lineHeight and letterSpacing

--- src/theme/themeStyles.ts
+++ src/theme/themeStyles.ts
@@ -8,6 +8,17 @@ const materialTheme = createTheme({
       main: theme.primaryColor,
     },
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          fontSize: '0.875rem',
+          lineHeight: 1.43,
+          letterSpacing: '0.01071em',
+        },
+      },
+    },
+  },
 });
 
 export default materialTheme;
