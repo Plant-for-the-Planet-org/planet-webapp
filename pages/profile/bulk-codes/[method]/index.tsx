bfd6b015a286701920ffa75bb441220a9e136167
Shreyas Chorge <shreyaschorge81@gmail.com>
2022-05-24 10:34:50 +0530

    Fix warning

--- pages/profile/bulk-codes/[method]/index.tsx
+++ pages/profile/bulk-codes/[method]/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import UserLayout from '../../../../src/features/common/Layout/UserLayout/UserLayout';
 import BulkCodes from '../../../../src/features/user/BulkCodes';
 import Head from 'next/head';
@@ -16,19 +16,21 @@ export default function BulkCodeSelectProjectPage({}: Props): ReactElement {
   const { isReady, query } = useRouter();
   const { bulkMethod, setBulkMethod } = useBulkCode();
 
-  if (!bulkMethod) {
-    if (isReady) {
-      const _bulkMethod = query.method;
-      if (
-        _bulkMethod === BulkCodeMethods.GENERIC ||
-        _bulkMethod === BulkCodeMethods.IMPORT
-      ) {
-        setBulkMethod(_bulkMethod);
-      } else {
-        router.push(`/profile/bulk-codes`);
+  useEffect(() => {
+    if (!bulkMethod) {
+      if (isReady) {
+        const _bulkMethod = query.method;
+        if (
+          _bulkMethod === BulkCodeMethods.GENERIC ||
+          _bulkMethod === BulkCodeMethods.IMPORT
+        ) {
+          setBulkMethod(_bulkMethod);
+        } else {
+          router.push(`/profile/bulk-codes`);
+        }
       }
     }
-  }
+  }, []);
 
   return (
     <UserLayout>
