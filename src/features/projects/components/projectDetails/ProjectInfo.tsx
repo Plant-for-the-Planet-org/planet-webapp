5659fbe4ec872b902e2f9819c28478e12939c21e
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2022-06-08 14:16:56 +0530

    format since word if ownerName is missing

--- src/features/projects/components/projectDetails/ProjectInfo.tsx
+++ src/features/projects/components/projectDetails/ProjectInfo.tsx
@@ -279,12 +279,22 @@ function ProjectInfo({ project }: Props): ReactElement {
                 })}
               </div>
             )}
-
-            <div className={styles.infoText}>
-              {project?.metadata?.siteOwnerName}{' '}
-              {project.metadata.acquisitionYear ? 'since ' : <></>}
-              {project?.metadata?.acquisitionYear}
-            </div>
+            {project?.metadata?.siteOwnerName ||
+            project?.metadata?.acquisitionYear ? (
+              <div className={styles.infoText}>
+                {project?.metadata?.siteOwnerName}{' '}
+                {project?.metadata?.siteOwnerName &&
+                project?.metadata?.acquisitionYear ? (
+                  'since '
+                ) : (
+                  <></>
+                )}
+                {project?.metadata?.acquisitionYear ? 'Since ' : <></>}
+                {project?.metadata?.acquisitionYear}
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         )}
       </div>
