bde1cf7c6bec1e993020ddb5dd369f1491cb12b5
Sagar Aryal <sagararyal@users.noreply.github.com>
2022-06-15 13:30:43 +0200

    remove header

--- public/tenants/planet/configBeta.ts
+++ public/tenants/planet/configBeta.ts
@@ -1,24 +1,24 @@
 const config = {
   tenantName: 'planetbeta',
   tenantURL: 'beta.plant-for-the-planet.org',
-  tenantLogoURL: 'https://www.plant-for-the-planet.org',
-  languages: ['en', 'de', 'es', 'fr', 'it', 'pt-BR'],
-  fallbackCurrency: 'EUR',
-  tenantGoal:null,
-  showUNEPLogo:true,
-  showUNDecadeLogo:true,
-  showRedeemHint:true,
-  enableGuestSepa:false,
+  tenantGoal: null,
+  showUNEPLogo: true,
+  showUNDecadeLogo: true,
+  showRedeemHint: false,
+  enableGuestSepa: false,
+  darkModeEnabled: false,
+  // font family and it's property particular to tenant
   font: {
     primaryFontFamily: '"Raleway",Helvetica,Arial,sans-serif',
     primaryFontURL: "https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700;800&display=swap",
     secondaryFontFamily: '"Open Sans",Helvetica,Arial,sans-serif',
     secondaryFontURL: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap",
   },
+  languages: ['en', 'de', 'es', 'fr', 'it', 'pt-BR'],
   header: {
     isSecondaryTenant: false,
     tenantLogoURL: `${process.env.CDN_URL}/logo/svg/planet.svg`,
-    tenantLogoLink: 'https://www.plant-for-the-planet.org',
+    tenantLogoLink: '/',
     items: {
       home: {
         title: 'home',
@@ -40,22 +40,29 @@ const config = {
         onclick: '/all',
         visible: true,
       },
-      me:{
-        title: 'me',
+      me: {
+        title: 'signIn',
+        loggedInTitle: 'me',
         onclick: '/me',
         visible: true,
-      }
+      },
+      shop: {
+        title: 'shop',
+        onclick: 'https://thegoodshop.org',
+        visible: true,
+      },
     }
   },
   meta: {
     title: 'Plant trees around the world - Plant-for-the-Planet',
+    appTitle: 'Plant for the Planet',
     description:
-      "No matter where you are, it's never been easier to plant trees and become part of the fight against climate crisis.",
+      "We are children and youth on a mission: bring back a trillion trees! No matter where you are, it's never been easier to plant trees and become part of the fight against climate crisis.",
     image: `${process.env.CDN_URL}/media/images/app/bg_layer.jpg`,
     twitterHandle: '@trilliontrees',
     locale: 'en_US',
   },
-  footerLinks:["shop","privacy","terms","imprint","contact","downloads","annualReports","team","jobs","supportUs","blogs", "faqs"]
+  footerLinks:["shop","privacy","terms","imprint","contact","downloads","annualReports","team","jobs","supportUs","blogs", "faqs"],
+  manifest: '/tenants/planet/manifest.json',
 };
-
 export default config;
