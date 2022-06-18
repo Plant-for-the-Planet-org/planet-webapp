3a27bc85a30dd3eef41f9c6fc6493b620f954007
Norbert Schuler <norbert.schuler@epublica.de>
2022-06-18 11:51:41 +0200

    disabled apple-mobile-web-app-capable

--- src/features/common/Layout/Header/index.tsx
+++ src/features/common/Layout/Header/index.tsx
@@ -29,17 +29,8 @@ export default function Header() {
           return <meta key="og:locale:alternate" property="og:locale:alternate" content={locale} />;
         }
       })}
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="application-name" content={config.meta.title} />
       <meta name="apple-mobile-web-app-title" content={config.meta.appTitle || config.meta.title} />
-    {/* <!-- New in iOS6  alt, --> */}
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-touch-fullscreen" content="yes" />
-      <meta
-        name="apple-mobile-web-app-status-bar-style"
-        content="black-translucent"
-      />
       <meta name="format-detection" content="telephone=no" />
       <link
         rel="icon"
