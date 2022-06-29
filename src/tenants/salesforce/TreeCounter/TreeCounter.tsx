53b5e55ba759f557694bfa4887fe7a7e5db03393
prachigarg19 <prachigarg1923@gmail.com>
2022-06-29 12:03:13 +0530

    converted decimal to integer in salesforce tenant page

--- src/tenants/salesforce/TreeCounter/TreeCounter.tsx
+++ src/tenants/salesforce/TreeCounter/TreeCounter.tsx
@@ -3,11 +3,12 @@ import treeCounterStyles from './TreeCounter.module.scss';
 import { getFormattedNumber } from '../../../utils/getFormattedNumber';
 
 export default function TpoProfile(props: any) {
+  console.log(parseInt('5'))
   return (
     <div className={`${treeCounterStyles.treeCounter} ${props.planted > 0 ? treeCounterStyles.treeCounterReady : ''}`}>
       <div className={treeCounterStyles.treeCounterDataField}>
         <span aria-label={props.planted} role="heading">
-          {String(getFormattedNumber('en', Number(props.planted))).padStart(11).split("").map(function(char, index){
+          {String(getFormattedNumber('en', parseInt(props.planted))).padStart(11).split("").map(function(char, index){
             return <span aria-hidden="true" key={`planted-${index}`} className={char === ',' ? treeCounterStyles.treeCounterComma : treeCounterStyles.treeCounterNumber}>{char}</span>;
           })}
         </span>
