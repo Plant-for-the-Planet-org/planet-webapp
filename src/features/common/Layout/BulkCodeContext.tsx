5d6fe7878df338e34e9e79038d7564c39e909af4
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-07-07 17:28:31 +0530

    refactor: refactor interface BulkGiftImportData

--- src/features/common/Layout/BulkCodeContext.tsx
+++ src/features/common/Layout/BulkCodeContext.tsx
@@ -44,17 +44,19 @@ interface BulkGiftGenericData {
   value: number;
 }
 
-interface BulkGiftImportData {
+export interface Recipient {
+  units: number;
+  recipientName: string;
+  recipientEmail: string;
+  message: string;
+  notifyRecipient: boolean;
+  // occasion: string;
+}
+
+export interface BulkGiftImportData {
   comment?: string;
   occasion: string;
-  recipients: {
-    value: number;
-    recipientName: string;
-    recipientEmail: string;
-    message: string;
-    notifyRecipient: boolean;
-    // occasion: string;
-  }[];
+  recipients: Recipient[];
 }
 
 type BulkGiftData = BulkGiftGenericData | BulkGiftImportData;
