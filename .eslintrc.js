9e86e0c799026280554897ed303a39c1afad7e10
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-07-18 15:14:53 +0530

    chore: remove storybook eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-	'plugin:storybook/recommended',
     // 'airbnb',
   ],
   parser: '@typescript-eslint/parser',
