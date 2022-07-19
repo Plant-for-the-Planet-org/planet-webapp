07faa0309890601b0664868e64b95c6f7efa801c
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-07-19 12:24:18 +0530

    feat: show spinner while downloading codes

--- src/features/user/Account/components/DownloadCodes.tsx
+++ src/features/user/Account/components/DownloadCodes.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import i18next from '../../../../../i18n';
 import { getRequest } from '../../../../utils/apiRequests/api';
 import { ErrorHandlingContext } from '../../../common/Layout/ErrorHandlingContext';
@@ -14,12 +14,14 @@ interface DownloadCodesProps {
 
 const DownloadCodes = ({ codesUrl }: DownloadCodesProps): ReactElement => {
   const [t] = useTranslation('me');
+  const [isDownloading, setIsDownloading] = useState(false);
 
   const { handleError } = React.useContext(ErrorHandlingContext);
 
   function downloadCSV(data: [], filename: string) {
     const csv = unparse(data);
     const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    // Create a temporary link element to download the file (blob)
     const url = window.URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
@@ -31,6 +33,7 @@ const DownloadCodes = ({ codesUrl }: DownloadCodesProps): ReactElement => {
   }
 
   async function handleClick() {
+    setIsDownloading(true);
     try {
       const response = await getRequest<{
         type: string;
@@ -40,14 +43,18 @@ const DownloadCodes = ({ codesUrl }: DownloadCodesProps): ReactElement => {
       if (response) {
         if (response.items.length) {
           downloadCSV(response.items, 'codes.csv');
+          setIsDownloading(false);
         } else {
           handleError({ message: t('downloadCodesGeneralError') });
+          setIsDownloading(false);
         }
       } else {
         handleError({ message: t('downloadCodesNetworkError') });
+        setIsDownloading(false);
       }
     } catch (err) {
       handleError({ message: t('downloadCodesGeneralError') });
+      setIsDownloading(false);
     }
   }
 
@@ -58,8 +65,14 @@ const DownloadCodes = ({ codesUrl }: DownloadCodesProps): ReactElement => {
         className={styles.downloadCodesButton}
         onClick={handleClick}
         title={t('downloadCodes')}
+        disabled={isDownloading}
       >
         {t('downloadCodes')}
+        {isDownloading && (
+          <span
+            className={`${styles.spinner} ${styles['spinner--primary']}`}
+          ></span>
+        )}
       </button>
     </div>
   );
