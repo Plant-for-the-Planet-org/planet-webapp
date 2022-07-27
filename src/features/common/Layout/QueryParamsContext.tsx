207e0d6fd6d69f9bd1dfe7b83d00b538960d8d86
prachigarg19 <prachigarg1923@gmail.com>
2022-07-27 15:14:44 +0530

    Resolved typescript errors in QueryParamsContext

--- src/features/common/Layout/QueryParamsContext.tsx
+++ src/features/common/Layout/QueryParamsContext.tsx
@@ -1,32 +1,42 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, {
+  createContext,
+  FC,
+  ReactElement,
+  useEffect,
+  useState,
+} from 'react';
 import { useRouter } from 'next/router';
-import { string } from 'prop-types';
-export const ParamsContext = createContext({
-    embed: string||undefined,
-    singleProject : string||undefined
+
+type QueryParamType = string | undefined | string[];
+export interface ParamsContextType {
+  embed: QueryParamType;
+  singleProject: QueryParamType;
+}
+export const ParamsContext = createContext<ParamsContextType>({
+    embed: undefined,
+    singleProject: undefined
 });
 
+const QueryParamsProvider: FC = ({ children }) => {
+  const [embed, setEmbed] = useState<QueryParamType>(undefined);
+  const [singleProject, setSingleProject] = useState<QueryParamType>(undefined);
+  const router = useRouter();
 
+  useEffect(() => {
+    setEmbed(router.query.embed);
+    setSingleProject(router.query.singleproject);
+  }, [router]);
 
-const QueryParamsProvider =({children}: any) => {
-        
-    const [embed, setEmbed] = React.useState < string| undefined >(undefined);
-    const [singleProject, setSingleProject] = React.useState <string |undefined |string[] >(undefined);
-    const router = useRouter();
-
-    useEffect(()=>{
-        setEmbed(router.query.embed);
-        setSingleProject(router.query.singleproject);
-    },[router]);
-
-    return(
-    <ParamsContext.Provider value={{
+  return (
+    <ParamsContext.Provider
+      value={{
         embed,
-        singleProject
-    }}>
-        {children}
+        singleProject,
+      }}
+    >
+      {children}
     </ParamsContext.Provider>
-    );
-}
+  );
+};
 
 export default QueryParamsProvider;
