ac69ad5fc485c215b0ee3f1b9e451bd436bed93f
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-08-05 11:31:24 +0530

    feat: add locale query param
    - also refactor QueryParamContext to set state separately in useEffects
    - set language if provided in query parameter

--- src/features/common/Layout/QueryParamsContext.tsx
+++ src/features/common/Layout/QueryParamsContext.tsx
@@ -1,29 +1,59 @@
 import React, { createContext, FC, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import i18next from '../../../../i18n';
 
-type QueryParamType = string | undefined | string[];
+const { useTranslation } = i18next;
+
+type QueryParamType = string | undefined | string[] | null;
 export interface ParamsContextType {
   embed: QueryParamType;
   singleProject: QueryParamType;
   callbackUrl: QueryParamType;
+  language: QueryParamType;
 }
 export const ParamsContext = createContext<ParamsContextType>({
   embed: undefined,
   singleProject: undefined,
   callbackUrl: undefined,
+  language: undefined,
 });
 
 const QueryParamsProvider: FC = ({ children }) => {
+  const { i18n } = useTranslation();
+
   const [embed, setEmbed] = useState<QueryParamType>(undefined);
   const [singleProject, setSingleProject] = useState<QueryParamType>(undefined);
   const [callbackUrl, setCallbackUrl] = useState<QueryParamType>(undefined);
+  const [language, setLanguage] = useState<QueryParamType>(undefined);
   const router = useRouter();
+  const { query } = router;
 
   useEffect(() => {
     setEmbed(router.query.embed);
-    setSingleProject(router.query.singleproject);
-    setCallbackUrl(router.query.callback);
-  }, [router]);
+  }, [query.embed]);
+
+  useEffect(() => {
+    setSingleProject(query.singleProject);
+  }, [query.singleProject]);
+
+  useEffect(() => {
+    setCallbackUrl(query.callback);
+  }, [query.callback]);
+
+  useEffect(() => {
+    if (query.locale) {
+      setLanguage(query.locale);
+    } else {
+      setLanguage(localStorage.getItem('language'));
+    }
+  }, [query.locale]);
+
+  useEffect(() => {
+    if (i18n && i18n.isInitialized) {
+      i18n.changeLanguage(language as string);
+      localStorage.setItem('language', language as string);
+    }
+  }, [language, i18n.isInitialized]);
 
   return (
     <ParamsContext.Provider
@@ -31,6 +61,7 @@ const QueryParamsProvider: FC = ({ children }) => {
         embed,
         singleProject,
         callbackUrl,
+        language,
       }}
     >
       {children}
