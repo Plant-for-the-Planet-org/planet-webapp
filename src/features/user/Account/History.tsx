ea949bafde1bbcebd570edbdbc57aa97cd6cd7e8
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-08-11 15:12:05 +0530

    fix: open top item (history) only on large screens
    1. if width < 768px, top item is not opened by default
    2. update styles to show modal view of records only if width <768px
    - Point 2 is done for Payment History and Recurring Donations

--- src/features/user/Account/History.tsx
+++ src/features/user/Account/History.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, useContext, useEffect } from 'react';
 import i18next from '../../../../i18n';
 import BackButton from '../../../../public/assets/images/icons/BackButton';
 import TransactionListLoader from '../../../../public/assets/images/icons/TransactionListLoader';
@@ -13,6 +13,7 @@ import AccountRecord, {
 } from './components/AccountRecord';
 import styles from './AccountHistory.module.scss';
 import { useRouter } from 'next/router';
+import { ProjectPropsContext } from '../../common/Layout/ProjectPropsContext';
 
 const { useTranslation } = i18next;
 
@@ -38,6 +39,7 @@ export default function History({
     null
   );
   const [openModal, setOpenModal] = React.useState(false);
+  const { isMobile } = useContext(ProjectPropsContext);
   const router = useRouter();
 
   const handleRecordOpen = (index: number) => {
@@ -69,7 +71,7 @@ export default function History({
 
   useEffect(() => {
     const { ref } = router.query;
-    let refIndex = 0;
+    let refIndex = !isMobile ? 0 : undefined;
     if (paymentHistory?.items && paymentHistory.items.length > 0) {
       if (ref) {
         const _refIndex = paymentHistory?.items?.findIndex(
@@ -77,7 +79,7 @@ export default function History({
         );
         _refIndex && _refIndex !== -1 && (refIndex = _refIndex);
       }
-      handleRecordOpen(refIndex);
+      if (refIndex !== undefined) handleRecordOpen(refIndex);
     }
   }, [paymentHistory]);
 
