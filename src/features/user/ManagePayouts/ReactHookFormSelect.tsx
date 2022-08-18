f6e63e8e06c2e82c803b0d4ef854fbbc07613ee5
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-08-18 11:17:20 +0530

    feat: add error display to ReactHookFormSelect

--- src/features/user/ManagePayouts/ReactHookFormSelect.tsx
+++ src/features/user/ManagePayouts/ReactHookFormSelect.tsx
@@ -1,27 +1,44 @@
-import { FormControl, InputLabel, Select } from '@mui/material';
-import { Controller } from 'react-hook-form';
+import { ReactElement, ReactNode } from 'react';
+import { Controller, Control, RegisterOptions } from 'react-hook-form';
+import { FormControl, TextField } from '@mui/material';
+
+interface Props {
+  name: string;
+  label: string;
+  error?: boolean;
+  helperText?: string;
+  control: Control;
+  rules?: Exclude<
+    RegisterOptions,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs'
+  >;
+  defaultValue?: unknown;
+  children: ReactNode;
+}
 
 const ReactHookFormSelect = ({
   name,
   label,
+  error,
+  helperText,
   control,
+  rules,
   defaultValue,
   children,
   ...props
-}) => {
-  const labelId = `${name}-label`;
+}: Props): ReactElement => {
   return (
     <FormControl {...props}>
-      <InputLabel id={labelId}>{label}</InputLabel>
       <Controller
         as={
-          <Select labelId={labelId} label={label}>
+          <TextField select label={label} error={error} helperText={helperText}>
             {children}
-          </Select>
+          </TextField>
         }
         name={name}
         control={control}
         defaultValue={defaultValue}
+        rules={rules}
       />
     </FormControl>
   );
