1888914a9b24932b882e38a18b6d0a642265dd2a
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-08-23 11:59:11 +0530

    feat: create basic transactions screen with data
    - fetch data for planetcash transactions
    - create accordion view ui for planet cash transactions
    - remove duplicated styles for .record

--- src/features/user/PlanetCash/screens/Transactions.tsx
+++ src/features/user/PlanetCash/screens/Transactions.tsx
@@ -1,7 +1,62 @@
-import { ReactElement } from 'react';
+import { ReactElement, useContext, useState, useEffect } from 'react';
+
+import AccountRecord from '../../Account/components/AccountRecord';
+
+import { getAuthenticatedRequest } from '../../../../utils/apiRequests/api';
+import { UserPropsContext } from '../../../common/Layout/UserPropsContext';
+import { ErrorHandlingContext } from '../../../common/Layout/ErrorHandlingContext';
 
 const Transactions = (): ReactElement => {
-  return <div>Transactions Screen</div>;
+  const { token, contextLoaded } = useContext(UserPropsContext);
+  const { handleError } = useContext(ErrorHandlingContext);
+  const [transactionList, setTransactionList] =
+    useState<Payments.PaymentHistory | null>(null);
+  const [selectedRecord, setSelectedRecord] = useState<number | null>(null);
+
+  const handleRecordToggle = (index: number) => {
+    if (selectedRecord === index) {
+      setSelectedRecord(null);
+    } else {
+      setSelectedRecord(index);
+    }
+  };
+
+  const fetchTransactions = async () => {
+    if (contextLoaded && token) {
+      const transactions: Payments.PaymentHistory =
+        await getAuthenticatedRequest(
+          `/app/paymentHistory?filter=planet-cash&limit=15`,
+          token,
+          {},
+          handleError,
+          '/profile/planetcash'
+        );
+      setTransactionList(transactions);
+    }
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [contextLoaded, token]);
+
+  return transactionList ? (
+    <>
+      {transactionList.items.map((record, index) => {
+        return (
+          <AccountRecord
+            key={index}
+            handleRecordOpen={handleRecordToggle}
+            index={index}
+            selectedRecord={selectedRecord}
+            record={record}
+            paymentHistory={transactionList}
+          />
+        );
+      })}
+    </>
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
 export default Transactions;
