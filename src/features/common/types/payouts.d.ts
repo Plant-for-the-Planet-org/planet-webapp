52a52667009a467cb3116888310620353cd34407
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-09-13 17:37:57 +0530

    feat: set up PayoutsContext

---
+++ src/features/common/types/payouts.d.ts
@@ -0,0 +1,21 @@
+declare namespace Payouts {
+  export interface BankAccount {
+    id: string;
+    currency?: string;
+    payoutMinAmount: number;
+    bankName: string;
+    bankAddress: string;
+    routingNumber: string | null;
+    bic: string;
+    branchCode: string | null;
+    holderName: string;
+    holderAddress: string;
+    accountNumber: string;
+    holderType: string;
+    remarks: string | null;
+  }
+
+  interface PayoutMinAmounts {
+    [key: string]: number; //key will be 'CHF', 'EUR' etc.
+  }
+}
