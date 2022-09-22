6e80b34b931db59fda2df2f8918c32a0055f1fa0
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-09-22 17:23:59 +0530

    fix: cleanup for release
    - remove unused countries.d.ts
    - add unmount cleanup useEffect to /profile/planetcash

--- pages/profile/planetcash/index.tsx
+++ pages/profile/planetcash/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import TopProgressBar from '../../../src/features/common/ContentLoaders/TopProgressBar';
 import UserLayout from '../../../src/features/common/Layout/UserLayout/UserLayout';
 import Head from 'next/head';
@@ -12,6 +12,13 @@ const { useTranslation } = i18next;
 export default function PlanetCashPage(): ReactElement {
   const { t, ready } = useTranslation('me');
   const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    // Cleanup function to reset state and address Warning: Can't perform a React state update on an unmounted component.
+    return () => {
+      setProgress(0);
+    };
+  }, []);
 
   return (
     <>
