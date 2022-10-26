296a4cb62de6da1c97d1055152c4d9b299831d4a
prachigarg19 <prachigarg1923@gmail.com>
2022-10-26 11:31:59 +0530

    add condition for open units=0

--- src/features/user/GiftFunds/Details.tsx
+++ src/features/user/GiftFunds/Details.tsx
@@ -12,28 +12,31 @@ const Details = () => {
     ready &&
     user.planetCash && (
       <div className={styles.allGiftFundscontainer}>
-        {user.planetCash.giftFunds.map((gift, index) => (
-          <div className={styles.container} key={index}>
-            <div className={styles.container_heading}>
-              <b>
-                {user.planetCash?.country}/{user.planetCash?.currency}{' '}
-                {t('title')}
-              </b>
-            </div>
-            <hr />
-            <div className={styles.container_details}>
-              <div className={styles.project}>
-                <b>{t('project')}</b>
-                <p>{gift.project}</p>
+        {user.planetCash.giftFunds
+          .filter((gift) => gift.openUnits !== 0)
+          .map((gift, index) => (
+            //Not displaying details for gift fund where open units = 0
+            <div className={styles.container} key={index}>
+              <div className={styles.container_heading}>
+                <b>
+                  {user.planetCash?.country}/{user.planetCash?.currency}{' '}
+                  {t('title')}
+                </b>
               </div>
+              <hr />
+              <div className={styles.container_details}>
+                <div className={styles.project}>
+                  <b>{t('project')}</b>
+                  <p>{gift.project}</p>
+                </div>
 
-              <div className={styles.unit}>
-                <b>{t('units')}</b>
-                <p>{Number(gift.openUnits / 100).toFixed(2)}</p>
+                <div className={styles.unit}>
+                  <b>{t('units')}</b>
+                  <p>{Number(gift.openUnits / 100).toFixed(2)}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     )
   );
