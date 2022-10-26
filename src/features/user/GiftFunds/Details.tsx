838acd4236ae2869b96d514ae00681f23bf32111
prachigarg19 <prachigarg1923@gmail.com>
2022-10-26 11:16:51 +0530

    Changes made: 1. Add translation 2. Add box shadow

--- src/features/user/GiftFunds/Details.tsx
+++ src/features/user/GiftFunds/Details.tsx
@@ -1,32 +1,34 @@
-import styled from '@emotion/styled';
-import Grid from '@mui/material/Grid';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { UserPropsContext } from '../../common/Layout/UserPropsContext';
-// import giftFunds from './giftFunds';
 import styles from './GiftFunds.module.scss';
+import i18next from '../../../../i18n';
 
+const { useTranslation } = i18next;
 const Details = () => {
   const { user } = useContext(UserPropsContext);
+  const { t, ready } = useTranslation('giftfunds');
 
   return (
+    ready &&
     user.planetCash && (
       <div className={styles.allGiftFundscontainer}>
         {user.planetCash.giftFunds.map((gift, index) => (
           <div className={styles.container} key={index}>
             <div className={styles.container_heading}>
               <b>
-                {user.planetCash?.country}/{user.planetCash?.currency} GiftFund
+                {user.planetCash?.country}/{user.planetCash?.currency}{' '}
+                {t('title')}
               </b>
             </div>
             <hr />
             <div className={styles.container_details}>
               <div className={styles.project}>
-                <b>Project</b>
+                <b>{t('project')}</b>
                 <p>{gift.project}</p>
               </div>
 
               <div className={styles.unit}>
-                <b>Available Units</b>
+                <b>{t('units')}</b>
                 <p>{Number(gift.openUnits / 100).toFixed(2)}</p>
               </div>
             </div>
