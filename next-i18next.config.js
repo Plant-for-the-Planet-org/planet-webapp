30c653222a909a4209eb4a21b9bd5dcb3819f28e
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2022-11-03 20:24:59 +0530

    i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,11 +2,9 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    fallbackLng: 'en',
+    localeDetection: false,
     defaultLocale: 'en',
     locales: ['en', 'de', 'cs', 'es', 'fr', 'it', 'pt-BR'],
-    localeDetection: false,
   },
   localePath: path.resolve('./public/static/locales'),
-  reloadOnPrerender: false,
 };
