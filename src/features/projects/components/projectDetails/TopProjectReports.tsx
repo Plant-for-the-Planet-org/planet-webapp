09379d117068b4fe5c9c9b3774b4193b4d89c194
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-12-07 23:49:51 +0530

    resolve i18n import error

--- src/features/projects/components/projectDetails/TopProjectReports.tsx
+++ src/features/projects/components/projectDetails/TopProjectReports.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import styles from './../../styles/ProjectDetails.module.scss';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import { getPDFFile } from '../../../../utils/getImageURL';
-import i18next from '../../../../../i18n';
 import parse from 'date-fns/parse';
 import format from 'date-fns/format';
 import { Trans } from 'react-i18next';
+import { useTranslation } from 'next-i18next';
 
 interface Props {
   data: object[];
 }
 export default function TopProjectReports(data: Props) {
-  const { useTranslation } = i18next;
-  const { t, ready } = useTranslation('common');
+  const { t, ready } = useTranslation(['common']);
   return (
     ready && (
       <>
