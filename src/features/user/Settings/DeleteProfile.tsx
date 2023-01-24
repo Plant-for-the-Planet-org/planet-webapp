b20da0fd131371b79d8e03a1f33f4ee7ecf45150
Shreyas Chorge <shreyaschorge81@gmail.com>
2023-01-24 12:11:45 +0530

    refactor: DeleteProfile deleteAuthenticatedRequest

--- src/features/user/Settings/DeleteProfile.tsx
+++ src/features/user/Settings/DeleteProfile.tsx
@@ -8,6 +8,7 @@ import { ErrorHandlingContext } from '../../common/Layout/ErrorHandlingContext';
 import CustomModal from '../../common/Layout/CustomModal';
 import router from 'next/router';
 import { useTranslation } from 'next-i18next';
+import { APIError, handleError, SerializedError } from '@planet-sdk/common';
 
 export default function DeleteProfile({}: any) {
   const { user, token, logoutUser } = React.useContext(UserPropsContext);
@@ -15,25 +16,35 @@ export default function DeleteProfile({}: any) {
   const handleChange = (e) => {
     e.preventDefault();
   };
-  const { handleError } = React.useContext(ErrorHandlingContext);
+  const { setErrors } = React.useContext(ErrorHandlingContext);
   const [isUploadingData, setIsUploadingData] = React.useState(false);
   const [isModalOpen, setisModalOpen] = React.useState(false); //true when subscriptions are present
   const [canDeleteAccount, setcanDeleteAccount] = React.useState(false);
 
-  const handleDeleteAccount = () => {
+  const handleDeleteAccount = async () => {
     setIsUploadingData(true);
-    deleteAuthenticatedRequest('/app/profile', token, handleError).then(
-      (res) => {
-        if (res.error_code === 'active_subscriptions') {
-          setIsUploadingData(false);
-          setisModalOpen(true);
-        } else if (res == 404) {
-          console.log(res.errorText);
-        } else {
-          logoutUser(`${process.env.NEXTAUTH_URL}/`);
+    try {
+      await deleteAuthenticatedRequest('/app/profile', token);
+      logoutUser(`${process.env.NEXTAUTH_URL}/`);
+    } catch (err) {
+      setIsUploadingData(false);
+      const serializedErrors = handleError(err as APIError);
+      const _serializedErrors: SerializedError[] = [];
+
+      for (const error of serializedErrors) {
+        switch (error.message) {
+          case 'active_subscriptions':
+            setisModalOpen(true);
+            break;
+
+          default:
+            _serializedErrors.push(error);
+            break;
         }
       }
-    );
+
+      setErrors(_serializedErrors);
+    }
   };
 
   const handleSubscriptions = () => {
