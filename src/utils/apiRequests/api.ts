a8f1f695aebd279fc5fb02c38a30b7c35a4e8da8
Shreyas Chorge <shreyaschorge81@gmail.com>
2023-01-26 15:23:14 +0530

    refator: avoid anti-pattern to use async/await inside of a new Promise() constructor

--- src/utils/apiRequests/api.ts
+++ src/utils/apiRequests/api.ts
@@ -104,32 +104,34 @@ export function getRequest<T>(
     ? url
     : `${process.env.API_ENDPOINT}${url}${queryStringSuffix}`;
 
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(fullUrl, {
-        method: 'GET',
-        headers: {
-          'tenant-key': `${TENANT_ID}`,
-          'X-SESSION-ID': await getsessionId(),
-          'x-locale': `${
-            localStorage.getItem('language')
-              ? localStorage.getItem('language')
-              : 'en'
-          }`,
-          'x-accept-versions': version ? version : '1.0.3',
-        },
-      });
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(fullUrl, {
+          method: 'GET',
+          headers: {
+            'tenant-key': `${TENANT_ID}`,
+            'X-SESSION-ID': await getsessionId(),
+            'x-locale': `${
+              localStorage.getItem('language')
+                ? localStorage.getItem('language')
+                : 'en'
+            }`,
+            'x-accept-versions': version ? version : '1.0.3',
+          },
+        });
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
+        }
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
-      }
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
 
@@ -145,33 +147,35 @@ export function getAuthenticatedRequest<T>(
   const queryString = getQueryString(query);
   const queryStringSuffix = queryString ? '?' + queryString : '';
 
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(
-        `${process.env.API_ENDPOINT}${url}${queryStringSuffix}`,
-        {
-          method: 'GET',
-          headers: {
-            'tenant-key': `${TENANT_ID}`,
-            'X-SESSION-ID': await getsessionId(),
-            Authorization: `Bearer ${token}`,
-            'x-locale': `${lang}`,
-            'x-accept-versions': version ? version : '1.0.3',
-            ...(header ? header : {}),
-          },
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(
+          `${process.env.API_ENDPOINT}${url}${queryStringSuffix}`,
+          {
+            method: 'GET',
+            headers: {
+              'tenant-key': `${TENANT_ID}`,
+              'X-SESSION-ID': await getsessionId(),
+              Authorization: `Bearer ${token}`,
+              'x-locale': `${lang}`,
+              'x-accept-versions': version ? version : '1.0.3',
+              ...(header ? header : {}),
+            },
+          }
+        );
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
         }
-      );
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
-      }
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
 
@@ -181,161 +185,168 @@ export function postAuthenticatedRequest<T>(
   token: any,
   headers?: any
 ) {
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(process.env.API_ENDPOINT + url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'tenant-key': `${TENANT_ID}`,
-          'X-SESSION-ID': await getsessionId(),
-          Authorization: `Bearer ${token}`,
-          'x-locale': `${
-            localStorage.getItem('language')
-              ? localStorage.getItem('language')
-              : 'en'
-          }`,
-          ...(headers ? headers : {}),
-        },
-      });
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(process.env.API_ENDPOINT + url, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+            'tenant-key': `${TENANT_ID}`,
+            'X-SESSION-ID': await getsessionId(),
+            Authorization: `Bearer ${token}`,
+            'x-locale': `${
+              localStorage.getItem('language')
+                ? localStorage.getItem('language')
+                : 'en'
+            }`,
+            ...(headers ? headers : {}),
+          },
+        });
 
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
+        }
+
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
-      }
-
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
 
 export function postRequest<T>(url: any, data: any) {
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(process.env.API_ENDPOINT + url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'tenant-key': `${TENANT_ID}`,
-          'X-SESSION-ID': await getsessionId(),
-          'x-locale': `${
-            localStorage.getItem('language')
-              ? localStorage.getItem('language')
-              : 'en'
-          }`,
-        },
-      });
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(process.env.API_ENDPOINT + url, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+            'tenant-key': `${TENANT_ID}`,
+            'X-SESSION-ID': await getsessionId(),
+            'x-locale': `${
+              localStorage.getItem('language')
+                ? localStorage.getItem('language')
+                : 'en'
+            }`,
+          },
+        });
 
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
+        }
+
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
-      }
-
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
 
 export function deleteAuthenticatedRequest<T>(url: any, token: any) {
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(process.env.API_ENDPOINT + url, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'tenant-key': `${TENANT_ID}`,
-          'X-SESSION-ID': await getsessionId(),
-          Authorization: `Bearer ${token}`,
-          'x-locale': `${
-            localStorage.getItem('language')
-              ? localStorage.getItem('language')
-              : 'en'
-          }`,
-        },
-      });
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(process.env.API_ENDPOINT + url, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'tenant-key': `${TENANT_ID}`,
+            'X-SESSION-ID': await getsessionId(),
+            Authorization: `Bearer ${token}`,
+            'x-locale': `${
+              localStorage.getItem('language')
+                ? localStorage.getItem('language')
+                : 'en'
+            }`,
+          },
+        });
 
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
-      }
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
+        }
 
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
 
 export function putAuthenticatedRequest<T>(url: any, data: any, token: any) {
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(process.env.API_ENDPOINT + url, {
-        method: 'PUT',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'tenant-key': `${TENANT_ID}`,
-          'X-SESSION-ID': await getsessionId(),
-          Authorization: `Bearer ${token}`,
-          'x-locale': `${
-            localStorage.getItem('language')
-              ? localStorage.getItem('language')
-              : 'en'
-          }`,
-        },
-      });
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(process.env.API_ENDPOINT + url, {
+          method: 'PUT',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+            'tenant-key': `${TENANT_ID}`,
+            'X-SESSION-ID': await getsessionId(),
+            Authorization: `Bearer ${token}`,
+            'x-locale': `${
+              localStorage.getItem('language')
+                ? localStorage.getItem('language')
+                : 'en'
+            }`,
+          },
+        });
 
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
-      }
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
+        }
 
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
 
 export function getRasterData<T>(id: any) {
-  return new Promise<T>(async (resolve, reject) => {
-    try {
-      const res = await fetch(
-        `${process.env.SITE_IMAGERY_API_URL}/api/v1/project/${id}`
-      );
+  return new Promise<T>((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await fetch(
+          `${process.env.SITE_IMAGERY_API_URL}/api/v1/project/${id}`
+        );
 
-      if (!res.ok) {
-        throw new APIError(res.status, await res.json());
+        if (!res.ok) {
+          throw new APIError(res.status, await res.json());
+        }
+
+        if (res.status === 204) {
+          resolve(true as T);
+        } else {
+          resolve(await res.json());
+        }
+      } catch (err) {
+        reject(err);
       }
-
-      if (res.status === 204) {
-        resolve(true as T);
-      } else {
-        resolve(await res.json());
-      }
-
-    } catch (err) {
-      reject(err);
-    }
+    })();
   });
 }
