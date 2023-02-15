4ba94315a1ca0a3aaec240d13a238769db892bae
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2023-02-15 11:51:28 +0530

    fix: alert for non existence email

--- src/features/user/Settings/ImpersonationActivated.tsx
+++ src/features/user/Settings/ImpersonationActivated.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'next-i18next';
 import LogoutIcon from '../../../../public/assets/images/icons/Sidebar/LogoutIcon';
 import styles from './SwitchUser.module.scss';
 import { ParamsContext } from '../../common/Layout/QueryParamsContext';
-import { UserPropsContext } from '../../common/Layout/UserPropsContext';
 import { useContext } from 'react';
 
 const ImpersonationActivated = () => {
-  const { setEmail, email } = useContext(ParamsContext);
-  const { alertError } = useContext(UserPropsContext);
+  const { setEmail, email, alertError, setAlertError } =
+    useContext(ParamsContext);
+
   const closeAlert = () => {
     localStorage.removeItem('targetEmail');
     setEmail('');
   };
-
+  useEffect(() => {
+    setAlertError('');
+  }, [email]);
   const { t } = useTranslation('me');
 
   return (
     <div className={styles.impersonationAlertContainer}>
       {alertError ? (
+        <div>{t('me:userNotexist')}</div>
+      ) : (
         <>
-          {' '}
           <div>{t('me:targetUser')}</div>
           <div>{email && `<${email}>`}</div>
         </>
-      ) : (
-        <div>{t('me:userNotexist')}</div>
       )}
 
       <div className={styles.logoutContainer}>
