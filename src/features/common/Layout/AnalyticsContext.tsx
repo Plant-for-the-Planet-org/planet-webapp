5fce084456c3b6341a59bd699373a87ee78b3423
Shreyaschorge <shreyaschorge81@gmail.com>
2023-02-23 12:08:43 +0530

    Remove Unused Variables

--- src/features/common/Layout/AnalyticsContext.tsx
+++ src/features/common/Layout/AnalyticsContext.tsx
@@ -1,41 +1,47 @@
-import {useContext, createContext, useMemo, useState, Dispatch, SetStateAction, FC} from 'react'
+import { useContext, createContext, useMemo, useState, FC } from 'react';
 
 import { SetState } from '../types/common';
 
 export interface Project {
-    guid: string;
-    slug: string;
-    name: string;
+  guid: string;
+  slug: string;
+  name: string;
 }
 
 interface AnalyticsContextInterface {
-    projectList: Project[] | null;
-    setProjectList: SetState<Project[] | null>
-    project: Project | null;
-    setProject: SetState<Project | null>
+  projectList: Project[] | null;
+  setProjectList: SetState<Project[] | null>;
+  project: Project | null;
+  setProject: SetState<Project | null>;
 }
 
-const AnalyticsContext = createContext<AnalyticsContextInterface | null>(null)
+const AnalyticsContext = createContext<AnalyticsContextInterface | null>(null);
 
-export const AnalyticsProvider: FC = ({children}) => {
+export const AnalyticsProvider: FC = ({ children }) => {
+  const [projectList, setProjectList] = useState<Project[] | null>(null);
+  const [project, setProject] = useState<Project | null>(null);
 
-    const [projectList, setProjectList] = useState<Project[] | null>(null)
-    const [project, setProject] = useState<Project| null>(null)
+  const value: AnalyticsContextInterface | null = useMemo(
+    () => ({
+      projectList,
+      setProjectList,
+      project,
+      setProject,
+    }),
+    [projectList, setProjectList, project, setProject]
+  );
 
-    const value: AnalyticsContextInterface | null = useMemo(() => ({
-        projectList,
-        setProjectList,
-        project,
-        setProject
-    }), [projectList, setProjectList, project, setProject])
-
-    return (<AnalyticsContext.Provider value={value}>{children}</AnalyticsContext.Provider>)
-}
+  return (
+    <AnalyticsContext.Provider value={value}>
+      {children}
+    </AnalyticsContext.Provider>
+  );
+};
 
 export const useAnalytics = (): AnalyticsContextInterface => {
-    const context = useContext(AnalyticsContext);
-    if (!context) {
-      throw new Error('AnalyticsContext must be used within AnalyticsProvider');
-    }
-    return context;
-  };
+  const context = useContext(AnalyticsContext);
+  if (!context) {
+    throw new Error('AnalyticsContext must be used within AnalyticsProvider');
+  }
+  return context;
+};
