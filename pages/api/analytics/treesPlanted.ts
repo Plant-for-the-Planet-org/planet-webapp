fbac9b11b8e4de5cdc91b826085cdd04b566e1ba
Shreyaschorge <shreyaschorge81@gmail.com>
2023-02-23 18:15:08 +0530

    Clean code

--- pages/api/analytics/treesPlanted.ts
+++ pages/api/analytics/treesPlanted.ts
@@ -1,8 +1,7 @@
 export default function handler(req, res) {
   if (req.method === 'GET') {
-    // Process a POST request - Example:
     res.status(200).json({ trees_planted: 'hello_world' });
-    
+
   } else {
     res.status(400).send(`${req.method} Method not supported`);
   }
