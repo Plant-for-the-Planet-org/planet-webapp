f23c9dd146a47401675e5e6d61e10e23122cbfb7
Shreyaschorge <shreyaschorge81@gmail.com>
2023-02-23 18:58:23 +0530

    Add API handler for trees-planted

--- pages/api/analytics/trees-planted.ts
+++ pages/api/analytics/trees-planted.ts
@@ -1,8 +1,34 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ trees_planted: 'hello_world' });
+import { NextApiRequest } from 'next';
+import db from '../../../src/utils/connectDB';
 
+export default async function handler(req: NextApiRequest, response) {
+  const { projectId, startDate, endDate } = req.body;
+
+  if (req.method === 'POST') {
+    try {
+      const res = await db.query(
+        'SELECT \
+          plant_project_id, \
+          DATE_SUB(plant_date, INTERVAL WEEKDAY(plant_date) DAY) AS week_start_date, \
+          DATE_ADD(DATE_SUB(plant_date, INTERVAL WEEKDAY(plant_date) DAY), INTERVAL 6 DAY) AS week_end_date, \
+          WEEK(plant_date) AS week_data, \
+          SUM(trees_planted) AS trees_planted \
+        FROM plant_location \
+        WHERE plant_project_id = ? AND plant_date BETWEEN ? AND ? \
+        GROUP BY plant_project_id, week_data \
+        ORDER BY plant_date;',
+        [projectId, startDate, endDate]
+      );
+
+      await db.end();
+
+      response.status(200).json({ trees_planted: res });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await db.quit();
+    }
   } else {
-    res.status(400).send(`${req.method} Method not supported`);
+    response.status(400).send(`${req.method} Method not supported`);
   }
 }
