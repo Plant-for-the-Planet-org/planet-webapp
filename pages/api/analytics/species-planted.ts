faa4cf981ce906cf893e628d4ccb8e07cd69600f
Shreyaschorge <shreyaschorge81@gmail.com>
2023-03-09 17:55:20 +0530

    Update Species Planted query

--- pages/api/analytics/species-planted.ts
+++ pages/api/analytics/species-planted.ts
@@ -9,24 +9,29 @@ export default async function handler(
 
   if (req.method === 'POST') {
     try {
-      const res = await db.query(
+      const query =
         'SELECT \
-            pl.plant_project_id, \
-            pl.plant_date, \
-            ps.tree_count, \
-            ps.scientific_species_id, \
-            ss.name, \
-            ps.other_species \
-            FROM planted_species ps \
+          ps.other_species, \
+          ps.scientific_species_id, \
+          ss.name, \
+          SUM(ps.tree_count) AS total_tree_count \
+        FROM planted_species ps \
         INNER JOIN plant_location pl ON ps.plant_location_id = pl.id \
         LEFT JOIN scientific_species ss ON ps.scientific_species_id = ss.id \
-        WHERE pl.plant_project_id = ? AND plant_date BETWEEN ? AND ?',
-        [projectId, startDate, `${endDate} 23:59:59.999`]
-      );
+        JOIN plant_project pp ON pl.plant_project_id = pp.id \
+        WHERE pp.guid = ? AND pl.plant_date BETWEEN ? AND ? \
+        GROUP BY ps.scientific_species_id, ss.name, ps.other_species \
+        ORDER BY total_tree_count DESC';
+
+      const res = await db.query(query, [
+        projectId,
+        startDate,
+        `${endDate} 23:59:59.999`,
+      ]);
 
       await db.end();
 
-      response.status(200).json({ species_planted: res });
+      response.status(200).json({ data: res });
     } catch (err) {
       console.log(err);
     } finally {
