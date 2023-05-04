c65948ce2a396aa251bf60892d9c692e365508b2
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-05-04 18:47:43 +0530

    feat: adds "challenge over" text (landing section)

--- src/tenants/salesforce/Campaign/components/Landing.tsx
+++ src/tenants/salesforce/Campaign/components/Landing.tsx
@@ -12,6 +12,10 @@ export default function Landing({ tenantScore }: Props) {
         <div className={styles.landingContentTop}>
           <h3>MOVING Toward 100 Million Trees</h3>
           <TreeCounter target={100000000} planted={tenantScoreData || 0} />
+          <p>
+            This Challenge ended April 30th, thank you for your participation.
+            Join us next year!
+          </p>
         </div>
       </div>
     </section>
