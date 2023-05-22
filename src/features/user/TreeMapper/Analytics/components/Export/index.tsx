febaeb259ef7f72f505da16183c192e82e1289f3
Shreyaschorge <shreyaschorge81@gmail.com>
2023-05-22 13:35:30 +0530

    Fix createdAt header title

--- src/features/user/TreeMapper/Analytics/components/Export/index.tsx
+++ src/features/user/TreeMapper/Analytics/components/Export/index.tsx
@@ -127,7 +127,7 @@ export const Export = () => {
     },
     {
       title: 'created',
-      description: t('exportColumnHeaders.createds'),
+      description: t('exportColumnHeaders.created'),
     },
   ];
 
