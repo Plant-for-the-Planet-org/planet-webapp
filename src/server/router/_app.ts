c1b41eb9e7edb7b515190af6740340d431120639
Shreyaschorge <shreyaschorge81@gmail.com>
2023-05-29 14:15:07 +0530

    Add schema

--- src/server/router/_app.ts
+++ src/server/router/_app.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from "@prisma/client";
-import {router, procedure} from "../trpc"
+import { PrismaClient } from '@prisma/client';
+import { router, procedure } from '../trpc';
 
-const prisma = new PrismaClient()
-prisma.$connect()
+const prisma = new PrismaClient();
+prisma.$connect();
 
 export const appRouter = router({
-    contribution: procedure.query(async()=>{
-       const data = await prisma.contribution.findMany()
-        console.log(data)
-        return data
-    })
-})
+  contribution: procedure.query(async () => {
+    const data = await prisma.contribution.findMany({ take: 5 });
+    console.log(data);
+    return data;
+  }),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -9,9 +9,6 @@ function getBaseUrl() {
     if (process.env.VERCEL_URL)
       // reference for vercel.com
       return `https://${process.env.VERCEL_URL}`;
-    if (process.env.RENDER_INTERNAL_HOSTNAME)
-      // reference for render.com
-      return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
     // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`;
   }
