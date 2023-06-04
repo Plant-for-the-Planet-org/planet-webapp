0c721982a28cfbbd51999cc464ac520855700f31
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-06-05 01:27:24 +0530

    feat: add all project icons

--- src/features/projects/components/ProjectTypeIcon.tsx
+++ src/features/projects/components/ProjectTypeIcon.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import NaturalRegenerationIcon from '../../../../public/assets/images/icons/project/NaturalRegenerationIcon';
+import Agroforestry from '../../../../public/assets/images/icons/project/Agroforestry';
+import Mangroves from '../../../../public/assets/images/icons/project/Mangroves';
+import NaturalRegeneration from '../../../../public/assets/images/icons/project/NaturalRegeneration';
+import UrbanRestoration from '../../../../public/assets/images/icons/project/UrbanRestoration';
+import Conservation from '../../../../public/assets/images/icons/project/Conservation';
+import ManagedRegeneration from '../../../../public/assets/images/icons/project/ManagedRegeneration';
+import TreePlanting from '../../../../public/assets/images/icons/project/TreePlanting';
+import OtherRestoration from '../../../../public/assets/images/icons/project/OtherRestoration';
 interface Props {
   projectType: string;
 }
 const ProjectTypeIcon = ({ projectType }: Props) => {
   switch (projectType) {
     case 'natural-regeneration':
-      return <NaturalRegenerationIcon />;
+      return <NaturalRegeneration />;
+    case 'mangroves':
+      return <Mangroves />;
+    case 'managed-regeneration':
+      return <ManagedRegeneration />;
+    case 'agroforestry':
+      return <Agroforestry />;
+    case 'urban-planting':
+      return <UrbanRestoration />;
+    case 'conservation':
+      return <Conservation />;
+    case 'restoration' || 'other-planting':
+      return <OtherRestoration />;
+    case 'large-scale-planting':
+      return <TreePlanting />;
     default:
       return null;
   }
