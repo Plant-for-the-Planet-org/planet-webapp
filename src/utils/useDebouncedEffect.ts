6c59bd5e1d7d1f198a10b9a2f053171e122cfd87
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-06-15 11:08:30 +0530

    docs: documents `useDebouncedEffect`

--- src/utils/useDebouncedEffect.ts
+++ src/utils/useDebouncedEffect.ts
@@ -1,6 +1,12 @@
 // credits to https://stackoverflow.com/questions/54666401/how-to-use-throttle-or-debounce-with-react-hook
 import { useCallback, useEffect } from 'react';
 
+/**
+ * Produces a debounced version of `useEffect`
+ * @param effect - callback function to run within the `useEffect`
+ * @param delay - time delay in ms
+ * @param deps - dependency array
+ */
 export const useDebouncedEffect = (
   effect: () => void,
   delay: number,
