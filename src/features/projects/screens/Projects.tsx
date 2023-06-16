205c2d3cafac0599ff7393bb8c2dcb6c071d5be9
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-06-16 15:39:38 +0530

    Revert "fix: resolves react bad setState() warning"
    This reverts commit 5b9758b5a031ed49e67071badb91842923f9d975.

--- src/features/projects/screens/Projects.tsx
+++ src/features/projects/screens/Projects.tsx
@@ -11,7 +11,6 @@ import { useDebouncedEffect } from '../../../utils/useDebouncedEffect';
 import Explore from '../components/maps/Explore';
 import { ParamsContext } from '../../common/Layout/QueryParamsContext';
 import { useUserProps } from '../../../../src/features/common/Layout/UserPropsContext';
-import { ProjectMapInfo } from '@planet-sdk/common';
 
 interface Props {
   projects: any;
@@ -42,9 +41,6 @@ function ProjectsList({
   const [searchMode, setSearchMode] = React.useState(false);
   const [searchValue, setSearchValue] = React.useState('');
   const [trottledSearchValue, setTrottledSearchValue] = React.useState('');
-  const [searchProjectResults, setSearchProjectResults] = React.useState<
-    ProjectMapInfo[] | undefined
-  >();
 
   useDebouncedEffect(
     () => {
@@ -70,7 +66,7 @@ function ProjectsList({
     }
   }
 
-  function getSearchProjects(projects: Array<ProjectMapInfo>, keyword: string) {
+  function getSearchProjects(projects: Array<any>, keyword: string) {
     let resultProjects = [];
     if (keyword !== '') {
       const keywords = keyword.split(/[\s\-.,+]+/);
@@ -129,13 +125,10 @@ function ProjectsList({
     [projects]
   );
 
-  React.useEffect(() => {
-    const _searchProjectResults = getSearchProjects(
-      projects,
-      trottledSearchValue
-    );
-    setSearchProjectResults(_searchProjectResults);
-  }, [trottledSearchValue, projects]);
+  const searchProjectResults = React.useMemo(
+    () => getSearchProjects(projects, trottledSearchValue),
+    [trottledSearchValue, projects]
+  );
 
   const topProjects = React.useMemo(
     () => getProjects(projects, 'top'),
