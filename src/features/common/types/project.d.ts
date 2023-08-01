f4f596ffe142a254faca7d2f5fbdb38af63e667f
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-08-01 14:13:24 +0530

    feat: sets up ProfileProject types

--- src/features/common/types/project.d.ts
+++ src/features/common/types/project.d.ts
@@ -1,3 +1,9 @@
+import { CountryCode } from '@planet-sdk/common/build/types/country';
+import {
+  ConservationProjectExtended,
+  TreeProjectExtended,
+} from '@planet-sdk/common/build/types/project/extended';
+
 export interface ProjectOption {
   guid: string;
   slug: string;
@@ -33,3 +39,92 @@ export interface LastUpdated {
   timezone: string;
   timezone_type: number;
 }
+
+export interface ProfileProjectConservation
+  extends Omit<
+    ConservationProjectExtended,
+    | '_scope'
+    | 'certificates'
+    | 'fixedRates'
+    | 'images'
+    | 'isPublished'
+    | 'lastUpdated'
+    | 'minQuantity'
+    | 'options'
+    | 'sites'
+    | 'taxDeductionCountries'
+    | 'yearAcquired'
+    | 'treeCost'
+    | 'coordinates'
+    | 'expenses'
+    | 'geoLocation'
+    | 'reviews'
+    | 'tpo'
+  > {
+  publish: boolean;
+  taxDeductibleCountries: CountryCode[];
+  isFeatured: boolean;
+  isVerified: boolean;
+  verificationStatus: VerificationStatus;
+  acceptDonations: boolean;
+  geoLongitude: number;
+  geoLatitude: number;
+  isApproved: boolean;
+  isTopProject: boolean;
+  classification: null;
+}
+
+export interface ProfileProjectTrees
+  extends Omit<
+    TreeProjectExtended,
+    | '_scope'
+    | 'certificates'
+    | 'fixedRates'
+    | 'images'
+    | 'isPublished'
+    | 'lastUpdated'
+    | 'minQuantity'
+    | 'options'
+    | 'sites'
+    | 'taxDeductionCountries'
+    | 'yearAcquired'
+    | 'coordinates'
+    | 'countDonated'
+    | 'countPlanted'
+    | 'countRegistered'
+    | 'expenses'
+    | 'geoLocation'
+    | 'isCertified'
+    | 'location'
+    | 'minTreeCount'
+    | 'paymentDefaults'
+    | 'reviews'
+    | 'tpo'
+    | 'degradationCause'
+    | 'degradationYear',
+    | 'employeesCount'
+    | 'longTermPlan'
+    | 'mainChallenge'
+    | 'motivation'
+    | 'plantingDensity'
+    | 'plantingSeasons'
+    | 'siteOwnerName'
+    | 'yearAbandoned'
+  > {
+  publish: boolean;
+  taxDeductibleCountries: CountryCode[];
+  isFeatured: boolean;
+  isVerified: boolean;
+  verificationStatus: VerificationStatus;
+  acceptDonations: boolean;
+  geoLongitude: number;
+  geoLatitude: number;
+  revisionPeriodicityLevel: null;
+}
+
+type VerificationStatus =
+  | 'incomplete'
+  | 'accepted'
+  | 'processing'
+  | 'denied'
+  | 'pending';
