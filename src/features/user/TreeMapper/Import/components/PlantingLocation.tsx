76786aa61b80adc34db591153aecb2c0c2426c7e
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-09-11 12:04:37 +0530

    refactor: PlantingLocation

--- src/features/user/TreeMapper/Import/components/PlantingLocation.tsx
+++ src/features/user/TreeMapper/Import/components/PlantingLocation.tsx
@@ -1,5 +1,12 @@
 import React, { ReactElement } from 'react';
-import { Controller, useForm, useFieldArray } from 'react-hook-form';
+import {
+  Controller,
+  useForm,
+  useFieldArray,
+  FieldErrors,
+  FieldArrayWithId,
+  Control,
+} from 'react-hook-form';
 import styles from '../Import.module.scss';
 import { useTranslation } from 'next-i18next';
 import { localeMapForDate } from '../../../../../utils/language/getLanguageName';
@@ -23,6 +30,10 @@ import themeProperties from '../../../../../theme/themeProperties';
 import { handleError, APIError } from '@planet-sdk/common';
 import { ErrorHandlingContext } from '../../../../common/Layout/ErrorHandlingContext';
 import { MapProject } from '../../../../common/types/ProjectPropsContextInterface';
+import { Treemapper } from '../../Treemapper';
+import { FeatureCollection, GeometryObject } from 'geojson';
+import { Species } from '../../../../common/types/plantLocation';
+import { Geometry } from '@turf/turf';
 
 const dialogSx: SxProps = {
   '& .MuiButtonBase-root.MuiPickersDay-root.Mui-selected': {
@@ -41,12 +52,109 @@ const dialogSx: SxProps = {
   },
 };
 
+interface SpeciesProps {
+  index: number;
+  t: Function;
+  remove: Function;
+  errors: FieldErrors<Treemapper.PlantLocation>;
+  item: FieldArrayWithId<Treemapper.PlantLocation, 'plantedSpecies', 'id'>;
+  control: Control<Treemapper.PlantLocation, any>;
+}
+
+function PlantedSpecies({
+  index,
+  t,
+  remove,
+  errors,
+  item,
+  control,
+}: SpeciesProps): ReactElement {
+  return (
+    <div key={item.id} className={styles.speciesFieldGroup}>
+      <div className={styles.speciesNameField}>
+        {/* <SpeciesSelect label={t('treemapper:species')} name={`plantedSpecies[${index}].species`} mySpecies={mySpecies} control={control} /> */}
+        <Controller
+          name={`plantedSpecies[${index}].otherSpecies`}
+          control={control}
+          rules={{
+            required:
+              index > 0 ? false : t('treemapper:atLeastOneSpeciesRequired'),
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextField
+              label={t('treeSpecies')}
+              variant="outlined"
+              onChange={onChange}
+              onBlur={onBlur}
+              value={value}
+              error={
+                errors.plantedSpecies &&
+                errors.plantedSpecies[index]?.otherSpecies !== undefined
+              }
+              helperText={
+                errors.plantedSpecies &&
+                errors.plantedSpecies[index]?.otherSpecies &&
+                (errors.plantedSpecies[index]?.otherSpecies?.message ?? '')
+              }
+            />
+          )}
+        />
+      </div>
+      <div className={styles.speciesCountField}>
+        <Controller
+          name={`plantedSpecies[${index}].treeCount`}
+          control={control}
+          rules={{
+            required: index > 0 ? false : t('treemapper:treesRequired'),
+            validate: (value) => {
+              return parseInt(value, 10) >= 1
+                ? true
+                : t('treemapper:treesRequired');
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextField
+              label={t('treemapper:count')}
+              variant="outlined"
+              onChange={(e) => {
+                e.target.value = e.target.value.replace(/[^0-9]/g, '');
+                onChange(e.target.value);
+              }}
+              value={value > 0 ? value : ''}
+              onBlur={onBlur}
+              error={
+                errors.plantedSpecies &&
+                errors.plantedSpecies[index]?.treeCount !== undefined
+              }
+              helperText={
+                errors.plantedSpecies &&
+                errors.plantedSpecies[index]?.treeCount &&
+                (errors.plantedSpecies[index]?.treeCount?.message ?? '')
+              }
+            />
+          )}
+        />
+      </div>
+      {index > 0 ? (
+        <div
+          onClick={() => remove(index)}
+          className={`${styles.speciesDeleteField} ${styles.deleteActive}`}
+        >
+          <DeleteIcon />
+        </div>
+      ) : (
+        <div className={styles.speciesDeleteField}></div>
+      )}
+    </div>
+  );
+}
+
 interface Props {
   handleNext: () => void;
   userLang: string;
-  plantLocation: any;
+  plantLocation: Treemapper.PlantLocation | null;
   setPlantLocation: Function;
-  geoJson: any;
+  geoJson: Geometry;
   setGeoJson: Function;
   activeMethod: string;
   setActiveMethod: Function;
@@ -68,12 +176,12 @@ export default function PlantingLocation({
   const [projects, setProjects] = React.useState<MapProject[]>([]);
   const importMethods = ['import', 'editor'];
   const [geoJsonError, setGeoJsonError] = React.useState(false);
-  const [mySpecies, setMySpecies] = React.useState(null);
+  const [mySpecies, setMySpecies] = React.useState<Species | null>(null);
   const { setErrors } = React.useContext(ErrorHandlingContext);
 
   const { t } = useTranslation(['treemapper', 'common', 'maps']);
   const defaultValues = {
-    plantDate: new Date(),
+    plantDate: '',
     plantProject: '',
     geometry: {},
     plantedSpecies: [
@@ -112,7 +220,7 @@ export default function PlantingLocation({
 
   const loadMySpecies = async () => {
     try {
-      const species = await getAuthenticatedRequest(
+      const species: Species = await getAuthenticatedRequest(
         '/treemapper/species',
         token,
         logoutUser
@@ -130,8 +238,12 @@ export default function PlantingLocation({
     }
   }, [contextLoaded]);
 
-  const normalizeGeoJson = (geoJson: any) => {
-    if (gjv.isGeoJSONObject(geoJson) && geoJson.features?.length > 0) {
+  const normalizeGeoJson = (geoJson: GeometryObject | FeatureCollection) => {
+    if (
+      gjv.isGeoJSONObject(geoJson) &&
+      'features' in geoJson &&
+      geoJson.features?.length > 0
+    ) {
       const flattened = flatten(geoJson);
       if (flattened.features[0]?.geometry?.type === 'Polygon') {
         setGeoJsonError(false);
@@ -150,12 +262,12 @@ export default function PlantingLocation({
   };
 
   const onDrop = React.useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file: any) => {
+    acceptedFiles.forEach((file: File) => {
       const reader = new FileReader();
       reader.readAsText(file);
       reader.onabort = () => console.log('file reading was aborted');
       reader.onerror = () => console.log('file reading has failed');
-      reader.onload = (event: any) => {
+      reader.onload = () => {
         const fileType =
           file.name.substring(
             file.name.lastIndexOf('.') + 1,
@@ -165,11 +277,10 @@ export default function PlantingLocation({
           reader.readAsText(file);
           reader.onabort = () => console.log('file reading was aborted');
           reader.onerror = () => console.log('file reading has failed');
-          reader.onload = (event: any) => {
-            const dom = new DOMParser().parseFromString(
-              event.target.result,
-              'text/xml'
-            );
+          reader.onload = (event) => {
+            const result = event.target?.result;
+            if (typeof result !== 'string') return;
+            const dom = new DOMParser().parseFromString(result, 'text/xml');
             const geo = tj.kml(dom);
             if (gjv.isGeoJSONObject(geo) && geo.features.length !== 0) {
               const flattened = flatten(geo);
@@ -189,15 +300,18 @@ export default function PlantingLocation({
           reader.onabort = () => console.log('file reading was aborted');
           reader.onerror = () => console.log('file reading has failed');
           reader.onload = (event) => {
-            const geo = JSON.parse(event.target.result);
-            normalizeGeoJson(geo);
+            const result = event.target?.result;
+            if (typeof result === 'string') {
+              const geo = JSON.parse(result);
+              normalizeGeoJson(geo);
+            }
           };
         }
       };
     });
   }, []);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     accept: ['.geojson', '.kml'],
     multiple: false,
     onDrop: onDrop,
@@ -205,7 +319,7 @@ export default function PlantingLocation({
     onFileDialogCancel: () => setIsUploadingData(false),
   });
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: Treemapper.PlantLocation) => {
     if (geoJson) {
       setIsUploadingData(true);
       const submitData = {
@@ -213,7 +327,7 @@ export default function PlantingLocation({
         captureMode: 'external',
         geometry: geoJson,
         plantedSpecies: data.plantedSpecies,
-        plantDate: data.plantDate.toISOString(),
+        plantDate: new Date(data.plantDate).toISOString(),
         registrationDate: new Date().toISOString(),
         plantProject: data.plantProject,
       };
@@ -338,7 +452,7 @@ export default function PlantingLocation({
                 onChange={onChange}
                 onBlur={onBlur}
                 value={value}
-                error={errors.plantProject}
+                error={errors.plantProject !== undefined}
                 helperText={errors.plantProject && errors.plantProject.message}
               >
                 {projects.map((option) => (
@@ -393,6 +507,12 @@ export default function PlantingLocation({
           append({
             otherSpecies: '',
             treeCount: 0,
+            // Set to default or empty value for type match
+            scientificName: '',
+            created: '',
+            scientificSpecies: '',
+            id: '',
+            updated: '',
           });
         }}
         className={styles.addSpeciesButton}
@@ -415,100 +535,5 @@ export default function PlantingLocation({
         </Button>
       </div>
     </>
-  );
-}
-interface SpeciesProps {
-  index: number;
-  t: Function;
-  remove: Function;
-  errors: any;
-  item: any;
-  control: any;
-}
-
-function PlantedSpecies({
-  index,
-  t,
-  remove,
-  errors,
-  item,
-  control,
-}: SpeciesProps): ReactElement {
-  return (
-    <div key={item.id} className={styles.speciesFieldGroup}>
-      <div className={styles.speciesNameField}>
-        {/* <SpeciesSelect label={t('treemapper:species')} name={`plantedSpecies[${index}].species`} mySpecies={mySpecies} control={control} /> */}
-        <Controller
-          name={`plantedSpecies[${index}].otherSpecies`}
-          control={control}
-          rules={{
-            required:
-              index > 0 ? false : t('treemapper:atLeastOneSpeciesRequired'),
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextField
-              label={t('treeSpecies')}
-              variant="outlined"
-              onChange={onChange}
-              onBlur={onBlur}
-              value={value}
-              error={
-                errors.plantedSpecies &&
-                errors.plantedSpecies[index]?.otherSpecies
-              }
-              helperText={
-                errors.plantedSpecies &&
-                errors.plantedSpecies[index]?.otherSpecies &&
-                errors.plantedSpecies[index]?.otherSpecies.message
-              }
-            />
-          )}
-        />
-      </div>
-      <div className={styles.speciesCountField}>
-        <Controller
-          name={`plantedSpecies[${index}].treeCount`}
-          control={control}
-          rules={{
-            required: index > 0 ? false : t('treemapper:treesRequired'),
-            validate: (value: any) => {
-              return parseInt(value, 10) >= 1
-                ? true
-                : t('treemapper:treesRequired');
-            },
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextField
-              label={t('treemapper:count')}
-              variant="outlined"
-              onChange={(e: any) => {
-                e.target.value = e.target.value.replace(/[^0-9]/g, '');
-                onChange(e.target.value);
-              }}
-              value={value > 0 ? value : ''}
-              onBlur={onBlur}
-              error={
-                errors.plantedSpecies && errors.plantedSpecies[index]?.treeCount
-              }
-              helperText={
-                errors.plantedSpecies &&
-                errors.plantedSpecies[index]?.treeCount &&
-                errors.plantedSpecies[index]?.treeCount.message
-              }
-            />
-          )}
-        />
-      </div>
-      {index > 0 ? (
-        <div
-          onClick={() => remove(index)}
-          className={`${styles.speciesDeleteField} ${styles.deleteActive}`}
-        >
-          <DeleteIcon />
-        </div>
-      ) : (
-        <div className={styles.speciesDeleteField}></div>
-      )}
-    </div>
   );
 }
