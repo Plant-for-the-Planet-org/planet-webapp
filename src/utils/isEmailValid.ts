d381ca3f2b1eff652122959ea4e203d6f82dc4c4
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-10-19 17:16:34 +0530

    add INVALID_DOMAIN_LIST check

--- src/utils/isEmailValid.ts
+++ src/utils/isEmailValid.ts
@@ -1,9 +1,18 @@
 import { z } from 'zod';
 
+const INVALID_DOMAIN_LIST = ['dummy.de', 'example.com', 'company.de'];
+
 export const isEmailValid = (value: string): boolean => {
   try {
     const emailSchema = z.string().email();
-    emailSchema.parse(value);
+    const parsedEmail = emailSchema.parse(value);
+
+    const domain = parsedEmail.split('@')[1];
+
+    // Check if the domain is in the INVALID_DOMAIN_LIST
+    if (INVALID_DOMAIN_LIST.includes(domain)) {
+      return false;
+    }
   } catch (err) {
     return false;
   }
