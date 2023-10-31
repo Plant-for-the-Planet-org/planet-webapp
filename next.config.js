9e5844016ad99ee339e418d75441dde9dad298d9
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-10-31 11:29:23 +0530

    fix: fixes local next config warnings
    - Expected string, received boolean at "env.ENABLE_ANALYTICS"
    - "env.VERCEL_URL" is missing, expected string

--- next.config.js
+++ next.config.js
@@ -121,7 +121,7 @@ const nextConfig = {
     SITE_IMAGERY_API_URL: SITE_IMAGERY_API_URL,
     WIDGET_URL: process.env.WIDGET_URL,
     CONFIG_URL: process.env.CONFIG_URL,
-    ENABLE_ANALYTICS: DB_CONN_URL ? true : false,
+    ENABLE_ANALYTICS: DB_CONN_URL ? 'true' : 'false',
   },
   trailingSlash: false,
   reactStrictMode: true,
