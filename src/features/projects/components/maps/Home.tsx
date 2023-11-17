29ea240f9fbc51b1fe57ed5fcd2905b6f0d01e7c
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-11-17 20:32:03 +0530

    correct condition

--- src/features/projects/components/maps/Home.tsx
+++ src/features/projects/components/maps/Home.tsx
@@ -55,7 +55,7 @@ export default function Home({
 
   const notDonatableProjects = searchedProject.filter(
     (project) =>
-      !project.allowDonations &&
+      !project.properties.allowDonations &&
       !(project.properties.isTopProject && project.properties.isApproved)
   );
 
