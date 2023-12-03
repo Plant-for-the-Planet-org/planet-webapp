0371cc5e669077ac017de31d6d91430dd6ff7b10
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2023-12-03 13:21:29 +0530

    refactor marker popup

---
+++ src/features/user/ProfileV2/components/MicroComponents/PopUp.tsx
@@ -0,0 +1,106 @@
+import { useTranslation } from 'next-i18next';
+import MyForestMapStyle from '../../styles/MyForestMap.module.scss';
+import formatDate from '../../../../../utils/countryCurrency/getFormattedDate';
+
+interface PopUpLabelProps {
+  isConservation: boolean;
+  isNormalTreeDonation: boolean;
+  isRegisteredTree: boolean;
+}
+
+export const PopUpLabel = ({
+  isConservation,
+  isNormalTreeDonation,
+  isRegisteredTree,
+}: PopUpLabelProps) => {
+  const { t } = useTranslation(['me']);
+  return (
+    <div className={MyForestMapStyle.popUpLabel}>
+      {isConservation
+        ? t('me:conserved')
+        : isNormalTreeDonation
+        ? isRegisteredTree
+          ? t('me:registered')
+          : t('me:donated')
+        : null}
+    </div>
+  );
+};
+
+interface NumberOfContributionsProps {
+  isMoreThanOneContribution: boolean;
+  numberOfContributions: number;
+}
+
+export const NumberOfContributions = ({
+  isMoreThanOneContribution,
+  numberOfContributions,
+}: NumberOfContributionsProps) => {
+  const { t } = useTranslation(['me']);
+  return (
+    <div className={MyForestMapStyle.popUpDate}>
+      {isMoreThanOneContribution ? (
+        t('me:numberOfContributions', {
+          total: `${numberOfContributions}`,
+        })
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+interface DateInThePopUpProps {
+  isDate: boolean;
+  dateOfGift: Date | number;
+  dateOfDonation: Date | number;
+}
+export const DateInThePopUp = ({
+  isDate,
+  dateOfGift,
+  dateOfDonation,
+}: DateInThePopUpProps) => {
+  const { t } = useTranslation(['me']);
+  return (
+    <>
+      {isDate && (
+        <time className={MyForestMapStyle.popUpDate}>
+          {formatDate(dateOfDonation || dateOfGift)}
+        </time>
+      )}
+    </>
+  );
+};
+
+export const InfoInthePopUp = ({ geoJson }) => {
+  return (
+    <div className={MyForestMapStyle.popUpContainer}>
+      <div className={MyForestMapStyle.popUp}>
+        <PopUpLabel
+          isConservation={geoJson.properties?.purpose === 'conservation'}
+          isNormalTreeDonation={
+            geoJson.properties?.purpose === 'trees' ||
+            geoJson.properties?.purpose === null
+          }
+          isRegisteredTree={geoJson.properties.contributionType === 'planting'}
+        />
+        <NumberOfContributions
+          isMoreThanOneContribution={
+            geoJson.properties.totalContribution &&
+            geoJson.properties.totalContribution > 1
+          }
+          numberOfContributions={geoJson.properties.totalContribution}
+        />
+        <DateInThePopUp
+          isDate={
+            geoJson.properties.totalContribution < 2 ||
+            geoJson?.properties?.startDate ||
+            geoJson?.properties?.created
+          }
+          dateOfGift={geoJson?.properties?.created}
+          dateOfDonation={geoJson?.properties?.startDate}
+        />
+      </div>
+    </div>
+  );
+};
--- src/features/user/ProfileV2/components/MyForestMap/CustomPopupMarker.tsx
+++ src/features/user/ProfileV2/components/MyForestMap/CustomPopupMarker.tsx
@@ -1,12 +1,11 @@
 import { Popup } from 'react-map-gl';
-import { useTranslation } from 'next-i18next';
 import MyForestMapStyle from '../../styles/MyForestMap.module.scss';
 import { useUserProps } from '../../../../common/Layout/UserPropsContext';
 import { CustomPopupMarkerProps } from '../../../../common/types/map';
 import formatDate from '../../../../../utils/countryCurrency/getFormattedDate';
+import { InfoInthePopUp } from '../MicroComponents/PopUp';
 
 const CustomPopupMarker = ({ geoJson, showPopUp }: CustomPopupMarkerProps) => {
-  const { t } = useTranslation(['me']);
   const { isConservedButtonActive, isTreePlantedButtonActive } = useUserProps();
   return (
     <div className={MyForestMapStyle.singleMarkerContainer}>
@@ -20,37 +19,7 @@ const CustomPopupMarker = ({ geoJson, showPopUp }: CustomPopupMarkerProps) => {
           anchor="bottom"
           closeButton={false}
         >
-          <div className={MyForestMapStyle.popUpContainer}>
-            <div className={MyForestMapStyle.popUp}>
-              <div className={MyForestMapStyle.popUpLabel}>
-                {geoJson.properties?.purpose === 'conservation'
-                  ? t('me:conserved')
-                  : geoJson.properties?.purpose === 'trees' ||
-                    geoJson.properties?.purpose === null
-                  ? geoJson.properties.contributionType === 'planting'
-                    ? t('me:registered')
-                    : t('me:donated')
-                  : null}
-              </div>
-              <div className={MyForestMapStyle.popUpDate}>
-                {geoJson.properties.totalContribution &&
-                  geoJson.properties.totalContribution > 1 &&
-                  t('me:numberOfContributions', {
-                    total: `${geoJson.properties.totalContribution}`,
-                  })}
-              </div>
-              {(geoJson.properties.totalContribution < 2 ||
-                geoJson?.properties?.startDate ||
-                geoJson?.properties?.created) && (
-                <time className={MyForestMapStyle.popUpDate}>
-                  {formatDate(
-                    geoJson?.properties?.startDate ||
-                      geoJson?.properties?.created
-                  )}
-                </time>
-              )}
-            </div>
-          </div>
+          <InfoInthePopUp geoJson={geoJson} />
         </Popup>
       ) : (
         <></>
