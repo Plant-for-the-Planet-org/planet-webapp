e2b7b4ca920d5e28456e9689988e64a0ef183ae5
Shreyaschorge <shreyaschorge81@gmail.com>
2023-12-25 16:36:29 +0530

    Make /profile/donation-link page tenant aware

--- pages/profile/donation-link.tsx
+++ pages/profile/donation-link.tsx
@@ -1,51 +1,9 @@
-import React, { ReactElement } from 'react';
-import UserLayout from '../../src/features/common/Layout/UserLayout/UserLayout';
-import DonationLink from '../../src/features/user/Widget/DonationLink';
-import Head from 'next/head';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { GetStaticPropsContext } from 'next';
+// DONE
 
-export default function DonationLinkPage(): ReactElement {
-  const { t, ready } = useTranslation(['me']);
+export default function Donate() {
   return (
-    <UserLayout>
-      <Head>
-        <title>{ready ? t('donationLinkTitle') : ''}</title>
-      </Head>
-      <DonationLink />
-    </UserLayout>
+    <div style={{ margin: '400px 200px 200px 800px' }}>
+      Hello from /profile/donation-link
+    </div>
   );
-}
-
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  return {
-    props: {
-      ...(await serverSideTranslations(
-        locale || 'en',
-        [
-          'bulkCodes',
-          'common',
-          'country',
-          'donate',
-          'donationLink',
-          'editProfile',
-          'giftfunds',
-          'leaderboard',
-          'managePayouts',
-          'manageProjects',
-          'maps',
-          'me',
-          'planet',
-          'planetcash',
-          'redeem',
-          'registerTrees',
-          'tenants',
-          'treemapper',
-        ],
-        null,
-        ['en', 'de', 'fr', 'es', 'it', 'pt-BR', 'cs']
-      )),
-    },
-  };
 }
