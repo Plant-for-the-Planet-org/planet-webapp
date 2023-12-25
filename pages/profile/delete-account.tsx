155f755210ea873001170c8f27d7b2cc85af4efa
Shreyaschorge <shreyaschorge81@gmail.com>
2023-12-25 16:29:41 +0530

    Flag to be removed pages

--- pages/profile/delete-account.tsx
+++ pages/profile/delete-account.tsx
@@ -1,53 +1,9 @@
-import Head from 'next/head';
-import React, { ReactElement } from 'react';
-import UserLayout from '../../src/features/common/Layout/UserLayout/UserLayout';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import DeleteProfile from '../../src/features/user/Settings/DeleteProfile';
-import { GetStaticPropsContext } from 'next';
+// DONE
 
-function DeleteProfilePage(): ReactElement {
-  const { t } = useTranslation('me');
+export default function Donate() {
   return (
-    <UserLayout>
-      <Head>
-        <title>{t('deleteProfile')}</title>
-      </Head>
-      <DeleteProfile />
-    </UserLayout>
+    <div style={{ margin: '400px 200px 200px 800px' }}>
+      Hello from /profile/delete-account
+    </div>
   );
-}
-
-export default DeleteProfilePage;
-
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  return {
-    props: {
-      ...(await serverSideTranslations(
-        locale || 'en',
-        [
-          'bulkCodes',
-          'common',
-          'country',
-          'donate',
-          'donationLink',
-          'editProfile',
-          'giftfunds',
-          'leaderboard',
-          'managePayouts',
-          'manageProjects',
-          'maps',
-          'me',
-          'planet',
-          'planetcash',
-          'redeem',
-          'registerTrees',
-          'tenants',
-          'treemapper',
-        ],
-        null,
-        ['en', 'de', 'fr', 'es', 'it', 'pt-BR', 'cs']
-      )),
-    },
-  };
 }
--- pages/profile/history.tsx
+++ pages/profile/history.tsx
@@ -2,6 +2,6 @@
 
 export default function Donate() {
   return (
-    <div style={{ margin: '400px 200px 200px 800px' }}>Hello from /history</div>
+    <div style={{ margin: '400px 200px 200px 800px' }}>Hello from /profile/history</div>
   );
 }
