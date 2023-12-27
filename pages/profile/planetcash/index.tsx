759e1454924a4303a4853f05afd5b053799c1c4f
Shreyaschorge <shreyaschorge81@gmail.com>
2023-12-27 14:40:10 +0530

    Make /profile/planetcash/index.tsx page tenant aware

--- pages/profile/planetcash/index.tsx
+++ pages/profile/planetcash/index.tsx
@@ -1,70 +1,9 @@
-import React, { ReactElement, useState, useEffect } from 'react';
-import TopProgressBar from '../../../src/features/common/ContentLoaders/TopProgressBar';
-import UserLayout from '../../../src/features/common/Layout/UserLayout/UserLayout';
-import Head from 'next/head';
-import PlanetCash, {
-  PlanetCashTabs,
-} from '../../../src/features/user/PlanetCash';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { GetStaticPropsContext } from 'next';
+// DONE
 
-export default function PlanetCashPage(): ReactElement {
-  const { t, ready } = useTranslation('me');
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    // Cleanup function to reset state and address Warning: Can't perform a React state update on an unmounted component.
-    return () => {
-      setProgress(0);
-    };
-  }, []);
-
+export default function Donate() {
   return (
-    <>
-      {progress > 0 && (
-        <div className={'topLoader'}>
-          <TopProgressBar progress={progress} />
-        </div>
-      )}
-      <UserLayout>
-        <Head>
-          <title>{ready ? t('planetcash.titleAccount') : ''}</title>
-        </Head>
-        <PlanetCash step={PlanetCashTabs.ACCOUNTS} setProgress={setProgress} />
-      </UserLayout>
-    </>
+    <div style={{ margin: '400px 200px 200px 800px' }}>
+      Hello from /profile/planetcash/index.tsx
+    </div>
   );
-}
-
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  return {
-    props: {
-      ...(await serverSideTranslations(
-        locale || 'en',
-        [
-          'bulkCodes',
-          'common',
-          'country',
-          'donate',
-          'donationLink',
-          'editProfile',
-          'giftfunds',
-          'leaderboard',
-          'managePayouts',
-          'manageProjects',
-          'maps',
-          'me',
-          'planet',
-          'planetcash',
-          'redeem',
-          'registerTrees',
-          'tenants',
-          'treemapper',
-        ],
-        null,
-        ['en', 'de', 'fr', 'es', 'it', 'pt-BR', 'cs']
-      )),
-    },
-  };
 }
