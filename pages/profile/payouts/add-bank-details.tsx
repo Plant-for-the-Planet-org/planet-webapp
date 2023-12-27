fe29dfeca37f87eb528fb95c69c58d29930285de
Shreyaschorge <shreyaschorge81@gmail.com>
2023-12-27 13:59:55 +0530

    Make /profile/payouts/add-bank-details page tenant aware

--- pages/profile/payouts/add-bank-details.tsx
+++ pages/profile/payouts/add-bank-details.tsx
@@ -1,60 +1,9 @@
-import React, { ReactElement } from 'react';
-import UserLayout from '../../../src/features/common/Layout/UserLayout/UserLayout';
-import Head from 'next/head';
-import ManagePayouts, {
-  ManagePayoutTabs,
-} from '../../../src/features/user/ManagePayouts';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useUserProps } from '../../../src/features/common/Layout/UserPropsContext';
-import AccessDeniedLoader from '../../../src/features/common/ContentLoaders/Projects/AccessDeniedLoader';
-import { GetStaticPropsContext } from 'next';
+// DONE
 
-export default function AddBankDetailsPage(): ReactElement {
-  const { t, ready } = useTranslation('me');
-  const { user } = useUserProps();
+export default function Donate() {
   return (
-    <UserLayout>
-      <Head>
-        <title>{ready ? t('managePayouts.titleAddBankDetails') : ''}</title>
-      </Head>
-      {user?.type === 'tpo' ? (
-        <ManagePayouts step={ManagePayoutTabs.ADD_BANK_DETAILS} />
-      ) : (
-        <AccessDeniedLoader />
-      )}
-    </UserLayout>
+    <div style={{ margin: '400px 200px 200px 800px' }}>
+      Hello from /profile/payouts/add-bank-details
+    </div>
   );
-}
-
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  return {
-    props: {
-      ...(await serverSideTranslations(
-        locale || 'en',
-        [
-          'bulkCodes',
-          'common',
-          'country',
-          'donate',
-          'donationLink',
-          'editProfile',
-          'giftfunds',
-          'leaderboard',
-          'managePayouts',
-          'manageProjects',
-          'maps',
-          'me',
-          'planet',
-          'planetcash',
-          'redeem',
-          'registerTrees',
-          'tenants',
-          'treemapper',
-        ],
-        null,
-        ['en', 'de', 'fr', 'es', 'it', 'pt-BR', 'cs']
-      )),
-    },
-  };
 }
