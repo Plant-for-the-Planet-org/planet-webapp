7bb1c9c8a1410aa78d0b337e578ed77a7a469810
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-01-08 10:02:44 +0530

    fix: updates TreeCounter optional properties

--- src/tenants/salesforce/TreeCounter/TreeCounter.tsx
+++ src/tenants/salesforce/TreeCounter/TreeCounter.tsx
@@ -3,16 +3,28 @@ import treeCounterStyles from './TreeCounter.module.scss';
 
 interface Props {
   planted: number;
+  isLight?: boolean;
+  shouldShowMillions?: boolean;
 }
 
-export default function TpoProfile({ planted }: Props) {
+export default function TpoProfile({
+  planted,
+  isLight = false,
+  shouldShowMillions = false,
+}: Props) {
   const estMillionTreesPlanted = Math.floor(planted / 1000000);
-  const treeCounterValue = `${estMillionTreesPlanted} Million`;
-
+  const treeCounterValue = shouldShowMillions
+    ? `${estMillionTreesPlanted} Million`
+    : planted.toString();
   return (
     <div
       className={`${treeCounterStyles.treeCounter} ${
-        estMillionTreesPlanted > 0 ? treeCounterStyles.treeCounterReady : ''
+        isLight ? treeCounterStyles.treeCounterLight : ''
+      } ${
+        (shouldShowMillions && estMillionTreesPlanted > 0) ||
+        (!shouldShowMillions && planted > 0)
+          ? treeCounterStyles.treeCounterReady
+          : ''
       }`}
     >
       <div className={treeCounterStyles.treeCounterDataField}>
