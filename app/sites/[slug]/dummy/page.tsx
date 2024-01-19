f0ea1665ca99a729c0b062c9738687a5d8a55896
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-01-19 15:02:49 +0530

    Revert "feat: adds sample pages (app router)"
    This reverts commit 1db71f76a352a2b6487b402e6d8b612faea09763.

--- app/sites/[slug]/dummy/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DummyPage({ params }: { params: { slug: string } }) {
-  return <div>Dummy page: {params.slug}</div>;
-}
--- app/sites/[slug]/dummy/type1/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>Dummy type 1 page: {params.slug}</div>;
-}
