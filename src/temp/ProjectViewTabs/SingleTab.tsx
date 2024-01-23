3291f6869f7b3c4f96797590d32a173322e28d79
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-01-23 15:10:33 +0530

    refactor: add a common tab component

---
+++ src/temp/ProjectViewTabs/SingleTab.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import styles from './ProjectViewTabs.module.scss';
+
+const SingleTab = ({ icon, title, isSelected }) => {
+  return (
+    <>
+      <button
+        style={{
+          color: isSelected ? '#fff' : '#000',
+          backgroundColor: isSelected ? '#219653' : '#fff',
+          border: 'none',
+        }}
+        className={`${styles.option} ${styles.compact}`}
+      >
+        {icon}
+        <p>{title}</p>
+      </button>
+    </>
+  );
+};
+
+export default SingleTab;
