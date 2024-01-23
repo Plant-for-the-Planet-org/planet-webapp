3682e63e6f225538d4f1d22a58bda85e516299a7
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-01-23 15:11:15 +0530

    add story for single button

--- src/temp/stories/VegetationChange.stories.tsx
+++ src/temp/stories/VegetationChange.stories.tsx
@@ -1,24 +1,26 @@
-import Tabs from '../ProjectViewTabs/Tabs';
-export default {
-  component: Tabs,
-  title: 'VegetationChangeTabs',
+import SatelliteIcon from '../../../public/assets/images/icons/SatelliteIcon';
+import type { Meta, StoryObj } from '@storybook/react';
+import SingleTab from '../ProjectViewTabs/SingleTab';
+
+const meta: Meta<typeof SingleTab> = {
+  component: SingleTab,
 };
 
-const Template = (args) => <Tabs {...args} />;
+export default meta;
+type Story = StoryObj<typeof SingleTab>;
 
-export const FieldDataSelected = Template.bind({});
-
-FieldDataSelected.args = {
-  selectedMode: 'field',
+export const TimeTravelSelected: Story = {
+  args: {
+    icon: <SatelliteIcon color={'#fff'} />,
+    title: 'Time Travel',
+    isSelected: true,
+  },
 };
 
-export const SatelliteAnalysisSelected = Template.bind({});
-
-SatelliteAnalysisSelected.args = {
-  selectedMode: 'satellite',
-};
-export const TimeTravelSelected = Template.bind({});
-
-TimeTravelSelected.args = {
-  selectedMode: 'timeTravel',
+export const TimeTravelUnSelected: Story = {
+  args: {
+    icon: <SatelliteIcon color={'#000'} />,
+    title: 'Time Travel',
+    isSelected: false,
+  },
 };
