d8906220d7e7249e277c29e2a2620e0e605d9dea
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-02-06 14:35:21 +0530

    add report section

---
+++ src/temp/ReviewReports/ProjectReview.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styles from './ReviewReports.module.scss';
+import DownloadReportIcon from '../icons/DownloadReportIcon';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import { getPDFFile } from '../../utils/getImageURL';
+import parse from 'date-fns/parse';
+import format from 'date-fns/format';
+import { localeMapForDate } from '../../utils/language/getLanguageName';
+
+interface Props {
+  pdf: string;
+  issueMonth: string;
+}
+
+const ProjectReview = ({ issueMonth, pdf }: Props) => {
+  const displayDate = (date: string) => {
+    return format(parse(date, 'MM-yyyy', new Date()), 'LLLL yyyy', {
+      locale: localeMapForDate[localStorage.getItem('language') || 'en'],
+    });
+  };
+  return (
+    <div className={styles.reviewReportsContainer}>
+      <div className={styles.titleContainer}>
+        <h6>Review</h6>
+        <div className={styles.downloadReportContainer}>
+          {' '}
+          <p>Report</p>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={getPDFFile('projectReview', pdf)}
+          >
+            <DownloadReportIcon />
+          </a>
+        </div>
+      </div>
+      <div className={styles.reviewInfoContainer}>
+        <div className={styles.verifiedIcon}>
+          <VerifiedIcon sx={{ color: '#42A5F5', width: 16, height: 16 }} />
+        </div>
+        <p>
+          The project was inspected in a multiday field review in{' '}
+          {displayDate(issueMonth)} and fullfills our{' '}
+          <a
+            target="_blank"
+            href="https://www.plant-for-the-planet.org/standards/"
+            rel="noreferrer"
+            style={{ fontWeight: 400 }}
+          >
+            standards.
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectReview;
