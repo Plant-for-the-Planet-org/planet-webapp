0617c83a24876d9c6c866bac2ff25f04d94b7fa3
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-02-06 14:28:26 +0530

    add review tab

---
+++ src/temp/ReviewReports/Reports.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ProjectReview from './ProjectReview';
+
+const meta: Meta<typeof ProjectReview> = {
+  component: ProjectReview,
+};
+
+export default meta;
+type Story = StoryObj<typeof ProjectReview>;
+
+export const Preview: Story = {
+  args: {
+    pdf: '6453b5a357425095001115.pdf',
+    issueMonth: '11-2021',
+  },
+};
