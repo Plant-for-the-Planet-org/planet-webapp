24a4fcd041b6869e2f3f78e99e6824c5033d406f
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-02-28 18:36:58 +0530

    add component

---
+++ src/temp/TimeTravelDropdown/TimeTravelDropdown.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import styles from './Dropdown.module.scss';
+import CalendarIcon from '../icons/CalendarIcon';
+import DropdownArrow from '../icons/DropdownArrow';
+
+interface TimeTravelDropdownProps {
+  labelYear: string;
+  labelSource: string;
+  yearList: string[];
+  sourceList: string[];
+  isOpen: boolean;
+}
+
+const TimeTravelDropdown = ({
+  labelYear,
+  labelSource,
+  yearList,
+  sourceList,
+  isOpen,
+}: TimeTravelDropdownProps) => {
+  const [selectedYear, setSelectedYear] = useState(labelYear);
+  const [selectedSource, setSelectedSource] = useState(labelSource);
+  const [isMenuOpen, setIsMenuOpen] = useState(isOpen);
+  const handleChangeYear = (year: string) => {
+    setSelectedYear(year);
+  };
+
+  const handleChangeSource = (source: string) => {
+    setSelectedSource(source);
+  };
+
+  const styleForSelectedOption = (
+    selectedOption: string,
+    labelValue: string
+  ) => {
+    if (selectedOption.toLowerCase() === labelValue.toLowerCase()) {
+      return {
+        fontWeight: 700,
+        color: '#333',
+      };
+    } else {
+      return undefined;
+    }
+  };
+
+  return (
+    <div className={styles.menuContainer}>
+      <div
+        className={styles.menuButton}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <div className={styles.menuButtonTitle}>
+          <CalendarIcon width={14} color={'#2F3336'} />
+          <p>
+            <span>{selectedYear} </span>via {selectedSource}
+          </p>
+        </div>
+        <DropdownArrow width={8} color={'#2F3336'} />
+      </div>
+      {isMenuOpen && (
+        <div className={styles.menuItems}>
+          <ul className={styles.yearMenuContainer}>
+            {yearList?.map((year, index) => (
+              <time
+                key={index}
+                onClick={() => handleChangeYear(year)}
+                style={styleForSelectedOption(year, selectedYear)}
+              >
+                {year}
+              </time>
+            ))}
+          </ul>
+          <ul className={styles.sourceMenuContainer}>
+            {sourceList?.map((source, index) => (
+              <li
+                key={index}
+                onClick={() => handleChangeSource(source)}
+                style={styleForSelectedOption(source, selectedSource)}
+              >
+                {source}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TimeTravelDropdown;
