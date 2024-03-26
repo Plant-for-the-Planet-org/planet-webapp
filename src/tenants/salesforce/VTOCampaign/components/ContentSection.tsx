e77c5294d26e9a6f7b9b7407b402205f59b2e4c7
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-03-26 11:00:42 +0530

    feat: update page content for vto campaign 2024

--- src/tenants/salesforce/VTOCampaign/components/ContentSection.tsx
+++ src/tenants/salesforce/VTOCampaign/components/ContentSection.tsx
@@ -10,13 +10,16 @@ export default function ContentSection() {
         >
           <div className={`${gridStyles.colMd8} ${gridStyles.col12}`}>
             <h2>For Salesforce Employees</h2>
-            <h2>VTO Fitness-for-a-Cause Challenge, Sponsored by Earthforce</h2>
+            <h2>
+              VTO Fitness-for-a-Cause Challenge, Sponsored by Earthforce and
+              Abilityforce
+            </h2>
             <p className={styles.contentSectionSubhead}>
               April is Earthforce Champion Month, and we are inviting Salesforce
-              colleagues around the world to log their minutes of humanpowered
+              colleagues around the world to log their minutes of human-powered
               movement and exercise to champion Salesforce’s commitment to
               support and mobilize the conservation, restoration, and growth of
-              100 Million trees as a part of the{' '}
+              5 Million trees this fiscal year as a part of the{' '}
               <a href="https://www.1t.org/" target="_blank" rel="noreferrer">
                 1t.org
               </a>{' '}
@@ -42,16 +45,13 @@ export default function ContentSection() {
             <h3>How you can participate</h3>
             <p>
               All Salesforce employees globally are eligible to participate in
-              this challenge.
+              this inclusive challenge.
             </p>
             <p>
-              To join, donate $50 or more to a tree planting project of your
-              choice from the projects listed below. You will then receive a
-              donation confirmation email from Plant-for-the-Planet.
-            </p>
-            <p>
-              Next, request your Salesforce Donation Match in Volunteerforce and
-              upload your confirmation from{' '}
+              To join, donate $50 (USD) or more to a tree planting project of
+              your choice from the projects listed below on the page. Once your
+              donation is completed, you will receive a donation confirmation
+              email from{' '}
               <a
                 href="https://www.plant-for-the-planet.org/"
                 target="_blank"
@@ -59,19 +59,32 @@ export default function ContentSection() {
               >
                 Plant-for-the-Planet
               </a>
-              . Once your donation and match request are verified, visit our
-              slack channel #vto-fitness-moving-toward-100m-treesapr23 to log
-              your exercise minutes from April 1st - 30th via the slack
-              workflow, and share your thoughts, insights and workouts!
+              .
+            </p>
+            <p>
+              Next, request your Salesforce Donation Match in the Employee
+              Impact Hub and upload your receipt from Plant-for-the-Planet to
+              request your match. Email screenshots of your donation AND match
+              request to{' '}
+              <a href="mailto:vto.fitness@gmail.com">vto.fitness@gmail.com</a>{' '}
+              for verification. Once your donation and match request are
+              verified, you will be added to the slack channel
+              #vto-fitness-movetowards5miltrees-april24 to log your exercise
+              minutes from April 1st - 30th via the slack workflow.
             </p>
             <p>
               Participants in this challenge are able to earn up to one hour of
-              VTO per day in April (30 hours max).
+              VTO per day in April (30 hours max) by participating in this
+              fitness-for-a-cause challenge. For example, if you completed 30
+              minutes of exercise, you can log 30 minutes of VTO. If you
+              completed 2 hours of exercise, you can log 1 hour of VTO for that
+              day.
             </p>
             <p>
               Thank you for your interest and support. Continue below to choose
               a tree planting project that resonates most with you and make your
-              impact. Let’s get moving towards a healthier and greener tomorrow!
+              impact. Let’s get moving towards a healthier, cleaner, and greener
+              tomorrow!
             </p>
           </div>
         </div>
