c99b403b771f41545be42a649889bab6eac49367
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-04-15 18:29:40 +0530

    refactor: work on feedback

--- src/temp/ProjectBadge/ProjectBadge.tsx
+++ src/temp/ProjectBadge/ProjectBadge.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import NewInfoIcon from '../icons/NewInfoIcon';
 import OffSiteReviewedIcon from '../icons/OffSiteReviewedIcon';
 import FieldReviewedIcon from '../icons/FieldReviewedIcon';
@@ -12,43 +12,55 @@ interface Props {
   allowDonations: boolean;
 }
 
+interface TitleAndIconReturnType {
+  icon: ReactElement;
+  title: string;
+}
+
 const ProjectBadge = ({ isApproved, isTopProject, allowDonations }: Props) => {
   const { t } = useTranslation(['common', 'projectDetails']);
+
   const getTitleAndIcon = (
     isApproved: boolean,
     isTopProject: boolean,
     allowDonations: boolean
-  ) => {
-    if (!allowDonations)
-      return {
-        icon: <NewInfoIcon width={10} height={10} color={'var(--light)'} />,
-        title: t('common:notDonatable'),
-      };
+  ): TitleAndIconReturnType | undefined => {
+    return useMemo(() => {
+      if (!allowDonations) {
+        return {
+          icon: <NewInfoIcon width={10} height={10} color={'var(--light)'} />,
+          title: t('common:notDonatable'),
+        };
+      }
 
-    if (isTopProject && isApproved)
-      return {
-        icon: <TopProjectIcon color={'var(--light)'} width={11} />,
-        title: t('common:topProject'),
-      };
-    else if (isTopProject && !isApproved)
-      return {
-        icon: <FieldReviewedIcon width={10} color={'var(--light)'} />,
-        title: t('projectDetails:fieldReviewed'),
-      };
-    else if (!isTopProject && !isApproved)
-      return {
-        icon: <OffSiteReviewedIcon width={10} color={'var(--light)'} />,
-        title: t('projectDetails:offSiteReviewed'),
-      };
+      if (isTopProject && isApproved) {
+        return {
+          icon: <TopProjectIcon color={'var(--light)'} width={11} />,
+          title: t('common:topProject'),
+        };
+      }
+
+      if (isTopProject && !isApproved) {
+        return {
+          icon: <FieldReviewedIcon width={10} color={'var(--light)'} />,
+          title: t('projectDetails:fieldReviewed'),
+        };
+      }
+      if (!isTopProject && !isApproved) {
+        return {
+          icon: <OffSiteReviewedIcon width={10} color={'var(--light)'} />,
+          title: t('projectDetails:offSiteReviewed'),
+        };
+      }
+    }, [isApproved, isTopProject, allowDonations, t]);
   };
+
+  const { icon, title } =
+    getTitleAndIcon(isApproved, isTopProject, allowDonations) || {};
   return (
-    <div className={styles.topProjectBadge}>
-      <div className={styles.badgeIcon}>
-        {getTitleAndIcon(isApproved, isTopProject, allowDonations)?.icon}
-      </div>
-      <div className={styles.badgeTitle}>
-        {getTitleAndIcon(isApproved, isTopProject, allowDonations)?.title}
-      </div>
+    <div className={styles.projectBadge}>
+      <div className={styles.badgeIcon}>{icon}</div>
+      <div className={styles.badgeTitle}>{title}</div>
     </div>
   );
 };
