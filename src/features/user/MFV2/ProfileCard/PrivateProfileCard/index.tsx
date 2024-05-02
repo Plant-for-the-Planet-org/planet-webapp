60ce419f5bf08a1b12dfba3b234db2bbbbbe6aa7
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-05-02 20:31:41 +0530

    feat: update style for mobile design

--- src/features/user/MFV2/ProfileCard/PrivateProfileCard/index.tsx
+++ src/features/user/MFV2/ProfileCard/PrivateProfileCard/index.tsx
@@ -5,11 +5,17 @@ import getImageUrl from '../../../../../utils/getImageURL';
 import { DefaultUserProfileImage } from '../../../../../../public/assets/images/ProfilePageIcons';
 import styles from './PrivateProfileCard.module.scss';
 import { SettingsIcon } from '../../../../../../public/assets/images/icons/ProfilePageV2Icons';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import ProfileCardButtonContainer from '../ProfileCardButton/ProfileCardButtonContainer';
+import { useRouter } from 'next/router';
 
 const PrivateProfileCard = ({ userProfile }: ProfileProps) => {
   const t = useTranslations('Profile');
+  const screenWidth = window.innerWidth;
+  const isMobile = screenWidth <= 481;
+  const router = useRouter();
+  const locale = useLocale();
+  const isPublicAccount = router.asPath === `/${locale}/profile/mfv2-public`;
 
   return (
     <div className={styles.profileCardContainer}>
@@ -26,9 +32,11 @@ const PrivateProfileCard = ({ userProfile }: ProfileProps) => {
         )}
       </div>
       <div className={styles.profileDetailsContainer}>
-        <button className={styles.editProfileIcon}>
-          <SettingsIcon />
-        </button>
+        {!isPublicAccount && (
+          <button className={styles.editProfileIcon}>
+            <SettingsIcon />
+          </button>
+        )}
         <div className={styles.profileInfo}>
           <h2>{userProfile?.displayName}</h2>
           <p>
@@ -38,7 +46,13 @@ const PrivateProfileCard = ({ userProfile }: ProfileProps) => {
             {userProfile?.bio}
           </p>
         </div>
-        <ProfileCardButtonContainer isPrivate={true} />
+
+        {isPublicAccount && isMobile ? (
+          // renders public account features for mobile screen
+          <ProfileCardButtonContainer isPrivate={false} />
+        ) : (
+          <ProfileCardButtonContainer isPrivate={true} />
+        )}
       </div>
     </div>
   );
--- src/features/user/MFV2/PublicProfileLayout/index.tsx
+++ src/features/user/MFV2/PublicProfileLayout/index.tsx
@@ -4,12 +4,15 @@ import { useState, useEffect } from 'react';
 import { useUserProps } from '../../../common/Layout/UserPropsContext';
 import PublicProfileCard from '../ProfileCard/PublicProfileCard';
 import styles from './PublicProfileLayout.module.scss';
+import PrivateProfileCard from '../ProfileCard/PrivateProfileCard';
 
 // We may choose to accept the components for each section as props depending on how we choose to pass data. In that case, we would need to add an interface to accept the components as props.
 const PublicProfileLayout = () => {
   const router = useRouter();
   const { user, contextLoaded } = useUserProps();
   const [profile, setProfile] = useState<null | User>();
+  const screenWidth = window.innerWidth;
+  const isMobile = screenWidth <= 481;
 
   useEffect(() => {
     if (contextLoaded) {
@@ -22,7 +25,11 @@ const PublicProfileLayout = () => {
   return (
     <article className={styles.publicProfileLayout}>
       <section id="profile-container" className={styles.profileContainer}>
-        <PublicProfileCard userProfile={profile} />
+        {isMobile ? (
+          <PrivateProfileCard userProfile={profile} />
+        ) : (
+          <PublicProfileCard userProfile={profile} />
+        )}
       </section>
       <section id="map-container" className={styles.mapContainer}>
         Map
