8ad06f5e046b0aa10616f9eade7a2e38e437bb50
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2024-05-02 16:40:06 +0530

    single marker layout

--- src/features/user/MFV2/ContributionsMap/index.tsx
+++ src/features/user/MFV2/ContributionsMap/index.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react';
 import Map, { MapStyle } from 'react-map-gl-v7/maplibre';
 import getMapStyle from '../../../../utils/maps/getMapStyle';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { NavigationControl } from 'react-map-gl-v7';
+import { Marker, NavigationControl } from 'react-map-gl-v7';
 import MyForestMapCredit from '../../Profile/components/MyForestMap/microComponents/MyForestMapCredit';
+import RegisteredTreeIcon from '../../../../../public/assets/images/icons/myForestV2Icons/RegisteredTreeIcon';
+import style from './contributionMap.module.scss';
 
 interface MapState {
   mapStyle: MapStyle;
@@ -20,6 +22,50 @@ const EMPTY_STYLE = {
 } as const;
 
 function ContributionsMap() {
+  const contributionLocation = [
+    {
+      geometry: {
+        coordinates: [73.09064459228588, 19.19969904795899],
+        type: 'Point',
+      },
+      properties: {
+        contributionType: 'registered',
+        _type: 'contribution',
+        startDate: 'Thu May 02 2024 05:30:00 GMT+0530 (India Standard Time)',
+        quantity: '1',
+        projectStatus: 'a',
+        projectType: 'treePlantation',
+      },
+    },
+    {
+      geometry: {
+        coordinates: [56.09064459228588, 23.19969904795899],
+        type: 'Point',
+      },
+      properties: {
+        contributionType: 'registered',
+        _type: 'contribution',
+        startDate: 'Thu May 02 2024 05:30:00 GMT+0530 (India Standard Time)',
+        quantity: '1',
+        projectStatus: 'b',
+        projectType: 'conservation',
+      },
+    },
+    {
+      geometry: {
+        coordinates: [24.09064459228588, 45.19969904795899],
+        type: 'Point',
+      },
+      properties: {
+        contributionType: 'registered',
+        _type: 'contribution',
+        startDate: 'Thu May 02 2024 05:30:00 GMT+0530 (India Standard Time)',
+        quantity: '1',
+        projectStatus: 'c',
+        projectType: 'restoration',
+      },
+    },
+  ];
   // mapState and viewState logic will need to be refined and move elsewhere (either context or props) once we fetch data from the API
   const [mapState, setMapState] = useState<MapState>({
     mapStyle: EMPTY_STYLE,
@@ -35,6 +81,30 @@ function ContributionsMap() {
     zoom: 1,
   });
 
+  const renderIcons = ({ projectType }: any) => {
+    if (projectType === 'restoration') {
+      return (
+        <div
+          className={`${style.markerIconContainerR} ${style.markerIconContainer}`}
+        >
+          {/* {can include switch statment to render different icons} */}
+          <RegisteredTreeIcon />
+        </div>
+      );
+    } else if (projectType === 'conservation') {
+      return (
+        <div className={`${style.conservation} ${style.markerIconContainer}`}>
+          <RegisteredTreeIcon />
+        </div>
+      );
+    } else {
+      return (
+        <div className={`${style.treePlantation} ${style.markerIconContainer}`}>
+          <RegisteredTreeIcon />
+        </div>
+      );
+    }
+  };
   useEffect(() => {
     async function loadMapStyle() {
       const result = await getMapStyle('myForestMap');
@@ -52,6 +122,17 @@ function ContributionsMap() {
       onMove={(e) => setViewState(e.viewState)}
       attributionControl={false}
     >
+      {contributionLocation.map((singleLocation, key) => {
+        return (
+          <Marker
+            key={key}
+            longitude={singleLocation?.geometry.coordinates[0]}
+            latitude={singleLocation?.geometry.coordinates[1]}
+          >
+            {renderIcons(singleLocation.properties)}
+          </Marker>
+        );
+      })}
       <MyForestMapCredit />
       <NavigationControl position="bottom-right" showCompass={false} />
     </Map>
