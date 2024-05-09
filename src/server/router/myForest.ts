9e3ad9eed6cba66228c70460243a08048c4cf98f
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-05-09 15:49:22 +0530

    feat: adds new myForestv2 api route & projectList procedure
    - returns a dictionary of Projects with guid as index

--- src/server/router/myForest.ts
+++ src/server/router/myForest.ts
@@ -3,9 +3,14 @@ import { router } from '../trpc';
 import { contributions } from '../procedures/myForest/contributions';
 import { stats } from '../procedures/myForest/stats';
 import { contributionsGeoJson } from '../procedures/myForest/contributionsGeoJson';
+import { projectListsProcedure } from '../procedures/myForestV2/projectList';
 
 export const myForestRouter = router({
   contributions: contributions,
   stats: stats,
   contributionsGeoJson: contributionsGeoJson,
+});
+
+export const myForestV2Router = router({
+  projectList: projectListsProcedure,
 });
