65c97921503bc45433f09865c6e71f274e54bc1d
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2024-05-27 20:15:36 +0530

    replace the registered tree marker

--- src/features/user/MFV2/ContributionsMap/Markers/SinglePointMarkers.tsx
+++ src/features/user/MFV2/ContributionsMap/Markers/SinglePointMarkers.tsx
@@ -6,11 +6,12 @@ import Mangroves from '../../../../../../public/assets/images/icons/myForestV2Ic
 import ManagedRegeneration from '../../../../../../public/assets/images/icons/myForestV2Icons/ManagedRegeneration';
 import Agroforestry from '../../../../../../public/assets/images/icons/myForestV2Icons/Agroforestry';
 import UrbanRestoration from '../../../../../../public/assets/images/icons/myForestV2Icons/UrbanRestoration';
-import Conservation from '../../../../../../public/assets/images/icons/myForestV2Icons/Conservation';
 import TreePlanting from '../../../../../../public/assets/images/icons/myForestV2Icons/TreePlanting';
 import OtherPlanting from '../../../../../../public/assets/images/icons/myForestV2Icons/OtherPlanting';
 import themeProperties from '../../../../../theme/themeProperties';
 import { useMyForestV2 } from '../../../../common/Layout/MyForestContextV2';
+import style from '.././Common/common.module.scss';
+import { UnitTypes } from '@planet-sdk/common';
 
 type Classification =
   | 'natural-regeneration'
@@ -18,26 +19,35 @@ type Classification =
   | 'managed-regeneration'
   | 'agroforestry'
   | 'urban-planting'
-  | 'conservation'
   | 'large-scale-planting'
   | 'other-planting';
 interface ProjectTypeIconProps {
   purpose: 'conservation' | 'restoration' | 'trees';
   classification: Classification | null;
+  unitType: UnitTypes;
 }
 
-const ProjectTypeIcon = ({ purpose, classification }: ProjectTypeIconProps) => {
-  const getMarkerColor = (purpose: string) => {
+const ProjectTypeIcon = ({
+  purpose,
+  classification,
+  unitType,
+}: ProjectTypeIconProps) => {
+  const getMarkerColor = (purpose: string, unitType: UnitTypes) => {
     switch (purpose) {
       case 'conservation':
-        return `${themeProperties.mediumBlue}`;
-      case 'restoration':
-        return `${themeProperties.electricPurple}`;
+        return themeProperties.mediumBlue;
+      case 'trees':
+        return unitType === 'm2'
+          ? themeProperties.electricPurple
+          : themeProperties.primaryDarkColorX;
       default:
-        return `${themeProperties.primaryDarkColorX}`;
+        return themeProperties.primaryDarkColorX;
     }
   };
-  const markercolor = useMemo(() => getMarkerColor(purpose), [purpose]);
+  const markercolor = useMemo(
+    () => getMarkerColor(purpose, unitType),
+    [purpose, unitType]
+  );
   const IconProps = {
     width: 42,
     color: markercolor,
@@ -53,8 +63,6 @@ const ProjectTypeIcon = ({ purpose, classification }: ProjectTypeIconProps) => {
       return <Agroforestry {...IconProps} />;
     case 'urban-planting':
       return <UrbanRestoration {...IconProps} />;
-    case 'conservation':
-      return <Conservation {...IconProps} />;
     case 'large-scale-planting':
       return <TreePlanting {...IconProps} />;
     case 'other-planting':
@@ -77,7 +85,9 @@ const SinglePointMarkers = () => {
               offset={[0, -15]}
               key={key}
             >
-              <RegisteredTreeIcon width={42} />
+              <div className={style.registeredTreeMarkerContainer}>
+                <RegisteredTreeIcon />
+              </div>
             </Marker>
           );
         } else {
@@ -97,6 +107,7 @@ const SinglePointMarkers = () => {
               classification={
                 singleLocation.properties.projectInfo.classification
               }
+              unitType={singleLocation.properties.projectInfo.unitType}
             />
           </Marker>
         );
