27e711c3f1cc418a949757e32693a96f3b9b47a3
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-05-30 16:57:40 +0530

    feat: add `hide/show` logic

--- src/features/user/MFV2/PublicProfileLayout/index.tsx
+++ src/features/user/MFV2/PublicProfileLayout/index.tsx
@@ -4,13 +4,13 @@ import { UserPublicProfile } from '@planet-sdk/common';
 import { ProfileLoader } from '../../../common/ContentLoaders/ProfileV2';
 import ContributionsMap from '../ContributionsMap';
 import CommunityContributions from '../CommunityContributions';
-
 interface Props {
   profile: UserPublicProfile;
 }
 
 // We may choose to accept the components for each section as props depending on how we choose to pass data. In that case, we would need to add an interface to accept the components as props.
 const PublicProfileLayout = ({ profile }: Props) => {
+  const showLeaderboard = true;
   return (
     <article className={styles.publicProfileLayout}>
       <section id="profile-container" className={styles.profileContainer}>
@@ -29,19 +29,22 @@ const PublicProfileLayout = ({ profile }: Props) => {
       <section
         id="my-contributions-container"
         className={styles.myContributionsContainer}
-      >
-        My Contributions
-      </section>
-      <section
-        id="community-contributions-container"
-        className={styles.communityContributionsContainer}
-      >
-        {profile ? (
-          <CommunityContributions userProfile={profile} profileType="public" />
-        ) : (
-          <ProfileLoader />
-        )}
-      </section>
+      ></section>
+      {showLeaderboard ? (
+        <section
+          id="community-contributions-container"
+          className={styles.communityContributionsContainer}
+        >
+          {profile ? (
+            <CommunityContributions
+              userProfile={profile}
+              profileType="public"
+            />
+          ) : (
+            <ProfileLoader />
+          )}
+        </section>
+      ) : null}
       <section id="info-cta-container" className={styles.infoAndCtaContainer}>
         Additional information and CTAs - Become a member, Treegame, SDG Slider
       </section>
