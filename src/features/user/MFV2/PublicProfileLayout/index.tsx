6d11f1be76afc5e60ad0382e8993dc00ee4d2e43
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2024-05-31 09:45:44 +0530

    public profile map

--- src/features/user/MFV2/PublicProfileLayout/index.tsx
+++ src/features/user/MFV2/PublicProfileLayout/index.tsx
@@ -2,6 +2,7 @@ import styles from './PublicProfileLayout.module.scss';
 import ProfileCard from '../ProfileCard';
 import { UserPublicProfile } from '@planet-sdk/common';
 import { ProfileLoader } from '../../../common/ContentLoaders/ProfileV2';
+import ContributionsMap from '../ContributionsMap';
 
 interface Props {
   profile: UserPublicProfile;
@@ -19,7 +20,7 @@ const PublicProfileLayout = ({ profile }: Props) => {
         )}
       </section>
       <section id="map-container" className={styles.mapContainer}>
-        Map
+        <ContributionsMap />
       </section>
       <section id="progress-container" className={styles.progressContainer}>
         Progress
