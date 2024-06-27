89c59635520d44a68cf3c350d96c501650e19933
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-06-27 17:41:33 +0530

    refactor: remove class from label in webapp button

--- src/features/common/WebappButton/index.tsx
+++ src/features/common/WebappButton/index.tsx
@@ -38,11 +38,7 @@ function WebappButton({
           {otherProps.icon !== undefined && (
             <div className={styles.webappButtonIcon}>{otherProps.icon}</div>
           )}
-          <div
-            className={`${styles.webappButtonLabel} ${otherProps.buttonClasses}`}
-          >
-            {otherProps.text}
-          </div>
+          <div className={styles.webappButtonLabel}>{otherProps.text}</div>
         </button>
       </Link>
     );
