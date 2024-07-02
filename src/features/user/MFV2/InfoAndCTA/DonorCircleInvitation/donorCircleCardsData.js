022b133d2448875f4e46be8816b0bde9196610d7
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-07-02 18:15:26 +0530

    styles: improve styles

--- src/features/user/MFV2/InfoAndCTA/DonorCircleInvitation/donorCircleCardsData.js
+++ src/features/user/MFV2/InfoAndCTA/DonorCircleInvitation/donorCircleCardsData.js
@@ -1,26 +1,26 @@
 const donorCircleCardsData = [
   {
     link: 'https://www.plant-for-the-planet.org/children-youth/',
-    image: '/assets/images/donorCircleInvitationImages/youthEmpowerment.svg',
+    image: '/assets/images/donorCircleInvitationImages/youthEmpowerment.png',
     title: 'Youth Empowerment',
     subtext: 'children & youth to fight the climate crisis',
   },
   {
     link: 'https://www.plant-for-the-planet.org/restoration-organizations/',
-    image: '/assets/images/donorCircleInvitationImages/restorationTools.svg',
+    image: '/assets/images/donorCircleInvitationImages/restorationTools.png',
     title: 'Restoration Tools   ',
     subtext: 'to fund, restore, monitor & conserve forests',
   },
   {
     link: 'https://www.plant-for-the-planet.org/advice/',
-    image: '/assets/images/donorCircleInvitationImages/restorationAdvice.svg',
+    image: '/assets/images/donorCircleInvitationImages/restorationAdvice.png',
     title: 'Restoration Advice',
     subtext: '& Research',
   },
   {
     link: 'https://plant-for-the-planet.org/yucatan/',
     image:
-      '/assets/images/donorCircleInvitationImages/forestRestorationAndConservation.svg',
+      '/assets/images/donorCircleInvitationImages/forestRestorationAndConservation.png',
     title: 'Forest Restoration & Conservation',
     subtext: 'in Mexico, Ghana & Spain',
   },
