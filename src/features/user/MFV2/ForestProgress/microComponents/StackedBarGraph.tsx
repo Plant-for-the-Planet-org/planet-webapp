ad85993d3ed3208031dba641ad0babdfef9f54d8
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-07-02 16:20:18 +0530

    feat: reafactors totalAchievedPercentage calc. allowing it to be greater than 100

--- src/features/user/MFV2/ForestProgress/microComponents/StackedBarGraph.tsx
+++ src/features/user/MFV2/ForestProgress/microComponents/StackedBarGraph.tsx
@@ -1,44 +1,45 @@
 import styles from '../ForestProgress.module.scss';
 import { ProgressDataProps } from './ProgressData';
 
-export type StackedBarGraphProps = Omit<
-  ProgressDataProps,
-  'dataType' | 'gift' | 'personal'
->;
+export type StackedBarGraphProps = Omit<ProgressDataProps, 'dataType'>;
 
 const StackedBarGraph = ({
   personalSegmentPercentage,
   giftSegmentPercentage,
+  gift,
+  personal,
   target,
 }: StackedBarGraphProps) => {
-  const totalAchievedPercentage = giftPercentage + personalPercentage;
+  const calculateTotalPercentage = () => {
+    if (target <= 0) return null;
 
-  const isInteger = Number.isInteger(totalAchievedPercentage);
+    const totalPercentage = ((gift + personal) / target) * 100;
+    return Number.isInteger(totalPercentage)
+      ? totalPercentage
+      : Number(totalPercentage.toFixed(1));
+  };
+
+  const totalAchievedPercentage = calculateTotalPercentage();
 
   return (
     <div className={styles.graphMainContainer}>
       <div className={styles.barContainer}>
         <div
           style={{
-            width: `${personalPercentage}%`,
+            width: `${personalSegmentPercentage}%`,
           }}
           className={styles.personalPercentageBar}
         ></div>
         <div
           style={{
-            width: `${giftPercentage}%`,
+            width: `${giftSegmentPercentage}%`,
           }}
           className={styles.giftPercentageBar}
         ></div>
       </div>
-      <div>
-        {target > 0 &&
-          `${
-            isInteger
-              ? totalAchievedPercentage
-              : totalAchievedPercentage.toFixed(1)
-          }%`}
-      </div>
+      {totalAchievedPercentage !== null && (
+        <div>{`${totalAchievedPercentage}%`}</div>
+      )}
     </div>
   );
 };
