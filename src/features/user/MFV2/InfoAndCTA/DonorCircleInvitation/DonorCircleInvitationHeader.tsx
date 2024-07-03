f2b1bec75b179285f9ab4b8af47333ec06ba81cc
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2024-07-03 13:21:28 +0530

    feat: add translations

--- src/features/user/MFV2/InfoAndCTA/DonorCircleInvitation/DonorCircleInvitationHeader.tsx
+++ src/features/user/MFV2/InfoAndCTA/DonorCircleInvitation/DonorCircleInvitationHeader.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import styles from '../InfoAndCta.module.scss';
 import WebappButton from '../../../../common/WebappButton';
+import { useTranslations } from 'next-intl';
 
 const DonorCircleInvitationHeader = () => {
+  const t = useTranslations('Profile');
   return (
     <div className={styles.donorCircleInvitationHeader}>
       <h2 className={styles.headerTitle}>
-        Plant-for-the-Planet donor circle member
+        {t('infoAndCtaContainer.pfpDonorCircleMember')}
       </h2>
       <WebappButton
         variant="tertiary"
         href="https://www.plant-for-the-planet.org/donor-circle/"
         elementType="link"
         target="_blank"
-        text="Become a member"
+        text={t('infoAndCtaContainer.becomeAMember')}
         buttonClasses={styles.customJoinDonorCircleButton}
       />
     </div>
--- src/features/user/MFV2/InfoAndCTA/DonorCircleInvitation/index.tsx
+++ src/features/user/MFV2/InfoAndCTA/DonorCircleInvitation/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import styles from '../InfoAndCta.module.scss';
 import DonorCircleInvitationHeader from './DonorCircleInvitationHeader';
-import donorCircleCardsData from './donorCircleCardsData';
 import SingleInvitationCard from './SingleInvitationCard';
+import { useTranslations } from 'next-intl';
 
 const DonorCircleInvitation = () => {
+  const t = useTranslations('Profile');
+
+  const donorCircleCardsData = [
+    {
+      link: 'https://www.plant-for-the-planet.org/children-youth/',
+      image: '/assets/images/donorCircleInvitationImages/youthEmpowerment.png',
+      title: t('infoAndCtaContainer.youthEmpowermentTitle'),
+      subtext: t('infoAndCtaContainer.youthEmpowermentSubtext'),
+    },
+    {
+      link: 'https://www.plant-for-the-planet.org/restoration-organizations/',
+      image: '/assets/images/donorCircleInvitationImages/restorationTools.png',
+      title: t('infoAndCtaContainer.restorationToolsTitle'),
+      subtext: t('infoAndCtaContainer.restorationToolsSubtext'),
+    },
+    {
+      link: 'https://www.plant-for-the-planet.org/advice/',
+      image: '/assets/images/donorCircleInvitationImages/restorationAdvice.png',
+      title: t('infoAndCtaContainer.restorationAdviceTitle'),
+      subtext: t('infoAndCtaContainer.restorationAdviceSubtext'),
+    },
+    {
+      link: 'https://plant-for-the-planet.org/yucatan/',
+      image:
+        '/assets/images/donorCircleInvitationImages/forestRestorationAndConservation.png',
+      title: t('infoAndCtaContainer.restorationAndConservationTitle'),
+      subtext: t('infoAndCtaContainer.restorationAndConservationSubtext'),
+    },
+  ];
+
   return (
     <div className={styles.donorCircleInvitationContainer}>
       <DonorCircleInvitationHeader />
