0b669c6df9ff5157952027759dff37b388b9a1d3
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2024-07-18 19:18:54 +0530

    feat: provide toggle to switch between map and projectlist

--- src/features/projectsV2/MobileLandingPageView.tsx
+++ src/features/projectsV2/MobileLandingPageView.tsx
@@ -1,18 +1,46 @@
 import style from './MobileLandingPageView.module.scss';
 import { projects } from '../../utils/projectRedesign';
+import { useState } from 'react';
+
+const ProjectList = ({ isMapVisible }: { isMapVisible: boolean }) => {
+  return (
+    <>
+      {!isMapVisible && (
+        <div className={style.projectListMainContainer}>
+          <div className={style.projectListSubContainer}>
+            {projects.map((project) => (
+              <div key={project.id} className={style.projectCard}>
+                <h3>{project.title}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+const MainMap = () => {
+  return <div className={style.mainMapContainer}></div>;
+};
 
 const MobileLandingPageView = () => {
+  const [isMapVisible, setIsMapVisible] = useState(false);
+
+  const mapButtonClass = isMapVisible
+    ? style.activeMapTab
+    : style.inActiveMapTab;
+
   return (
     <div className={style.mobileLandingPage}>
-      <div className={style.projectListControltabsContainer}>Control tabs</div>
-      <div className={style.projectListMainContainer}>
-        <div className={style.projectListSubContainer}>
-          {projects.map((project) => (
-            <div key={project.id} className={style.projectCard}>
-              <h3>{project.title}</h3>
-            </div>
-          ))}
-        </div>
+      {isMapVisible ? <MainMap /> : <ProjectList isMapVisible={isMapVisible} />}
+      <div className={style.projectListControltabsContainer}>
+        <button
+          className={mapButtonClass}
+          onClick={() => setIsMapVisible(!isMapVisible)}
+        >
+          Map
+        </button>
       </div>
     </div>
   );
