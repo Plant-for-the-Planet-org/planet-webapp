e13871c0f3284c42023fce0b80b1de4daaa7228f
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-07-19 13:16:19 +0530

    feat: adds basic map using maplibre

--- src/features/common/Layout/ProjectsLayout/index.tsx
+++ src/features/common/Layout/ProjectsLayout/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styles from './ProjectsLayout.module.scss';
 import Credits from '../../../projects/components/maps/Credits';
 import { SetState } from '../../types/common';
+import ProjectsMap from './ProjectsMap';
 
 interface ProjectsLayoutProps {
   setCurrencyCode: SetState<string>;
@@ -15,7 +16,9 @@ const ProjectsLayout: FC<ProjectsLayoutProps> = ({
     <div className={styles.projectsLayout}>
       <main className={styles.mainContent}>
         <section className={styles.contentContainer}>{children}</section>
-        <section className={styles.mapContainer}></section>
+        <section className={styles.mapContainer}>
+          <ProjectsMap />
+        </section>
       </main>
       <Credits setCurrencyCode={setCurrencyCode} />
     </div>
