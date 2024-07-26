bd5ff5349ec863154fe921a96e13c5aae5830a92
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-07-26 11:54:18 +0530

    fix: hides popup tip

--- src/features/projectsV2/ProjectsMap/ProjectPopup/index.tsx
+++ src/features/projectsV2/ProjectsMap/ProjectPopup/index.tsx
@@ -1,5 +1,6 @@
 import { Popup } from 'react-map-gl-v7/maplibre';
 import { MapProject } from '../../../common/types/projectv2';
+import styles from './ProjectPopup.module.scss';
 
 type Props = {
   project: MapProject;
@@ -15,12 +16,14 @@ const ProjectPopup = ({ project, handlePopupLeave, visitProject }: Props) => {
       latitude={coordinates[1]}
       longitude={coordinates[0]}
       closeButton={false}
+      className={styles.projectPopup}
     >
       <div
         onMouseLeave={handlePopupLeave}
         onClick={() => visitProject(project.properties.slug)}
         onKeyDown={() => visitProject(project.properties.slug)}
       >
+        {/* Dummy content to be replaced with actual ProjectSnippet once ready */}
         <h3>{project.properties.name}</h3>
       </div>
     </Popup>
