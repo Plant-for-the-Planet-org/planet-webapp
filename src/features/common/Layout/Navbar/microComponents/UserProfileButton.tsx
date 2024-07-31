cf31536c80934c3eeecf3aa883ddc9ffa5f298dd
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2024-07-31 11:37:54 +0530

    feat: provide loader for useProfile icon

--- src/features/common/Layout/Navbar/microComponents/UserProfileButton.tsx
+++ src/features/common/Layout/Navbar/microComponents/UserProfileButton.tsx
@@ -4,13 +4,27 @@ import { useRouter } from 'next/router';
 import { useAuth0 } from '@auth0/auth0-react';
 import DefaultProfileImageIcon from '../../../../../../public/assets/images/icons/headerIcons/DefaultProfileImageIcon';
 import SignInButton from './SignInButton';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const ProfileIconSkeleton = () => {
+  return (
+    <div className="ProfileIconSkeleton">
+      <Skeleton />
+    </div>
+  );
+};
 
 const UserProfileButton = () => {
   const { user } = useUserProps();
   const router = useRouter();
   const { isAuthenticated, isLoading } = useAuth0();
 
-  if (!isAuthenticated && !isLoading) {
+  if (isLoading) {
+    return <ProfileIconSkeleton />;
+  }
+
+  if (!isAuthenticated) {
     return <SignInButton />;
   }
 
