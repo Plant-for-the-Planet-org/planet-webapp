00275248fa1f7489239c55c9f3b37e0713eb6d1e
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2024-11-08 10:58:09 +0530

    feat: enhance date formatting function to handle string cleanup and zero-padding

--- src/utils/countryCurrency/getFormattedDate.ts
+++ src/utils/countryCurrency/getFormattedDate.ts
@@ -3,23 +3,30 @@ import parseISO from 'date-fns/parseISO';
 import { localeMapForDate } from '../language/getLanguageName';
 
 export default function formatDate(date: number | Date | string) {
-  if (date) {
-    try {
-      if (typeof date === 'string') {
-        return format(parseISO(date), 'LLLL d, yyyy', {
-          locale: localeMapForDate[localStorage.getItem('language') || 'en'],
-        });
-      }
-      else {
-        return format(date, 'LLLL d, yyyy', {
-          locale: localeMapForDate[localStorage.getItem('language') || 'en'],
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      return '';
+  if (!date) {
+    return '';
+  }
+
+  try {
+    if (typeof date === 'string') {
+      // Clean up date string
+      const cleanDateString = date.split(' ')[0]; // Remove time portion
+      // Zero-padding month and day if needed
+      const [year, month, day] = cleanDateString.split('-');
+      const isoDateString = `${year}-${month.padStart(2, '0')}-${day.padStart(
+        2,
+        '0'
+      )}`;
+      return format(parseISO(isoDateString), 'LLLL d, yyyy', {
+        locale: localeMapForDate[localStorage.getItem('language') || 'en'],
+      });
+    } else {
+      return format(date, 'LLLL d, yyyy', {
+        locale: localeMapForDate[localStorage.getItem('language') || 'en'],
+      });
     }
-  } else {
+  } catch (error) {
+    console.log(error);
     return '';
   }
 }
