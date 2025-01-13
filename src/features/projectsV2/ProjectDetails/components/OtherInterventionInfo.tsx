98c4379098102b9464f9db20a671c2d3bd8af331
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-01-13 14:09:19 +0700

    refactor: extract createCardData fn outside component

--- src/features/projectsV2/ProjectDetails/components/OtherInterventionInfo.tsx
+++ src/features/projectsV2/ProjectDetails/components/OtherInterventionInfo.tsx
@@ -21,79 +21,35 @@ interface MetaDataValue {
   label: string;
 }
 
-
 interface PublicMetaData {
   [key: string]: string | MetaDataValue;
 }
 
-
-interface Props {
-  plantLocationInfo: OtherInterventions | null;
-  isMobile: boolean;
-  setSelectedSamplePlantLocation: SetState<SamplePlantLocation | null>;
+function isJsonString(str: string) {
+  try {
+    const parsed = JSON.parse(str);
+    return typeof parsed === 'object' && parsed !== null;
+  } catch (e) {
+    return false;
+  }
 }
 
-const OtherInterventionInfo = ({
-  plantLocationInfo,
-  isMobile,
-  setSelectedSamplePlantLocation,
-}: Props) => {
-  const tProjectDetails = useTranslations('ProjectDetails');
+const createCardData = (plantLocationInfo: OtherInterventions | null) => {
+  // Initialize an array to store the cleaned key-value pairs
+  const cleanedData: { key: string; value: string }[] = [];
 
+  // Extract metadata from the plantLocationInfo object, if it exists
+  const parsedData = plantLocationInfo?.metadata;
 
-  const { totalTreesCount } = useMemo(() => {
-    const totalTreesCount =
-      plantLocationInfo &&
-        plantLocationInfo.plantedSpecies &&
-        plantLocationInfo.plantedSpecies.length > 0
-        ? plantLocationInfo.plantedSpecies.reduce(
-          (sum, species: { treeCount: number }) => sum + species.treeCount,
-          0
-        )
-        : 0;
-    return { totalTreesCount };
-  }, [plantLocationInfo, plantLocationInfo?.type]);
-
-  const sampleInterventionSpeciesImages = useMemo(() => {
-    if (plantLocationInfo && plantLocationInfo.sampleInterventions.length > 0) {
-      const result = plantLocationInfo.sampleInterventions && plantLocationInfo.sampleInterventions.map((item) => {
-        return {
-          id: item.coordinates[0].id,
-          image: item.coordinates[0].image ?? '',
-          description: tProjectDetails('sampleTreeTag', { tag: item.tag }),
-        };
-      });
-      return result;
-    }
-  }, [plantLocationInfo]);
-
-  const shouldDisplayImageCarousel =
-    sampleInterventionSpeciesImages !== undefined &&
-    sampleInterventionSpeciesImages?.length > 0;
-
-
-
-  function isJsonString(str: string) {
-    try {
-      const parsed = JSON.parse(str);
-      return typeof parsed === 'object' && parsed !== null;
-    } catch (e) {
-      return false;
-    }
-  }
-
-
-  const createCardData = () => {
-    // Initialize an array to store the cleaned key-value pairs
-    const cleanedData: { key: string; value: string }[] = [];
-  
-    // Extract metadata from the plantLocationInfo object, if it exists
-    const parsedData = plantLocationInfo?.metadata;
-  
-    // Check if `parsedData.public` exists, is an object, and is not an array
-    if (parsedData?.public && typeof parsedData.public === 'object' && !Array.isArray(parsedData.public)) {
-      // Iterate over the entries of `parsedData.public` as key-value pairs
-      Object.entries(parsedData.public as PublicMetaData).forEach(([key, value]) => {
+  // Check if `parsedData.public` exists, is an object, and is not an array
+  if (
+    parsedData?.public &&
+    typeof parsedData.public === 'object' &&
+    !Array.isArray(parsedData.public)
+  ) {
+    // Iterate over the entries of `parsedData.public` as key-value pairs
+    Object.entries(parsedData.public as PublicMetaData).forEach(
+      ([key, value]) => {
         // Skip the entry if the key is 'isEntireSite' as it's used to show point location and no use to user
         if (key !== 'isEntireSite') {
           // If the value is a string, directly add it to cleanedData
@@ -101,14 +57,23 @@ const OtherInterventionInfo = ({
             cleanedData.push({ value, key });
           }
           // If the value is an object with `value` and `label` properties
-          else if (typeof value === 'object' && value !== null && 'value' in value && 'label' in value) {
+          else if (
+            typeof value === 'object' &&
+            value !== null &&
+            'value' in value &&
+            'label' in value
+          ) {
             // Check if the `value` property contains a valid JSON string
             if (isJsonString(value.value)) {
               try {
                 // Parse the JSON string
                 const parsedValue = JSON.parse(value.value);
                 // If the parsed value is an object with a `value` property, add it to cleanedData
-                if (parsedValue && typeof parsedValue === 'object' && 'value' in parsedValue) {
+                if (
+                  parsedValue &&
+                  typeof parsedValue === 'object' &&
+                  'value' in parsedValue
+                ) {
                   cleanedData.push({
                     key: value.label, // Use the `label` property as the key
                     value: parsedValue.value, // Use the parsed `value` property
@@ -127,21 +92,68 @@ const OtherInterventionInfo = ({
             }
           }
         }
-      });
+      }
+    );
+  }
+
+  // Return the array of cleaned key-value pairs
+  return cleanedData;
+};
+
+interface Props {
+  plantLocationInfo: OtherInterventions | null;
+  isMobile: boolean;
+  setSelectedSamplePlantLocation: SetState<SamplePlantLocation | null>;
+}
+
+const OtherInterventionInfo = ({
+  plantLocationInfo,
+  isMobile,
+  setSelectedSamplePlantLocation,
+}: Props) => {
+  const tProjectDetails = useTranslations('ProjectDetails');
+
+  const { totalTreesCount } = useMemo(() => {
+    const totalTreesCount =
+      plantLocationInfo &&
+      plantLocationInfo.plantedSpecies &&
+      plantLocationInfo.plantedSpecies.length > 0
+        ? plantLocationInfo.plantedSpecies.reduce(
+            (sum, species: { treeCount: number }) => sum + species.treeCount,
+            0
+          )
+        : 0;
+    return { totalTreesCount };
+  }, [plantLocationInfo, plantLocationInfo?.type]);
+
+  const sampleInterventionSpeciesImages = useMemo(() => {
+    if (plantLocationInfo && plantLocationInfo.sampleInterventions.length > 0) {
+      const result =
+        plantLocationInfo.sampleInterventions &&
+        plantLocationInfo.sampleInterventions.map((item) => {
+          return {
+            id: item.coordinates[0].id,
+            image: item.coordinates[0].image ?? '',
+            description: tProjectDetails('sampleTreeTag', { tag: item.tag }),
+          };
+        });
+      return result;
     }
-  
-    // Return the array of cleaned key-value pairs
-    return cleanedData;
-  };
-  
+  }, [plantLocationInfo]);
 
-  const cleanedPublicMetadata = createCardData()
+  const shouldDisplayImageCarousel =
+    sampleInterventionSpeciesImages !== undefined &&
+    sampleInterventionSpeciesImages?.length > 0;
 
-
+  const cleanedPublicMetadata = createCardData(plantLocationInfo);
 
   const content = [
     <>
-      <InterventionHeader plHid={plantLocationInfo?.hid} interventionType={plantLocationInfo?.type} key="interventionHeader" />
+      <InterventionHeader
+        plHid={plantLocationInfo?.hid}
+        interventionType={plantLocationInfo?.type}
+        key="interventionHeader"
+      />
       {shouldDisplayImageCarousel && (
         <ImageSlider
           key="imageSlider"
@@ -153,34 +165,39 @@ const OtherInterventionInfo = ({
         />
       )}
     </>,
-    
-    cleanedPublicMetadata.length > 0 && <>
-    <OtherInterventionInfoHeader
-      key="interventionHeader"
-      plantDate={plantLocationInfo?.interventionStartDate}
-    />
-    <OtherInterventionMetaData
-      key="plantingDetails"
-      metaData={cleanedPublicMetadata}
-      plantDate={plantLocationInfo?.interventionStartDate}
-      type={plantLocationInfo?.type}
-    />,
-    </>,
-    plantLocationInfo?.plantedSpecies && plantLocationInfo.plantedSpecies.length > 0 && (
-      <SpeciesPlanted
-        key="speciesPlanted"
-        totalTreesCount={totalTreesCount}
-        plantedSpecies={plantLocationInfo.plantedSpecies}
-      />
+
+    cleanedPublicMetadata.length > 0 && (
+      <>
+        <OtherInterventionInfoHeader
+          key="interventionHeader"
+          plantDate={plantLocationInfo?.interventionStartDate}
+        />
+        <OtherInterventionMetaData
+          key="plantingDetails"
+          metaData={cleanedPublicMetadata}
+          plantDate={plantLocationInfo?.interventionStartDate}
+          type={plantLocationInfo?.type}
+        />
+        ,
+      </>
     ),
+    plantLocationInfo?.plantedSpecies &&
+      plantLocationInfo.plantedSpecies.length > 0 && (
+        <SpeciesPlanted
+          key="speciesPlanted"
+          totalTreesCount={totalTreesCount}
+          plantedSpecies={plantLocationInfo.plantedSpecies}
+        />
+      ),
     plantLocationInfo &&
-    plantLocationInfo.sampleInterventions && plantLocationInfo.sampleInterventions.length > 0 && (
-      <SampleTrees
-        key="sampleTrees"
-        sampleInterventions={plantLocationInfo.sampleInterventions}
-        setSelectedSamplePlantLocation={setSelectedSamplePlantLocation}
-      />
-    ),
+      plantLocationInfo.sampleInterventions &&
+      plantLocationInfo.sampleInterventions.length > 0 && (
+        <SampleTrees
+          key="sampleTrees"
+          sampleInterventions={plantLocationInfo.sampleInterventions}
+          setSelectedSamplePlantLocation={setSelectedSamplePlantLocation}
+        />
+      ),
   ].filter(Boolean);
   return isMobile ? (
     <>
