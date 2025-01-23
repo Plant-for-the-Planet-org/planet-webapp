65e2f27e6c0b629e05469ebd9f5b7a955b76ad06
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2025-01-23 16:50:21 +0530

    feat: implement ReceiptListRedirect component with translations and contact support section

--- src/features/user/DonationReceipt/microComponents/ReceiptListRedirect.tsx
+++ src/features/user/DonationReceipt/microComponents/ReceiptListRedirect.tsx
@@ -1,9 +1,49 @@
-const ReceiptFooterSection = () => {
+import ContactIcon from '../../../../../public/assets/images/icons/ContactIcon';
+import RedirectRightArrowIcon from '../../../../../public/assets/images/icons/RedirectRightArrowIcon';
+import styles from '../donationReceipt.module.scss';
+import { useTranslations } from 'next-intl';
+
+const ReceiptListRedirect = () => {
+  const t = useTranslations('Donate.donationReceipt');
+
+  //TODO: logic need to be implemented
+  const handleOnclick = () => {};
   return (
-    <section>
-      <div></div>
+    <section className={styles.receiptListRedirect}>
+      <div className={styles.receiptListRedirectContainer}>
+        <div className={styles.receiptListRedirectSubContainer}>
+          <h3>{t('viewAllTaxReceipts')}</h3>
+          <p>{t('donationReceiptsManagementInfo')}</p>
+        </div>
+        <button onClick={handleOnclick}>
+          <RedirectRightArrowIcon />
+        </button>
+      </div>
+      <div className={styles.contactInfo}>
+        <ContactIcon />
+        <div>
+          <div className={styles.contactSupportMessage}>
+            {t('contactSupportMessage')}
+          </div>
+          <span>
+            {t.rich('supportContactDetails', {
+              supportLink: (chunk) => (
+                <a
+                  className="planet-links"
+                  href="mailto:spende@plant-for-the-planet.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {chunk}
+                </a>
+              ),
+              strong: (chunk) => <strong>{chunk}</strong>,
+            })}
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default ReceiptFooterSection;
+export default ReceiptListRedirect;
