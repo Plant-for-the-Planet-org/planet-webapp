e1721c972ec1472ec63308042c3fbc8b39bac661
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2025-01-29 13:11:05 +0530

    feat(ui): add donor contact management layout with header and back button.

--- src/features/user/DonorReceipt/DonorContactManagement.tsx
+++ src/features/user/DonorReceipt/DonorContactManagement.tsx
@@ -1,7 +1,26 @@
+import { useTranslations } from 'next-intl';
+import BackButton from '../../../../public/assets/images/icons/BackButton';
+import styles from './donationReceipt.module.scss';
+
 const DonorContactManagement = () => {
+  const t = useTranslations('Donate.donationReceipt');
+
+  //TODO: logic is pending
+  const navigateToVerificationPage = () => {};
+
   return (
-    <section>
-      <div></div>
+    <section className={styles.donorReceiptLayout}>
+      <div className={styles.donorContactManagement}>
+        <div className={styles.headerContainer}>
+          <button onClick={navigateToVerificationPage}>
+            <BackButton />
+          </button>
+          <h2 className={styles.contactManagementHeader}>
+            {t('contactManagementHeader')}
+          </h2>
+        </div>
+        <form></form>
+      </div>
     </section>
   );
 };
