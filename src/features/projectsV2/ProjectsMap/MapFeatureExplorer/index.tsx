3874bfcc1c3234fc766e8b8552748adf8e6d7e49
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-01-31 16:39:46 +0700

    refactor: extract YearRangeSlider to separate component (unused)

--- src/features/projectsV2/ProjectsMap/MapFeatureExplorer/index.tsx
+++ src/features/projectsV2/ProjectsMap/MapFeatureExplorer/index.tsx
@@ -2,29 +2,15 @@ import type { MapOptions } from '../../ProjectsMapContext';
 
 import React, { useState } from 'react';
 import { useTranslations } from 'next-intl';
+import { Modal } from '@mui/material';
 import { ExploreIcon } from '../../../../../public/assets/images/icons/projectV2/ExploreIcon';
-import styles from './MapFeatureExplorer.module.scss';
-// import { SmallSlider } from './CustomSlider';
-// import PlayIcon from '../../../../../public/assets/images/icons/projectV2/PlayIcon';
 import CustomButton from './CustomButton';
 import MapSettings from './MapSettings';
-import { Modal } from '@mui/material';
-
-/* interface ExploreProjectProps {
-  label: string | string[];
-  isOpen: boolean;
-  startYear: number;
-  endYear: number;
-} */
+import styles from './MapFeatureExplorer.module.scss';
 
 interface EcosystemOptionProps {
   label: string;
   switchComponent: React.ReactNode;
-}
-
-export interface YearRangeSliderProps {
-  startYear: number;
-  endYear: number;
 }
 
 export const MapLayerToggle = ({
@@ -42,54 +28,6 @@ export const MapLayerToggle = ({
     </>
   );
 };
-
-/* export const YearRangeSlider = () => {
-  const minDistance = 10;
-  const [value1, setValue1] = useState<number[]>([20, 37]);
-
-  function valuetext(value: number) {
-    return `${value}°C`;
-  }
-
-  const handleChange1 = (
-    event: Event,
-    newValue: number | number[],
-    activeThumb: number
-  ) => {
-    if (!Array.isArray(newValue)) {
-      return;
-    }
-
-    if (activeThumb === 0) {
-      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
-    } else {
-      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
-    }
-  };
-
-  return (
-    <div className={styles.rangeMainContainer}>
-      <div className={styles.rangeContainer}>
-        <div className={styles.playIconContainer}>
-          <PlayIcon width={'8px'} />
-        </div>
-        <div className={styles.sliderContainer}>
-          <SmallSlider
-            getAriaLabel={() => 'Minimum distance'}
-            value={value1}
-            onChange={handleChange1}
-            getAriaValueText={valuetext}
-            disableSwap
-          />
-        </div>
-      </div>
-      <div className={styles.yearRangeContainer}>
-        <div className={styles.startYear}>{'2012'}</div>
-        <div className={styles.endYear}>{'2024'}</div>
-      </div>
-    </div>
-  );
-}; */
 
 type MapFeatureExplorerProps = {
   mapOptions: MapOptions;
