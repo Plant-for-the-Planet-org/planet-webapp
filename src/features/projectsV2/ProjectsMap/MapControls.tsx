2e7fe922e87f25bed9ccb37894dd303fee59ce7b
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-03-19 10:56:13 +0700

    feat: add embed mode handling in MapControls

--- src/features/projectsV2/ProjectsMap/MapControls.tsx
+++ src/features/projectsV2/ProjectsMap/MapControls.tsx
@@ -2,7 +2,7 @@ import type { ViewMode } from '../../common/Layout/ProjectsLayout/MobileProjects
 import type { SetState } from '../../common/types/common';
 import type { MobileOs } from '../../../utils/projectV2';
 import type { SelectedTab } from './ProjectMapTabs';
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 import ProjectSiteDropdown from './ProjectSiteDropDown';
 import InterventionDropDown from './InterventionDropDown';
 import ProjectListControlForMobile from '../ProjectListControls/ProjectListControlForMobile';
@@ -13,6 +13,7 @@ import LayerDisabled from '../../../../public/assets/images/icons/LayerDisabled'
 import CrossIcon from '../../../../public/assets/images/icons/projectV2/CrossIcon';
 import styles from '../ProjectsMap/ProjectsMap.module.scss';
 import { AllInterventions } from '../../../utils/constants/intervention';
+import { ParamsContext } from '../../common/Layout/QueryParamsContext';
 
 interface MapControlsProps {
   isMobile: boolean;
@@ -56,6 +57,7 @@ const MapControls = ({
     setDisableInterventionMenu,
     plantLocations,
   } = useProjects();
+  const { embed } = useContext(ParamsContext);
 
   const uniquePlantTypes = useMemo(() => {
     if (!plantLocations) return [];
@@ -140,18 +142,24 @@ const MapControls = ({
         : styles.layerToggleIos
       : styles.layerToggleDesktop
   }`;
+  const projectListControlsContainerStyles = `${
+    styles.projectListControlsContainer
+  } ${embed === 'true' ? styles.embedModeMobile : ''}`;
+  const projectDetailsControlsContainerStyles = `${
+    styles.projectDetailsControlsContainer
+  } ${embed === 'true' ? styles.embedModeMobile : ''}`;
 
   return (
     <>
       {isMobile && page === 'project-list' && (
-        <div className={styles.projectListControlsContainer}>
+        <div className={projectListControlsContainerStyles}>
           <ProjectListControlForMobile {...projectListControlProps} />
         </div>
       )}
       {isProjectDetailsPage && (
         <>
           {isMobile ? (
-            <div className={styles.projectDetailsControlsContainer}>
+            <div className={projectDetailsControlsContainerStyles}>
               {hasProjectSites && (
                 <ProjectSiteDropdown {...siteDropdownProps} />
               )}
