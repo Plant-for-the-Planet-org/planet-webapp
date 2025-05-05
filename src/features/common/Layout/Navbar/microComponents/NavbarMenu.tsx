413ef1ea4d1adaa1cb6b64230e126a0a278f6280
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2025-05-05 13:55:37 +0530

    feat: create NavigationGroup and NavbarMenu component for handling nav item interactions

---
+++ src/features/common/Layout/Navbar/microComponents/NavbarMenu.tsx
@@ -0,0 +1,59 @@
+import type { HeaderItem, MenuItem } from '../defaultTenantConfig';
+
+import styles from '../NavbarMenu.module.scss';
+import { useTranslations } from 'next-intl';
+import { GreenRightChevron } from '../../../../../../public/assets/images/icons/NavbarMenuIcons';
+import NavbarMenuItem from './NavbarMenuItem';
+
+const isNavbarMenuItem = (menu: unknown): menu is MenuItem =>
+  'onlyIcon' in (menu as MenuItem);
+
+const NavbarMenu = ({ navItem }: { navItem: HeaderItem }) => {
+  if (!navItem || !navItem.menu) return null;
+  const tNavbarMenu = useTranslations('Common.navbarMenu');
+  return (
+    <section className={styles.navbarMenuSection}>
+      {navItem.headerKey === 'tools' && (
+        <>
+          <h2 id="navbar-menu-title" className={styles.sectionTitle}>
+            {tNavbarMenu.rich(`${navItem.headerKey}`, {
+              highlight: (chunks) => (
+                <strong className={styles.highlightLabel}>{chunks}</strong>
+              ),
+            })}
+          </h2>
+          <a
+            className={styles.allTools}
+            href={
+              'https://www.plant-for-the-planet.org/restoration-organizations'
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={tNavbarMenu('all')}
+          >
+            <span className={styles.label}>{tNavbarMenu('all')}</span>
+            <span className={styles.chevronIcon}>
+              <GreenRightChevron />
+            </span>
+          </a>
+        </>
+      )}
+      <div className={styles[`menu-${navItem.headerKey}`]}>
+        {navItem.menu?.map((menu) =>
+          isNavbarMenuItem(menu) ? (
+            <NavbarMenuItem
+              key={menu.menuKey}
+              menuKey={menu.menuKey}
+              description={menu.description}
+              title={menu.title}
+              link={menu.link}
+              visible={menu.visible}
+              onlyIcon={menu.onlyIcon}
+            />
+          ) : null
+        )}
+      </div>
+    </section>
+  );
+};
+export default NavbarMenu;
---
+++ src/features/common/Layout/Navbar/microComponents/NavigationGroup.tsx
@@ -0,0 +1,102 @@
+import type {
+  HeaderItem,
+  MenuSection,
+  NavbarItemHeaderKey,
+} from '../defaultTenantConfig';
+
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+import styles from '../NavbarMenu.module.scss';
+import DropdownUpArrow from '../../../../../../public/assets/images/icons/projectV2/DropdownUpArrow';
+import DropdownDownArrow from '../../../../../../public/assets/images/icons/projectV2/DropdownDownArrow';
+import themeProperties from '../../../../../theme/themeProperties';
+import NavbarMenuSection from './NavbarMenuSection';
+import { useRouter } from 'next/router';
+import NavbarMenu from './NavbarMenu';
+
+type NavItemProps = {
+  navItem: HeaderItem;
+  setOpenMenuKey: (key: NavbarItemHeaderKey | null) => void;
+  openMenuKey: NavbarItemHeaderKey | null;
+};
+
+const renderMenuSections = (menu: MenuSection[]) => {
+  return menu.map((section) => (
+    <NavbarMenuSection
+      key={section.title}
+      items={section.items}
+      title={section.title}
+      description={section.description}
+      sectionKey={section.sectionKey}
+    />
+  ));
+};
+
+const NavigationGroup = ({
+  navItem,
+  setOpenMenuKey,
+  openMenuKey,
+}: NavItemProps) => {
+  if (!navItem.visible) return null;
+
+  const isNavMenuOpen = openMenuKey === navItem.headerKey;
+  const { primaryDarkColor } = themeProperties;
+  const tNavItem = useTranslations('Common');
+  const router = useRouter();
+
+  const isActive = () => {
+    const { pathname } = router;
+    const strippedPathname =
+      pathname.replace(/^\/sites\/\[slug\]\/\[locale\]/, '') || '/';
+
+    if (strippedPathname === navItem.link || isNavMenuOpen) return true;
+    return false;
+  };
+  const isActiveClass = isActive() ? styles.activeNavItem : '';
+
+  const handleClick = () =>
+    setOpenMenuKey(isNavMenuOpen ? null : navItem.headerKey);
+  const handleMouseEnter = () => setOpenMenuKey(navItem.headerKey);
+  const handleMouseLeave = () => setOpenMenuKey(null);
+  return (
+    <div
+      className={styles.navigationGroup}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {navItem.link ? (
+        <Link href={navItem.link} prefetch={false}>
+          <span className={isActiveClass}>{tNavItem(navItem.title)}</span>
+        </Link>
+      ) : (
+        <button
+          onClick={handleClick}
+          role="button"
+          aria-haspopup="true"
+          aria-expanded={isNavMenuOpen}
+        >
+          <span className={isActiveClass}>{tNavItem(navItem.title)}</span>
+          <span className={styles.chevron}>
+            {isNavMenuOpen ? (
+              <DropdownUpArrow width={12} color={primaryDarkColor} />
+            ) : (
+              <DropdownDownArrow width={15} />
+            )}
+          </span>
+          {isNavMenuOpen && <div className={styles.triangle} />}
+        </button>
+      )}
+      {isNavMenuOpen && navItem.menu && (
+        <div className={`${styles.navMenu} ${styles[navItem.headerKey]}`}>
+          {navItem.hasSection === true ? (
+            renderMenuSections(navItem.menu as MenuSection[])
+          ) : (
+            <NavbarMenu navItem={navItem} />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavigationGroup;
