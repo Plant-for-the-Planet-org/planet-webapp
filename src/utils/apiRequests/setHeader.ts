f94967d84d9cdd3037201f51b41e8fd4f6e84a2b
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2025-07-17 11:02:42 +0530

    refactor: allow passing impersonation data directly or fallback to localStorage

--- src/utils/apiRequests/setHeader.ts
+++ src/utils/apiRequests/setHeader.ts
@@ -2,17 +2,29 @@ import type { ImpersonationData } from '../../features/user/Settings/Impersonate
 /**
  * Sets keys for header in impersonation mode
  */
-export const setHeaderForImpersonation = (header: Record<string, string>) => {
+export const setHeaderForImpersonation = (
+  header: Record<string, string>,
+  impersonationData?: ImpersonationData
+) => {
   const impersonationDataFromLocal: ImpersonationData = JSON.parse(
     `${localStorage.getItem('impersonationData')}`
   );
-  if (impersonationDataFromLocal) {
-    if (impersonationDataFromLocal?.targetEmail) {
-      header['X-SWITCH-USER'] = impersonationDataFromLocal?.targetEmail;
+  if (impersonationDataFromLocal || impersonationData) {
+    if (
+      impersonationData?.targetEmail ||
+      impersonationDataFromLocal?.targetEmail
+    ) {
+      header['X-SWITCH-USER'] =
+        impersonationData?.targetEmail ||
+        impersonationDataFromLocal?.targetEmail;
     }
 
-    if (impersonationDataFromLocal?.supportPin) {
-      header['X-USER-SUPPORT-PIN'] = impersonationDataFromLocal?.supportPin;
+    if (
+      impersonationData?.supportPin ||
+      impersonationDataFromLocal?.supportPin
+    ) {
+      header['X-USER-SUPPORT-PIN'] =
+        impersonationData?.supportPin || impersonationDataFromLocal?.supportPin;
     }
     const impersonationHeader = header;
     return impersonationHeader;
