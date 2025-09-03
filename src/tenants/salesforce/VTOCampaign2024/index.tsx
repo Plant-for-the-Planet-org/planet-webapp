f6193ae48d22bb08ae8680d59e11eaf43306f1f2
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2025-09-03 14:36:17 +0530

    refactor(salesforce): refactor styles across multiple components to use design system variables for colors and backgrounds.

--- src/tenants/salesforce/VTOCampaign2024/index.tsx
+++ src/tenants/salesforce/VTOCampaign2024/index.tsx
@@ -7,6 +7,7 @@ import LeaderBoard from './components/LeaderBoardSection';
 import AdditionalInfo from './components/AdditionalInfo';
 import React from 'react';
 import { useTenant } from '../../../features/common/Layout/TenantContext';
+import themeProperties from '../../../theme/themeProperties';
 interface Props {
   leaderboard: {
     mostDonated: { created: string; donorName: string; treeCount: string }[];
@@ -27,7 +28,12 @@ export default function Campaign({
       <Head>
         <title>{`VTO Fitness Challenge 2024 | ${tenantConfig.config.meta.title}`}</title>
       </Head>
-      <main style={{ backgroundColor: 'white', paddingBottom: '60px' }}>
+      <main
+        style={{
+          backgroundColor: themeProperties.designSystem.colors.white,
+          paddingBottom: '60px',
+        }}
+      >
         <Landing tenantScore={tenantScore} isLoaded={isLoaded} />
         <ContentSection />
         <ProjectGrid />
