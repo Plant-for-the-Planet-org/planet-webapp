ff6f5aaf7280c91560a17b84486e3392c0e6eb49
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-09-22 11:35:58 +0530

    feat: updates PlanetCashAccount interface
    - reorders properties
    - adds `temporaryCreditLimit` and `paymentMethods` properties
    - sets up `PaymentMethodInterface` and `PaymentMethodType`

--- src/features/common/types/planetcash.d.ts
+++ src/features/common/types/planetcash.d.ts
@@ -1,13 +1,26 @@
+import type { Nullable } from '@planet-sdk/common/build/types/util';
+
+export type PaymentMethodType = 'card' | 'sepa_debit';
+
+export interface PaymentMethodInterface {
+  id: string;
+  type: PaymentMethodType;
+  brand: string; //e.g. 'visa', 'mastercard', 'sepa';
+  last4: string; //last 4 digits of card or iban in case of sepa;
+}
+
 export interface PlanetCashAccount {
   id: string;
+  isActive: boolean;
   ownerName: string;
   balance: number;
   debit: number;
+  fee: number;
   creditLimit: number;
   currency: string; //'EUR';
   country: string; //'DE';
-  topUpThreshold: number;
-  topUpAmount: number;
-  isActive: boolean;
-  fee: number;
+  topUpThreshold: Nullable<number>;
+  topUpAmount: Nullable<number>;
+  temporaryCreditLimit: number;
+  paymentMethods: Array<PaymentMethodType>;
 }
