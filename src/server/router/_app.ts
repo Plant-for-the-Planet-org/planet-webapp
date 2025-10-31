68caa1ef5f9113d8f6aab986e4e4953522739498
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-10-31 11:55:15 +0530

    chore: remove remnants of trpc/prisma

--- src/server/router/_app.ts
+++
@@ -1,8 +0,0 @@
-import { router } from '../trpc';
-
-export const appRouter = router({
-  // myForest endpoints now handled by external API
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
