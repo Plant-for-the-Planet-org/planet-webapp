f4ea46807af44f0609f327c4b6a02de4ebaac39f
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-11-20 12:45:54 +0530

    fix: update iframe source URL to correct poster time parameter

--- src/tenants/concentrix/Home/sections/Banner/index.tsx
+++ src/tenants/concentrix/Home/sections/Banner/index.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
     <section className={styles.banner}>
       <div className={styles.bannerVideoContainer}>
         <iframe
-          src="https://customer-3h4q1m4a9rqr5i6y.cloudflarestream.com/352fc80fc79d7704e07962555fcb2041/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-3h4q1m4a9rqr5i6y.cloudflarestream.com%2F352fc80fc79d7704e07962555fcb2041%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
+          src="https://customer-3h4q1m4a9rqr5i6y.cloudflarestream.com/352fc80fc79d7704e07962555fcb2041/iframe?muted=true&preload=true&loop=true&autoplay=true&controls=false&poster=https%3A%2F%2Fcustomer-3h4q1m4a9rqr5i6y.cloudflarestream.com%2F352fc80fc79d7704e07962555fcb2041%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D0s%26height%3D600"
           className={styles.iframe}
           allow={
             'accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;'
