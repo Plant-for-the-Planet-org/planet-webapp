77b195888dffb4b374edb37ee812ed1dedfec6d3
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2025-12-22 13:02:36 +0530

    style: adjust error container and icon sizes for improved layout

--- src/features/common/WebGLGuard/index.tsx
+++ src/features/common/WebGLGuard/index.tsx
@@ -37,8 +37,8 @@ const WebGLGuard: FC<WebGLGuardProps> = ({
       <div className={styles.errorContainer}>
         <div className={styles.errorIcon}>
           <svg
-            width="48"
-            height="48"
+            width="36"
+            height="36"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
