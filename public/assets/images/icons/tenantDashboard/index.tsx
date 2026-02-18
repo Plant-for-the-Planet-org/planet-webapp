50fb23141eeefee68bc63023f983f1435e58105a
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-02-18 15:32:15 +0530

    feat: add barrel file for tenant dashboard icons

---
+++ public/assets/images/icons/tenantDashboard/index.tsx
@@ -0,0 +1,5 @@
+export { default as TreesPlantedIcon } from './TreesPlantedIcon';
+export { default as HectaresRestoredIcon } from './HectaresRestoredIcon';
+export { default as DonorsIcon } from './DonorsIcon';
+export { default as TotalDonatedIcon } from './TotalDonatedIcon';
+export { default as CountriesIcon } from './CountriesIcon';
