dbbf4085edc77c02e64e8bc5216404eefc6a0c7d
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-02-18 10:17:39 +0530

    feat: add common layout

--- pages/sites/[slug]/[locale]/profile/tenant-dashboard.tsx
+++ pages/sites/[slug]/[locale]/profile/tenant-dashboard.tsx
@@ -14,9 +14,20 @@ import {
 import { defaultTenant } from '../../../../../tenant.config';
 import getMessagesForPage from '../../../../../src/utils/language/getMessagesForPage';
 import TenantDashBoard from '../../../../../src/features/user/TenantDashboard';
+import UserLayout from '../../../../../src/features/common/Layout/UserLayout/UserLayout';
+import { useTranslations } from 'next-intl';
+import Head from 'next/head';
 
 const TenantDashboardPage = () => {
-  return <TenantDashBoard />;
+  const t = useTranslations('Profile.tenant');
+  return (
+    <UserLayout>
+      <Head>
+        <title>{t('tenantReport')}</title>
+      </Head>
+      <TenantDashBoard />
+    </UserLayout>
+  );
 };
 
 export default TenantDashboardPage;
@@ -50,7 +61,7 @@ export const getStaticProps: GetStaticProps<PageProps> = async (
 ): Promise<GetStaticPropsResult<PageProps>> => {
   const messages = await getMessagesForPage({
     locale: context.params?.locale as string,
-    filenames: ['common'],
+    filenames: ['profile', 'common', 'me'],
   });
 
   const tenantConfig =
