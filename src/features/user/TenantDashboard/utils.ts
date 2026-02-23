58ab42781f70558ae88d1a4dba6a68fd814a07d9
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-02-23 15:18:03 +0530

    refactor: remove redundant null check

--- src/features/user/TenantDashboard/utils.ts
+++ src/features/user/TenantDashboard/utils.ts
@@ -3,8 +3,6 @@ import type { Global } from '.';
 import { localizedAbbreviatedNumber } from '../../../utils/getFormattedNumber';
 
 export const formatStatNumber = (value: number, locale: string): string => {
-  if (value === null || value === undefined) return '0';
-
   const rounded = Math.floor(value);
 
   return rounded < 1000000
