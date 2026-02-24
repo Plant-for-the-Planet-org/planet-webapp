add6182cdf4c17221e6b97d4fb889856981909f2
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-02-24 09:51:01 +0530

    feat: add icon for empty state

--- src/features/user/TenantDashboard/components/microComponents/EmptyStateInfo.tsx
+++ src/features/user/TenantDashboard/components/microComponents/EmptyStateInfo.tsx
@@ -1,12 +1,14 @@
 import { clsx } from 'clsx';
 import { useTranslations } from 'next-intl';
 import styles from '../../TenantDashboard.module.scss';
+import NoDataFound from '../../../../../../public/assets/images/icons/projectV2/NoDataFound';
 
 const EmptyStateInfo = () => {
   const t = useTranslations('Profile.tenant');
   return (
     <div className={clsx(styles.card, styles.emptyState)}>
-      <p>{t('noDataForRange')}</p>
+      <NoDataFound />
+      <p className={styles.description}>{t('noDataForRange')}</p>
     </div>
   );
 };
