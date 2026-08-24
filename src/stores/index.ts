6a5d295bdaef04471b982139dadff69f33ca055e
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2026-08-24 11:21:56 +0530

    refactor: rename managePayoutStore.ts for consistency

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -5,7 +5,7 @@ export { useQueryParamStore } from './queryParamStore';
 export { useProjectMapStore } from './projectMapStore';
 export { useMyForestStore } from './myForestStore';
 export { useCurrencyStore } from './currencyStore';
-export { useManagePayoutStore } from './useManagePayout';
+export { useManagePayoutStore } from './managePayoutStore';
 export { usePlanetCashStore } from './planetCashStore';
 export { useProjectStore } from './projectStore';
 export { useSingleProjectStore } from './singleProjectStore';
