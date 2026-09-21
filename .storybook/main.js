8be2c0db251b6537749abdf0b18adeeced2e8db9
sunilsabatp <101264823+sunilsabatp@users.noreply.github.com>
2026-09-21 10:03:10 +0530

    chore(storybook): drop the unused addon-links package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs'],
 
   framework: {
     name: '@storybook/nextjs',
